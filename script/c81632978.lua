7e4c97a90285e9867bddbde66ae03b50c915eafc
Fran Luque <franluque2@gmail.com>
2022-11-25 20:18:05 -0300

    Bufff White Shield of the barians

--- script/c81632978.lua
+++ script/c81632978.lua
@@ -92,30 +92,57 @@ function s.putbackseraphfilter(c)
     return c:IsSetCard(0x86) and not c:IsPublic() and c:IsAbleToDeck() and c:IsMonster()
 end
 
+function s.resetbariancardfilter(c)
+	return c:IsCode(97769122, 91110378) and c:IsAbleToDeck()
+end
+
+function s.addstarseraphfilter(c)
+	return c:IsSetCard(0x86) and c:IsMonster() and c:IsAbleToHand()
+end
+
 --effects to activate during the main phase go here
 function s.flipcon2(e,tp,eg,ep,ev,re,r,rp)
 	--OPT check
 	--checks to not let you activate anything if you can't, add every flag effect used for opt/opd here
-	if Duel.GetFlagEffect(tp,id+1)>0  then return end
+	if Duel.GetFlagEffect(tp,id+1)>0 and Duel.GetFlagEffect(tp, id+3)>0 then return end
 	--Boolean checks for the activation condition: b1, b2
 
 --do bx for the conditions for each effect, and at the end add them to the return
 	local b1=Duel.GetFlagEffect(tp,id+1)==0
 			and Duel.IsExistingMatchingCard(s.putbackseraphfilter,tp,LOCATION_HAND,0,1,nil)
+		
+	local b2=Duel.GetFlagEffect(tp,id+3)==0
+	and Duel.IsExistingMatchingCard(s.resetbariancardfilter,tp,LOCATION_HAND,0,1,nil)
+	and Duel.IsExistingMatchingCard(s.addstarseraphfilter,tp,LOCATION_DECK,0,1,nil)
 
 
 
 --return the b1 or b2 or .... in parenthesis at the end
-	return aux.CanActivateSkill(tp) and (b1)
+	return aux.CanActivateSkill(tp) and (b1 or b2)
 end
 function s.flipop2(e,tp,eg,ep,ev,re,r,rp)
 	--"pop" the skill card
 	Duel.Hint(HINT_CARD,tp,id)
 	--Boolean check for effect 1:
+	local b1=Duel.GetFlagEffect(tp,id+1)==0
+			and Duel.IsExistingMatchingCard(s.putbackseraphfilter,tp,LOCATION_HAND,0,1,nil)
+		
+	local b2=Duel.GetFlagEffect(tp,id+3)==0
+	and Duel.IsExistingMatchingCard(s.resetbariancardfilter,tp,LOCATION_HAND,0,1,nil)
+	and Duel.IsExistingMatchingCard(s.addstarseraphfilter,tp,LOCATION_DECK,0,1,nil)
+
 
 --copy the bxs from above
+local op=Duel.SelectEffect(tp, {b1,aux.Stringid(id,1)},
+									  {b2,aux.Stringid(id,2)})
+	op=op-1
 
-	s.operation_for_res0(e,tp,eg,ep,ev,re,r,rp)
+	if op==0 then
+		s.operation_for_res0(e,tp,eg,ep,ev,re,r,rp)
+	else
+		s.operation_for_res1(e,tp,eg,ep,ev,re,r,rp)
+	end
+		
 end
 
 
@@ -136,6 +163,24 @@ function s.operation_for_res0(e,tp,eg,ep,ev,re,r,rp)
 --sets the opt (replace RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END with 0 to make it an opd)
 	Duel.RegisterFlagEffect(tp,id+1,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,0)
 end
+
+function s.operation_for_res1(e,tp,eg,ep,ev,re,r,rp)
+    local tc=Duel.SelectMatchingCard(tp, s.resetbariancardfilter, tp, LOCATION_HAND, 0, 1, 1,false,nil)
+    if tc then
+        Duel.ConfirmCards(1-tp, tc)
+		Duel.SendtoDeck(tc, tp, SEQ_DECKBOTTOM , REASON_EFFECT)
+        
+        local seraph=Duel.SelectMatchingCard(tp, s.addstarseraphfilter, tp, LOCATION_DECK, 0, 1, 1,false,nil)
+        Duel.SendtoHand(seraph, tp, REASON_EFFECT)
+
+        Duel.ConfirmCards(1-tp, seraph)
+    end
+
+
+--sets the opt (replace RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END with 0 to make it an opd)
+	Duel.RegisterFlagEffect(tp,id+3,0,0,0)
+end
+
 
 function s.flipcon3(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.GetCurrentChain()==0 and Duel.GetFlagEffect(tp, id+2)==0 and Duel.GetTurnCount()~=1 and Duel.GetTurnPlayer()==tp
