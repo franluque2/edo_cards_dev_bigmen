c07b7e45e4a17d26b775e4ce32e24854d7fc6be8
Fran Luque <franluque2@gmail.com>
2022-11-29 21:18:20 -0300

    Buffs to venomous bog

--- script/c81632958.lua
+++ script/c81632958.lua
@@ -17,6 +17,18 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 	aux.AddSkillProcedure(c,2,false,s.flipcon2,s.flipop2)
 end
+
+--change this to change the locations where this acts
+local LOCATIONS=LOCATION_ALL-LOCATION_OVERLAY
+
+--add archetype setcode here
+local ARCHETYPE=0x50
+
+function s.archetypefilter(c)
+  return c:IsType(TYPE_MONSTER) and c:IsRace(RACE_REPTILE)
+end
+
+
 function s.op(e,tp,eg,ep,ev,re,r,rp)
 	if e:GetLabel()==0 then
 		local e1=Effect.CreateEffect(e:GetHandler())
@@ -40,8 +52,34 @@ function s.op(e,tp,eg,ep,ev,re,r,rp)
 		Duel.RegisterEffect(e2,tp)
 
 
+		local e3=Effect.CreateEffect(e:GetHandler())
+		e3:SetType(EFFECT_TYPE_FIELD)
+		e3:SetCode(EFFECT_ADD_SETCODE)
+		e3:SetTargetRange(LOCATIONS,0)
+		e3:SetTarget(aux.TargetBoolFunction(s.archetypefilter))
+		e3:SetValue(ARCHETYPE)
+		Duel.RegisterEffect(e3,tp)
+
+
+		local e4=Effect.CreateEffect(e:GetHandler())
+		e4:SetType(EFFECT_TYPE_FIELD)
+		e4:SetCode(EFFECT_SUMMON_PROC)
+		e4:SetTargetRange(LOCATION_HAND,0)
+		e4:SetCondition(s.ntcon)
+		e4:SetTarget(aux.FieldSummonProcTg(s.nttg))
+		Duel.RegisterEffect(e4,tp)
+
+
 	end
 	e:SetLabel(1)
+end
+
+function s.ntcon(e,c,minc)
+	if c==nil then return true end
+	return minc==0 and Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE)>0
+end
+function s.nttg(e,c)
+	return c:IsSetCard(0x50) and c:GetTributeRequirement()>0
 end
 
 
