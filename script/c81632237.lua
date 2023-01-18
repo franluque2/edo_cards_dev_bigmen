b539f3a00e91d55a7818484dd002e91467523b88
Fran Luque <franluque2@gmail.com>
2023-01-17 21:47:02 -0300

    more bugfixes

--- script/c81632237.lua
+++ script/c81632237.lua
@@ -34,7 +34,7 @@ function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.Remove(e:GetHandler(),POS_FACEUP,REASON_COST)
 end
 function s.spfilter2(c,e,tp)
-	return (c:IsCode(511000005) or c:IsOriginalCode(81632229)) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+	return c:IsCode(511000005) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
@@ -65,7 +65,7 @@ function s.cfilter(c,ft,tp)
 	return c:IsFaceup() and c:IsCTToy() and (ft>0 or (c:IsControler(tp) and c:GetSequence()<5))
 end
 function s.filter(c,e,tp)
-	return c:IsCode(511000006) and c:IsCanBeSpecialSummoned(e,0,tp,true,true)
+	return (c:IsCode(511000006) or c:IsOriginalCode(81632229)) and c:IsCanBeSpecialSummoned(e,0,tp,true,true)
 end
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
