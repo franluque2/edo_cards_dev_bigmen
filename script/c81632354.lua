ce2f7f24ff5c5aaf3cad4cc3d93eba0cb2af9433
CaptPurpleHeart <ben.simga@gmail.com>
2023-02-05 13:46:49 +0000

    Overlord Fix Attempt Final

--- script/c81632354.lua
+++ script/c81632354.lua
@@ -40,7 +40,7 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 			local e1=Effect.CreateEffect(c)
 			e1:SetType(EFFECT_TYPE_SINGLE)
 			e1:SetCode(EFFECT_UPDATE_ATTACK)
-			e1:SetValue(g:GetFirst():GetLevel()*200 and g:GetRank()*200 and g:GetLink()*200)
+			e1:SetValue(g:GetFirst():GetLevel()+g:GetFirst():GetRank()+g:GetFirst():GetLink())
 				e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
 				c:RegisterEffectRush(e1)
 			end
