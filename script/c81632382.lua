3cbc330dd99337a53c4abb148e6ce2dddc1f0aea
CaptPurpleHeart <ben.simga@gmail.com>
2023-02-07 19:12:01 +0000

    Scripts for RDCA

--- script/c81632382.lua
+++ script/c81632382.lua
@@ -1,4 +1,41 @@
 --Savage Claw Tiger (CT)
-function c81632382.initial_effect(c)
-	
+local s,id=GetID()
+function s.initial_effect(c)
+	--Gain 400 ATK per level 7+ monster you control
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_ATKCHANGE)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetCountLimit(1)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDiscardDeckAsCost(tp,1) end
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsLevelAbove,7),tp,LOCATION_MZONE,0,1,nil) end
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	--Requirement
+	if Duel.DiscardDeck(tp,1,REASON_COST)>0 then
+		--Effect
+		if c:IsRelateToEffect(e) and c:IsFaceup() then
+			local atk=Duel.GetMatchingGroupCount(s.LowerLevelFilter,tp,LOCATION_MZONE,0,nil)
+			--Increase ATK
+			local e1=Effect.CreateEffect(c)
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_UPDATE_ATTACK)
+			e1:SetProperty(EFFECT_FLAG_COPY_INHERIT)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE+RESET_PHASE+PHASE_END)
+			e1:SetValue(atk*400)
+			c:RegisterEffect(e1)
+		end
+	end
+end
+function s.LowerLevelFilter(c)
+    return c:IsFaceup() and (c:IsLevelBelow(7) or c:IsRankBelow(7) or c:IsLinkBelow (3))
 end
