4188e5eeb13cd88ef29a31675c860a9e0e1de0d7
CaptPurpleHeart <ben.simga@gmail.com>
2023-04-29 22:12:58 +0100

    wow I suck

--- script/c81633008.lua
+++ script/c81633008.lua
@@ -49,9 +49,9 @@ function s.op(e,tp,eg,ep,ev,re,r,rp)
 
         local e5=Effect.CreateEffect(e:GetHandler())
         e5:SetType(EFFECT_TYPE_FIELD)
-        e5:SetCode(EFFECT_ADD_CODE)
+        e5:SetCode(EFFECT_ADD_RACE)
         e5:SetTargetRange(LOCATION_ONFIELD,0)
-        e5:SetTarget(function(_,c)  return c:IsHasEffect(id) end)
+        e5:SetTarget(aux.TargetBoolFunction(s.archetypefilter))
         e5:SetValue(RACE_DRAGON)
         Duel.RegisterEffect(e5,tp)
     
