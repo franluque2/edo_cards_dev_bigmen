7d511329145688457c2e402bb9362bee9d61fb1b
CaptPurpleHeart <ben.simga@gmail.com>
2023-05-01 02:38:28 +0100

    Fix

--- script/c81633010.lua
+++ script/c81633010.lua
@@ -51,7 +51,7 @@ function s.op(e,tp,eg,ep,ev,re,r,rp)
         e5:SetType(EFFECT_TYPE_FIELD)
         e5:SetCode(EFFECT_ADD_SETCODE)
         e5:SetTargetRange(LOCATIONS,0)
-        e5:SetTarget(function(_,c)  return c:IsHasEffect(id) end)
+        e5:SetTarget(aux.TargetBoolFunction(s.archetypefilter))
         e5:SetValue(ARCHETYPE)
         Duel.RegisterEffect(e5,tp)
     
@@ -88,32 +88,6 @@ function s.markedfilter(c,e)
     return #c:IsHasEffect(e)>0
 end
 
-
-function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetCurrentChain()==0 and Duel.GetTurnCount()==1 and Duel.GetFlagEffect(tp, id)==0
-end
-function s.flipop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
-	Duel.Hint(HINT_CARD,tp,id)
-
-    local g=Duel.GetMatchingGroup(s.archetypefilter, tp, LOCATION_ALL, LOCATION_ALL, nil)
-
-    if #g>0 then
-		local tc=g:GetFirst()
-		while tc do
-			
-				local e3=Effect.CreateEffect(e:GetHandler())
-				e3:SetType(EFFECT_TYPE_SINGLE)
-				e3:SetCode(id)
-				tc:RegisterEffect(e3)
-
-
-			tc=g:GetNext()
-		end
-	end
-
-	Duel.RegisterFlagEffect(tp,id,0,0,0)
-end
 
 function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.GetCurrentChain()==0 and Duel.GetTurnCount()==1 and Duel.GetFlagEffect(tp, id)==0
