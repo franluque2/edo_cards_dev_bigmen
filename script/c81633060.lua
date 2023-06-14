affd47f5036ac9c6ac387160ec494b93d4e639cb
CaptPurpleHeart <ben.simga@gmail.com>
2023-06-14 20:39:19 +0100

    fuck

--- script/c81633060.lua
+++ script/c81633060.lua
@@ -254,7 +254,7 @@ function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 end
 
 function s.isinfectionfly(c)
-    return c:IsOriginalCode(511002468)
+    return c:IsCode(511002468)
 end
 
 function s.infectionflyaddfilter(c)
