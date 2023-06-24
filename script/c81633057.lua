4d4261e57d62707e0fa78b67cde8c1da01e488c7
CaptPurpleHeart <ben.simga@gmail.com>
2023-06-24 06:35:30 +0100

    Fix

--- script/c81633057.lua
+++ script/c81633057.lua
@@ -102,7 +102,7 @@ function s.op(e,tp,eg,ep,ev,re,r,rp)
 end
 
 function s.xyzlv(e,c,rc)
-    if rc:IsSetCode(0x50e) then
+    if rc:IsMonster() then
         return 9,4,2,1, c:GetLevel()
     else return c:GetLevel()
     end
