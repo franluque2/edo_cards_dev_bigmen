31b9e406b7c7933afcf5f9ed946b993ae9c10ce6
CaptPurpleHeart <ben.simga@gmail.com>
2023-07-27 18:54:53 +0100

    Conscriptions

--- script/c81633078.lua
+++ script/c81633078.lua
@@ -1,4 +1,115 @@
 --Conscription of the Keepers
-function c81633078.initial_effect(c)
+--add archetype Template
+Duel.LoadScript("big_aux.lua")
+
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate Skill
+	aux.AddSkillProcedure(c,2,false,nil,nil)
+	local e1=Effect.CreateEffect(c)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_STARTUP)
+	e1:SetCountLimit(1)
+	e1:SetRange(0x5f)
+	e1:SetLabel(0)
+	e1:SetOperation(s.op)
+	c:RegisterEffect(e1)
+end
+
+
+
+--change this to change the locations where this acts
+local LOCATIONS=LOCATION_ALL-LOCATION_OVERLAY
+
+--add archetype setcode here
+local ARCHETYPE=0x1186
+
+--add the conditions for the archetype swap here
+function s.archetypefilter(c)
+  return c:IsType(TYPE_MONSTER) and c:IsRace(RACE_DINOSAUR)
+end
+
+
+
+
+
+function s.op(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetLabel()==0 then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e1:SetCode(EVENT_PHASE+PHASE_STANDBY)
+		e1:SetCondition(s.flipcon)
+		e1:SetOperation(s.flipop)
+		Duel.RegisterEffect(e1,tp)
+
+		--other passive duel effects go here
+
+        local e5=Effect.CreateEffect(e:GetHandler())
+        e5:SetType(EFFECT_TYPE_FIELD)
+        e5:SetCode(EFFECT_ADD_SETCODE)
+        e5:SetTargetRange(LOCATIONS,0)
+        e5:SetTarget(function(_,c)  return c:IsMonster() end)
+        e5:SetValue(0x152)
+        Duel.RegisterEffect(e5,tp)
+    
+
+	end
+	e:SetLabel(1)
+end
+
+
+function s.markedfilter(c,e)
+    return #c:IsHasEffect(e)>0
+end
+
+
+function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetCurrentChain()==0 and Duel.GetTurnCount()==1 and Duel.GetFlagEffect(tp, id)==0
+end
+function s.flipop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
+	Duel.Hint(HINT_CARD,tp,id)
+
+    local g=Duel.GetMatchingGroup(s.archetypefilter, tp, LOCATION_ALL, LOCATION_ALL, nil)
+
+    if #g>0 then
+		local tc=g:GetFirst()
+		while tc do
+			
+				local e3=Effect.CreateEffect(e:GetHandler())
+				e3:SetType(EFFECT_TYPE_SINGLE)
+				e3:SetCode(id)
+				tc:RegisterEffect(e3)
+
+
+			tc=g:GetNext()
+		end
+	end
+
+    local Sun=Duel.GetMatchingGroup(Card.IsCode, tp, LOCATION_EXTRA, 0, nil, 511009336)
+    if #Sun>0 then
+	local tc=Sun:GetFirst()
+		while tc do
 	
+            Fusion.AddProcMix(tc,true,true,57482479,aux.FilterBoolFunctionEx(Card.IsAttribute,ATTRIBUTE_LIGHT))
+
+
+			tc=Sun:GetNext()
+		end
+    end
+
+    local Moon=Duel.GetMatchingGroup(Card.IsCode, tp, LOCATION_EXTRA, 0, nil, 511009335)
+    if #Moon>0 then
+	local tc=Moon:GetFirst()
+		while tc do
+	
+            Fusion.AddProcMix(tc,true,true,64751286,aux.FilterBoolFunctionEx(Card.IsAttribute,ATTRIBUTE_DARK))
+
+
+			tc=Moon:GetNext()
+		end
+    end
+
+	Duel.RegisterFlagEffect(tp,id,0,0,0)
 end
