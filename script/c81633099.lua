2ca8013d943cd5be6a59fcde8237fc7ccfb68826
CaptPurpleHeart <ben.simga@gmail.com>
2023-09-24 23:20:22 +0100

    attempt

--- script/c81633099.lua
+++ script/c81633099.lua
@@ -31,7 +31,7 @@ function s.Goblins(c)
   end
   
   function s.Dinos(c)
-	return c:IsLevelBelow(6) and c:IsRace(Race_Dinosaur) and not c:IsCode(160203011)
+	return c:IsLevelBelow(6) and c:IsRace(Race_Dinosaur)
   end
   
   function s.Oasis(c)
