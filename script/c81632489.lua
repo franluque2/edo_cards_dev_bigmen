b891d2184d747cb45e6d26d4b036f236604e4604
CaptPurpleHeart <ben.simga@gmail.com>
2023-10-20 15:57:57 +0100

    Space Seed Driver

---
+++ script/c81632489.lua
@@ -0,0 +1,31 @@
+-- Space Seed Driver(CT)
+local s,id=GetID()
+function s.initial_effect(c)
+    --Add card to hand
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetCategory(CATEGORY_TOHAND)
+    e1:SetType(EFFECT_TYPE_IGNITION)
+    e1:SetRange(LOCATION_MZONE)
+    e1:SetCountLimit(1)
+    e1:SetCondition(s.condition)
+    e1:SetTarget(s.target)
+    e1:SetOperation(s.operation)
+    c:RegisterEffect(e1)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+    return Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsCode(81632487)),tp,LOCATION_ONFIELD,0,1,nil)
+end
+function s.thfilter(c)
+    return c:IsRace(RACE_CYBERSE) and c:IsLevelAbove(2) and c:IsAbleToHand()
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_GRAVE,0,1,e:GetHandler()) end
+    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_GRAVE)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    --effect
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+    local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_GRAVE,0,1,1,e:GetHandler())
+end
