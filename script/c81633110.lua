be4d4b13263655d827d55754d3e9cecf3f8b7e02
Fran Luque <franluque2@gmail.com>
2023-11-06 00:54:40 -0300

    add pegasus

--- script/c81633110.lua
+++ script/c81633110.lua
@@ -87,6 +87,29 @@ function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 		end
 	end
 
+	g=Duel.GetMatchingGroup(Card.IsOriginalRace, tp, LOCATION_ALL, LOCATION_ALL, nil,RACE_ILLUSION)
+
+    if #g>0 then
+		local tc=g:GetFirst()
+		while tc do
+
+			local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_FIELD)
+		e1:SetCode(EFFECT_CANNOT_BATTLE_INDES)
+		e1:SetRange(LOCATION_MZONE)
+		e1:SetTargetRange(LOCATION_MZONE,0)
+		e1:SetValue(s.batval)
+		tc:RegisterEffect(e1)
+
+            tc=g:GetNext()
+        end
+    end
+
 	Duel.RegisterFlagEffect(tp,id,0,0,0)
+end
+
+
+function s.batval(e,re,c)
+	return re:GetHandler():IsOriginalRace(RACE_ILLUSION)
 end
 
