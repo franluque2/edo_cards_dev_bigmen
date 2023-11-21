f1c2b65f2d8e0880142b6c4ecdac1bd641b3b604
Fran Luque <franluque2@gmail.com>
2023-11-20 22:53:34 -0300

    Almost done with this monster

--- script/c81633113.lua
+++ script/c81633113.lua
@@ -39,31 +39,89 @@ function s.op(e,tp,eg,ep,ev,re,r,rp)
 	e:SetLabel(1)
 end
 
-local decks={}
-decks[0] = { {}, {} , {} }
-decks[1] = { {}, {} , {} }
-decks[2] = { {}, {} , {} }
-decks[3] = { {}, {} , {} }
-decks[4] = { {}, {} , {} }
-decks[5] = { {}, {} , {} }
-decks[6] = { {}, {} , {} }
-decks[7] = { {}, {} , {} }
-decks[8] = { {}, {} , {} }
-decks[9] = { {}, {} , {} }
-decks[10] = { {}, {} , {} }
-decks[11] = { {}, {} , {} }
-decks[12] = { {}, {} , {} }
-decks[13] = { {}, {} , {} }
-decks[14] = { {}, {} , {} }
-decks[15] = { {}, {} , {} }
-decks[16] = { {}, {} , {} }
-decks[17] = { {}, {} , {} }
-decks[18] = { {}, {} , {} }
-decks[19] = { {}, {} , {} }
-decks[20] = { {}, {} , {} }
-decks[21] = { {}, {} , {} }
-decks[22] = { {}, {} , {} }
-decks[23] = { {}, {} , {} }
+local decks={{},{}}
+
+-- monster, extra monster, backrow
+decks[0][0] = { {}, {} , {} }
+decks[1][0] = { {}, {} , {} }
+
+decks[0][1] = { {}, {} , {} }
+decks[1][1] = { {}, {} , {} }
+
+decks[0][2] = { {}, {} , {} }
+decks[1][2] = { {}, {} , {} }
+
+decks[0][3] = { {}, {} , {} }
+decks[1][3] = { {}, {} , {} }
+
+decks[0][4] = { {}, {} , {} }
+decks[1][4] = { {}, {} , {} }
+
+decks[0][5] = { {}, {} , {} }
+decks[1][5] = { {}, {} , {} }
+
+decks[0][6] = { {}, {} , {} }
+decks[1][6] = { {}, {} , {} }
+
+decks[0][7] = { {}, {} , {} }
+decks[1][7] = { {}, {} , {} }
+
+decks[0][8] = { {}, {} , {} }
+decks[1][8] = { {}, {} , {} }
+
+decks[0][9] = { {}, {} , {} }
+decks[1][9] = { {}, {} , {} }
+
+decks[0][10] = { {}, {} , {} }
+decks[1][10] = { {}, {} , {} }
+
+decks[0][11] = { {}, {} , {} }
+decks[1][11] = { {}, {} , {} }
+
+decks[0][12] = { {}, {} , {} }
+decks[1][12] = { {}, {} , {} }
+
+decks[0][13] = { {}, {} , {} }
+decks[1][13] = { {}, {} , {} }
+
+decks[0][14] = { {}, {} , {} }
+decks[1][14] = { {}, {} , {} }
+
+decks[0][15] = { {}, {} , {} }
+decks[1][15] = { {}, {} , {} }
+
+decks[0][16] = { {}, {} , {} }
+decks[1][16] = { {}, {} , {} }
+
+decks[0][17] = { {}, {} , {} }
+decks[1][17] = { {}, {} , {} }
+
+decks[0][18] = { {}, {} , {} }
+decks[1][18] = { {}, {} , {} }
+
+decks[0][19] = { {}, {} , {} }
+decks[1][19] = { {}, {} , {} }
+
+decks[0][20] = { {}, {} , {} }
+decks[1][20] = { {}, {} , {} }
+
+decks[0][21] = { {}, {} , {} }
+decks[1][21] = { {}, {} , {} }
+
+decks[0][22] = { {}, {} , {} }
+decks[1][22] = { {}, {} , {} }
+
+decks[0][23] = { {}, {} , {} }
+decks[1][23] = { {}, {} , {} }
+
+
+
+
+
+
+
+
+
 
 function s.getyearval(tp)
 	return math.ceil(Duel.GetTurnCount()/2) + Duel.GetFlagEffect(tp, id)
@@ -77,18 +135,114 @@ end
 function s.changeop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_CARD,tp,id)
 
-
+	s.flipdownbackrow(tp)
+	s.refillextra(tp)
+	s.shufflebackpends(tp)
+	s.changemononfield(tp)
+	s.changebackrowonfield(tp)
+	s.changeextra(tp)
+	s.changeallelse(tp)
 
 end
 
 function s.flipdownbackrow(tp)
+	local g=Duel.GetMatchingGroup(s.turnspelltrapfdfilter, tp, LOCATION_SZONE, 0, nil)
 
+	if g and #g>0 then
+		for tc in g:Iter() do
+			if tc:IsType(TYPE_PENDULUM) then
+				Duel.SendtoHand(tc, tp, REASON_RULE)
+			else 
+				Duel.SSet(tp,tc,tp,false)
+			end
+		end
+	end
 end
 
 function s.refillextra(tp)
+	local g=Duel.GetMatchingGroup(s.replaceextragravefilter, tp, LOCATION_GRAVE+LOCATION_REMOVED, 0, nil)
+
+	if g and #g>0 then
+		Duel.SendtoDeck(g, tp, SEQ_DECKTOP, REASON_RULE)
+	end
 end
 
 function s.shufflebackpends(tp)
+	local g=Duel.GetMatchingGroup(s.shufflependreplacefilter, tp, LOCATION_EXTRA, 0, nil)
+
+	if g and #g>0 then
+		Duel.SendtoDeck(g, tp, SEQ_DECKSHUFFLE, REASON_RULE)
+	end
+end
+
+function s.changemononfield(tp)
+	local g=Duel.GetMatchingGroup(s.replacemonfilter, tp, LOCATION_ONFIELD, 0, nil)
+
+	if g and #g>0 then
+		for tc in g:Iter() do
+			if tc:IsAbleToExtraAsCost() then
+				local num=Duel.GetRandomNumber(1, #decks[tp][s.getyearval(tp)][2])
+				Card.Recreate(tc, decks[tp][s.getyearval(tp)][2][num], nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,true)
+				table.remove(decks[tp][s.getyearval(tp)][2][num])
+			else
+				local num=Duel.GetRandomNumber(1, #decks[tp][s.getyearval(tp)][1])
+				Card.Recreate(tc, decks[tp][s.getyearval(tp)][1][num], nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,true)
+				table.remove(decks[tp][s.getyearval(tp)][1][num])
+			end
+		end
+	end
+end
+
+function s.changebackrowonfield(tp)
+	local g=Duel.GetMatchingGroup(s.replacespefilter, tp, LOCATION_ONFIELD, 0, nil)
+
+	if g and #g>0 then
+		for tc in g:Iter() do
+			local num=Duel.GetRandomNumber(1, #decks[tp][s.getyearval(tp)][3])
+			Card.Recreate(tc, decks[tp][s.getyearval(tp)][3][num],nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,true)
+			table.remove(decks[tp][s.getyearval(tp)][3][num])
+		end
+	end
+end
+
+function s.changeextra(tp)
+	local g=Duel.GetMatchingGroup(s.replacemonfilter, tp, LOCATION_EXTRA, 0, nil)
+
+	if g and #g>0 then
+		for tc in g:Iter() do
+			local num=Duel.GetRandomNumber(1, #decks[tp][s.getyearval(tp)][2])
+			Card.Recreate(tc, decks[tp][s.getyearval(tp)][2][num],nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,true)
+			table.remove(decks[tp][s.getyearval(tp)][2][num])
+		end
+	end
+end
+
+function s.changeallelse(tp)
+	local g=Duel.GetMatchingGroup(s.replacecardfilter, tp, LOCATION_HAND|LOCATION_DECK|LOCATION_GRAVE|LOCATION_REMOVED|LOCATION_OVERLAY, 0, nil)
+
+	if g and #g>0 then
+		for tc in g:Iter() do
+			local cardtyp=-1
+			if #decks[tp][s.getyearval(tp)][1]==0 then
+				if #decks[tp][s.getyearval(tp)][3]==0 then return end
+				cardtyp=3
+			end
+
+			if #decks[tp][s.getyearval(tp)][3]==0 then
+				cardtyp=1
+			end
+
+			if cardtyp==-1 then
+				cardtyp=Duel.GetRandomNumber(1,2)
+				if cardtyp==2 then cardtyp=3 end
+			end
+
+			local num=Duel.GetRandomNumber(1, #decks[tp][s.getyearval(tp)][cardtyp])
+			Card.Recreate(tc, decks[tp][s.getyearval(tp)][cardtyp][num],nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,true)
+			table.remove(decks[tp][s.getyearval(tp)][cardtyp][num])
+		end
+	end
+
 end
 
 local bosses={48626373,48626373,48626373,48626373,48626373,48626373}
@@ -128,6 +282,10 @@ end
 
 function s.nostalgiamonfilter(c)
     return c:IsCode(15845914,0010000) and c:IsFaceup()
+end
+
+function s.replacecardfilter(c)
+	return (c:GetOwner()==c:GetControler() and not c:IsType(TYPE_TOKEN)) and not ((c:GetFlagEffect(id)>0) or c:IsCode(15845914,0010000))
 end
 
 function s.replacemonfilter(c)
