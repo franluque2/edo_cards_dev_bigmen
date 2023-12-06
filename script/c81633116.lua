cee5ed59318167f06e1fe288d2d0f449c693e62e
Fran Luque <franluque2@gmail.com>
2023-12-06 20:18:06 -0300

    cleaner way to do it

--- script/c81633116.lua
+++ script/c81633116.lua
@@ -62,7 +62,6 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
     local card=re:GetHandler()
 
-    card:EnableCounterPermit(0x10)
     card:AddCounter(0x10,1)
 
 
@@ -75,6 +74,11 @@ end
 function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
 	Duel.Hint(HINT_CARD,tp,id)
+
+    local darkdragons=Duel.GetMatchingGroup(Card.IsOriginalCode, tp, LOCATION_ALL, LOCATION_ALL, nil, 60992105,100000282)
+    for dragon in darkdragons:Iter() do
+        dragon:EnableCounterPermit(0x10)
+    end
 
 	Duel.RegisterFlagEffect(tp,id,0,0,0)
 end
