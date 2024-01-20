ad374b4c03d6156450727cdcc21d9eda26325c11
fran L <franluque2@gmail.com>
2024-01-20 02:42:33 -0300

    Add Foucault's Broken Realm

---
+++ script/c811632007.lua
@@ -0,0 +1,57 @@
+--Foucault's Broken Realm
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate Skill
+	aux.AddSkillProcedure(c,2,false,nil,nil)
+	local e1=Effect.CreateEffect(c)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_STARTUP)
+	e1:SetCountLimit(1)
+	e1:SetRange(0x5f)
+	e1:SetLabel(0)
+	e1:SetOperation(s.op)
+	c:RegisterEffect(e1)
+end
+
+function s.op(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetLabel()==0 then
+        local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e1:SetCode(EVENT_PREDRAW)
+		e1:SetCondition(s.flipcon)
+		e1:SetOperation(s.flipop)
+		Duel.RegisterEffect(e1,tp)
+    
+
+		--other passive duel effects go here    
+
+        local e7=Effect.CreateEffect(e:GetHandler())
+        e7:SetType(EFFECT_TYPE_FIELD)
+        e7:SetCode(EFFECT_IMMUNE_EFFECT)
+        e7:SetTargetRange(LOCATION_ONFIELD,0)
+        e7:SetProperty(EFFECT_FLAG_SET_AVAILABLE+EFFECT_FLAG_IGNORE_IMMUNE)
+        e7:SetValue(s.efilter)
+        Duel.RegisterEffect(e7, tp)
+
+
+
+	end
+	e:SetLabel(1)
+end
+
+function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetCurrentChain()==0 and Duel.GetTurnCount()==1
+end
+function s.flipop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
+	Duel.Hint(HINT_CARD,tp,id)
+end
+
+
+function s.efilter(e,te, c)
+	return e:GetHandler()~=te:GetHandler() and c~=te:GetHandler() and te:GetHandler():IsLocation(LOCATION_ONFIELD) and not c:GetColumnGroup():IsContains(te:GetHandler())
+end
+
+
+
