836c33c69110b1e849f2e4bd5734c5d4248e93b0
CaptPurpleHeart <ben.simga@gmail.com>
2024-03-08 16:36:01 +0000

    effect gy

--- script/c81632554.lua
+++ script/c81632554.lua
@@ -11,6 +11,31 @@ function s.initial_effect(c)
 	e1:SetCondition(s.indcon)
 	e1:SetValue(1)
 	c:RegisterEffect(e1)
+
+    local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_FIELD)
+	e2:SetCategory(CATEGORY_TOHAND)
+	e2:SetCode(EVENT_PHASE+PHASE_STANDBY)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCondition(s.condition)
+    e2:SetCountLimit(1,{id,1})
+	e2:SetTarget(s.target)
+	e2:SetOperation(s.operation)
+	c:RegisterEffect(e2)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTurnPlayer()==tp and Duel.IsExistingMatchingCard(s.CuntSpells,tp,LOCATION_SZONE,0,1,nil)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsAbleToHand() end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,e:GetHandler(),1,0,0)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetHandler():IsRelateToEffect(e) then
+		Duel.SendtoHand(e:GetHandler(),nil,REASON_EFFECT)
+	end
+end
 end
 function s.AncientMonster(e,c)
 	return c:IsFaceup() and c:IsCode(511000126, 511000127, 38520918, 511000128, 76232340, 47986555, 32012841)
