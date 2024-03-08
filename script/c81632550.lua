f5bcfcebaa5399783953b555c1ceb98f5d197a5d
CaptPurpleHeart <ben.simga@gmail.com>
2024-03-08 09:18:27 +0000

    Ancient Key

---
+++ script/c81632550.lua
@@ -0,0 +1,32 @@
+--Ancient Key (CT)
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) or Duel.GetLocationCount(tp,LOCATION_MZONE)<2 then return end
+	local g=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_DECK,0,nil,e,tp)
+	local dg=g:Filter(s.filter2,nil,g)
+	if #dg>=1 then
+		local fid=e:GetHandler():GetFieldID()
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local sg=dg:Select(tp,1,1,nil)
+		local tc1=sg:GetFirst()
+		dg:RemoveCard(tc1)
+		local tc2=dg:Filter(Card.IsCode,nil,tc1:GetCode()):GetFirst()
+		Duel.SpecialSummonStep(tc1,0,tp,tp,false,false,POS_FACEUP)
+		Duel.SpecialSummonStep(tc2,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
+function s.spfilter(c,e,tp)
+	return c:IsLevelBelow(4) and c:IsRace(RACE_ROCK) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.filter2(c,g)
+	return g:IsExists(Card.IsCode,1,c,c:GetCode())
+end
