0b3e0a573e887c24c863e1d5d2a204ad9588fc1f
CaptPurpleHeart <ben.simga@gmail.com>
2024-03-08 07:47:27 +0000

    attempt

---
+++ script/c81632549.lua
@@ -0,0 +1,90 @@
+--Ancient City (CT)
+local s,id=GetID()
+Duel.LoadScript("c420.lua")
+function s.initial_effect(c)
+    --Activation Search
+    local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+
+    --Special Summon
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_ACTIVATE)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetCost(s.cost)
+	e2:SetTarget(s.spop)
+	e2:SetOperation(s.spop)
+	c:RegisterEffect(e2)
+
+    --Special Summon "Ancient Dragon" during at the end of the Turn it was destroyed 
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e3:SetCode(EVENT_DESTROYED)
+	e3:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+	e3:SetRange(LOCATION_FZONE)
+	e3:SetCondition(s.respcon)
+	e3:SetOperation(s.respop)
+	c:RegisterEffect(e3)
+
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(s.thfilter,tp,LOCATION_DECK,0,nil)
+	if #g>0 and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local sg=g:Select(tp,1,1,nil)
+		Duel.SendtoHand(sg,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,sg)
+	end
+end
+function s.thfilter(c)
+	return c:IsCode(511000123, 511000124, 511000125) and c:IsAbleToHand()
+end
+function s.spfilter(c)
+	return c:IsFaceup() and c:GetType()==TYPE_SPELL+TYPE_CONTINUOUS and c:IsAbleToGraveAsCost()
+end
+function s.Dragon(c)
+	return c:Is(c:IsCode(511000128))
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local tg=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_ONFIELD,0,nil)
+	if chk==0 then
+		e:SetLabel(1)
+		return Duel.GetLocationCount(tp,LOCATION_SZONE)>-3 and #tg>=3 and aux.SelectUnselectGroup(tg,e,tp,3,3,aux.ChkfMMZ(1),0)
+    end
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) or Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	local lv=e:GetLabel()
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.Dragon,tp,LOCATION_DECK|LOCATION_HAND|LOCATION_GRAVE,0,1,1,nil,e,tp,lv)
+	if #g>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
+function s.respcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.cfilter,1,nil,e,tp)
+end
+function s.respop(e,tp,eg,ep,ev,re,r,rp)
+	local g=eg:Filter(s.cfilter,nil,e,tp)
+	if #g==0 then return end
+	for tc in g:Iter() do
+		--Special Summon during the end of the turn
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e1:SetCode(EVENT_PHASE+PHASE_END)
+		e1:SetRange(LOCATION_GRAVE+LOCATION_REMOVED)
+		e1:SetReset(RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_END)
+		e1:SetCountLimit(1)
+		e1:SetOperation(s.spcop)
+		tc:RegisterEffect(e1)
+	end
+end
+function s.spcop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_CARD,0,id)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.SpecialSummon(e:GetHandler(),0,tp,tp,false,false,POS_FACEUP)
+end
