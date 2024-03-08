fde451b461706b11a1650b52e822c1a6133d04d1
CaptPurpleHeart <ben.simga@gmail.com>
2024-03-08 16:11:15 +0000

    Stone Giant

--- script/c81632554.lua
+++ script/c81632554.lua
@@ -1,5 +1,21 @@
 --Stone Giant (CT)
 local s,id=GetID()
 function s.initial_effect(c)
+	--Cannot be destroyed by battle or card effects
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_FIELD)
+    e1:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+    e1:SetTargetRange(LOCATION_MZONE,0)
+    e1:SetTarget(aux.TargetBoolFunction(s.maidenfilter,tp))
+    e1:SetValue(1)
+    Duel.RegisterEffect(e1,tp)
 
+end
+
+function s.maidenfilter(c,tp)
+	return c:IsCode(511000126, 511000127, 38520918, 511000128, 76232340, 47986555, 32012841) and c:IsFaceup() and Duel.IsExistingMatchingCard(s.defmaidenfilter, tp, LOCATION_SZONE, 0, 1, nil)
+end
+
+function s.defmaidenfilter(c)
+	return c:GetType()==TYPE_SPELL+TYPE_CONTINUOUS and c:IsFaceup()
 end
