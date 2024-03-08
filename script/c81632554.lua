67cebf07aee22f3c52b82f90630587fc5dbccb11
CaptPurpleHeart <ben.simga@gmail.com>
2024-03-08 16:18:04 +0000

    effect

--- script/c81632554.lua
+++ script/c81632554.lua
@@ -6,17 +6,18 @@ function s.initial_effect(c)
     e1:SetType(EFFECT_TYPE_FIELD)
     e1:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
     e1:SetTargetRange(LOCATION_MZONE,0)
-    e1:SetTarget(aux.TargetBoolFunction(s.maidenfilter))
+    e1:SetTarget(aux.TargetBoolFunction(s.maidenfilter,tp))
     e1:SetCondition(s.incon)
     e1:SetValue(1)
-    c:RegisterEffect(e1)
+    c:RegisterEffect(e1,tp)
 
 end
+
 function s.incon(e)
 	return  Duel.IsExistingMatchingCard(s.defmaidenfilter, tp, LOCATION_SZONE, 0, 1, nil)
 end
 
-function s.maidenfilter(c)
+function s.maidenfilter(c,tp)
 	return c:IsCode(511000126, 511000127, 38520918, 511000128, 76232340, 47986555, 32012841) and c:IsFaceup()
 end
 
