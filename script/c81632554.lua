bfca6dfb9c93190eeb7db01cd2021f1fd5c32c00
CaptPurpleHeart <ben.simga@gmail.com>
2024-03-08 16:28:05 +0000

    fix

--- script/c81632554.lua
+++ script/c81632554.lua
@@ -1,7 +1,7 @@
 --Stone Giant (CT)
 local s,id=GetID()
 function s.initial_effect(c)
-	-- "Therions" monsters protection
+	-- monsters protection
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
@@ -12,10 +12,14 @@ function s.initial_effect(c)
 	e1:SetValue(aux.indoval)
 	c:RegisterEffect(e1)
 end
-function s.AncientMonster(c)
+function s.AncientMonster(e,c)
 	return c:IsFaceup() and c:IsCode(511000126, 511000127, 38520918, 511000128, 76232340, 47986555, 32012841)
 end
 
+function s.CuntSpells(e,c)
+	return c:IsFaceup() and c:GetType()==TYPE_SPELL+TYPE_CONTINUOUS
+end
+
 function s.indcon(e)
-	return Duel.IsExistingMatchingCard(Card.IsType,e:GetHandlerPlayer(),LOCATION_ONFIELD,0,1,nil,TYPE_SPELL+TYPE_CONTINUOUS)
+	return Duel.IsExistingMatchingCard(s.CuntSpells,e:GetHandlerPlayer(),LOCATION_SZONE,0,1,nil)
 end
