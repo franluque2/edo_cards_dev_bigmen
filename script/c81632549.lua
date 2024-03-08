0f0bd285b257200e8cfcb2ee1b80810f8ea07414
CaptPurpleHeart <ben.simga@gmail.com>
2024-03-08 08:41:00 +0000

    fix

--- script/c81632549.lua
+++ script/c81632549.lua
@@ -44,7 +44,7 @@ function s.initial_effect(c)
 end
 
 function s.costfilter(c,code,ft)
-	return c:IsFaceup() and c:IsType(TYPE_SPELL+TYPE_CONTINUOUS) and c:IsAbleToGraveAsCost() and (not ft or ft>0 or c:GetSequence()<5) 
+	return c:IsFaceup() and c:IsCode(code) and c:IsAbleToGraveAsCost() and (not ft or ft>0 or c:GetSequence()<5) 
 end
 function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
