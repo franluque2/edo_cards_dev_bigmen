b57eccbd1db5cc84d7f483f557b0512ef67d28ec
CaptPurpleHeart <ben.simga@gmail.com>
2024-03-08 14:58:06 +0000

    fix

--- script/c81632553.lua
+++ script/c81632553.lua
@@ -105,7 +105,7 @@ function s.AncientSpells(c)
 	return c:IsFaceup() and c:IsCode(511000124, 511000125, 511000123)
 end
 
-function s.AncientMonster(c)
+function s.AncientMonster(e,c)
 	return c:IsFaceup() and c:IsCode(511000126, 511000127, 38520918, 511000128, 76232340, 47986555, 32012841)
 end
 
