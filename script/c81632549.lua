c6062c39e3206c0caf2f3f36f540ce51aa915f87
CaptPurpleHeart <ben.simga@gmail.com>
2024-03-08 08:03:17 +0000

    Fix

--- script/c81632549.lua
+++ script/c81632549.lua
@@ -13,7 +13,7 @@ function s.initial_effect(c)
     --Special Summon
 	local e2=Effect.CreateEffect(c)
 	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e2:SetType(EFFECT_TYPE_ACTIVATE)
+	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetCode(EVENT_FREE_CHAIN)
     e2:SetCondition(s.condition)
 	e2:SetOperation(s.spop)
