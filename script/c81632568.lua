2aa05b853ea2981fa48c632691eceeece6ff0fb2
CaptPurpleHeart <ben.simga@gmail.com>
2024-03-11 22:43:11 +0000

    SoulCharge

--- script/c81632568.lua
+++ script/c81632568.lua
@@ -10,6 +10,7 @@ function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e1:SetDescription(id,0)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetCode(EVENT_FREE_CHAIN)
     e1:SetRange(LOCATION_SZONE)
@@ -21,6 +22,7 @@ function s.initial_effect(c)
     --Give control
 	local e2=Effect.CreateEffect(c)
 	e2:SetCategory(CATEGORY_CONTROL+CATEGORY_SPECIAL_SUMMON)
+    e2:SetDescription(id,1)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
     e2:SetRange(LOCATION_SZONE)
@@ -29,6 +31,18 @@ function s.initial_effect(c)
 	e2:SetOperation(s.gtop)
     e2:SetCountLimit(1,{id,1})
 	c:RegisterEffect(e2)
+
+    --soul charge baby
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_IGNITION)
+    e4:SetDescription(id,2)
+	e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e4:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e4:SetCode(EVENT_FREE_CHAIN)
+    e4:SetCountLimit(1,{id,2})
+	e4:SetTarget(s.target)
+	e4:SetOperation(s.operation)
+	c:RegisterEffect(e4)
 end
 function s.filter(c,e,tp)
 	return c:IsLevelBelow(4) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
@@ -87,11 +101,36 @@ function s.gtop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	if tc and tc:IsRelateToEffect(e) and tc:IsFaceup() then
 		if Duel.GetControl(tc,1-tp,PHASE_END,1)>0 and Duel.IsExistingMatchingCard(s.The300FilerSummon, tp, LOCATION_HAND, 0, 1, nil, e, tp)
-        and Duel.GetLocationCount(tp, LOCATION_MZONE)>0 and Duel.SelectYesNo(tp, aux.Stringid(id, 1)) then
+        and Duel.GetLocationCount(tp, LOCATION_MZONE)>0 and Duel.SelectYesNo(tp, aux.Stringid(id, 3)) then
             Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local g=Duel.SelectMatchingCard(tp,s.The300FilerSummon,tp,LOCATION_HAND,0,1,1,nil,e,tp)
 	if #g==0 then return end
 	Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 	end
 end
+end
+function s.rfilter(c,lv)
+	return c:GetLevel()==lv
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return false end
+	local g=Duel.GetMatchingGroup(s.The300Filter,tp,LOCATION_GRAVE,0,nil,e,tp)
+	if chk==0 then return not Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT)
+		and Duel.GetLocationCount(tp,LOCATION_MZONE)>1
+		and g:GetClassCount(Card.GetLevel)>=4 end
+	local sg=aux.SelectUnselectGroup(g,e,tp,4,4,aux.dpcheck(Card.GetLevel),1,tp,HINTMSG_SPSUMMON)
+	Duel.SetTargetCard(sg)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,sg,4,0,0)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	if ft<=0 then return end
+	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
+	local sg=g:Filter(Card.IsRelateToEffect,nil,e)
+	if #sg>1 and Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then return end
+	if #sg>ft then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		sg=sg:Select(tp,ft,ft,nil)
+	end
+	Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
 end
