ad7914d58b599b1febd34d078b5c69d0b2c7d39c
CaptPurpleHeart <ben.simga@gmail.com>
2024-03-11 15:49:03 +0000

    fix

--- script/c81632562.lua
+++ script/c81632562.lua
@@ -6,11 +6,11 @@ function s.initial_effect(c)
     --Activation Search
     local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
-	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetDescription(aux.Stringid(id,3))
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetOperation(s.activate)
-    e1:SetCountLimit(1,{id,3})
+    e1:SetCountLimit(1,{id,1})
 	c:RegisterEffect(e1)
 
     --Activate 1 "Kabuki Stage" Field Spell from hand or deck
