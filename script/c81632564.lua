222d82e49c17c3df6a5aaab16a2c2cfa6ac36c91
CaptPurpleHeart <ben.simga@gmail.com>
2024-03-11 14:47:31 +0000

    Drum

--- script/c81632564.lua
+++ script/c81632564.lua
@@ -3,6 +3,7 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	aux.AddEquipProcedure(c)
+    c:SetUniqueOnField(1,0,511002146)
 	--sp summon
 	local e2=Effect.CreateEffect(c)
 	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
@@ -12,6 +13,7 @@ function s.initial_effect(c)
 	e2:SetTarget(s.sptg)
 	e2:SetOperation(s.spop)
 	e2:SetRange(LOCATION_SZONE)
+    e2:SetCountLimit(1,{id,1})
 	c:RegisterEffect(e2)
 
     --Prevent effect target
@@ -30,6 +32,18 @@ function s.initial_effect(c)
 	e4:SetCode(EFFECT_CANNOT_SELECT_BATTLE_TARGET)
 	e4:SetValue(s.atlimit)
 	c:RegisterEffect(e4)
+
+    --damage
+	local e5=Effect.CreateEffect(c)
+	e5:SetDescription(aux.Stringid(id,0))
+	e5:SetCategory(CATEGORY_DAMAGE)
+	e5:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e5:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e5:SetCode(EVENT_TO_GRAVE)
+	e5:SetCondition(s.damcon)
+	e5:SetTarget(s.damtg)
+	e5:SetOperation(s.damop)
+	c:RegisterEffect(e5)
 end
 function s.atlimit(e,c)
 	return c~=e:GetHandler():GetEquipTarget()
@@ -67,4 +81,17 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 end
 function s.eqlimit(e,c)
     return c==e:GetLabelObject()
+end
+function s.damcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsReason(REASON_DESTROY)
+end
+function s.damtg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return true end
+    Duel.SetTargetPlayer(tp)
+    Duel.SetTargetParam(500)
+    Duel.SetOperationInfo(0,CATEGORY_RECOVER,nil,0,tp,500)
+end
+function s.damop(e,tp,eg,ep,ev,re,r,rp)
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+    Duel.Recover(p,d,REASON_EFFECT)
 end
