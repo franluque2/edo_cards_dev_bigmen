7b81d0f68d368a6ed4e9d119700ba5930674e817
CaptPurpleHeart <ben.simga@gmail.com>
2024-03-11 14:09:12 +0000

    Drum

---
+++ script/c81632564.lua
@@ -0,0 +1,67 @@
+--Kabuki Stage Prop - New Year Drum (CT)
+--New Year Drum
+local s,id=GetID()
+function s.initial_effect(c)
+	aux.AddEquipProcedure(c)
+	--sp summon
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e2:SetCode(EVENT_BE_BATTLE_TARGET)
+	e2:SetCondition(s.spcon)
+	e2:SetTarget(s.sptg)
+	e2:SetOperation(s.spop)
+	e2:SetRange(LOCATION_SZONE)
+	c:RegisterEffect(e2)
+
+    --Prevent effect target
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_EQUIP)
+	e3:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+	e3:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+	e3:SetValue(aux.tgoval)
+	c:RegisterEffect(e3)
+
+    --target
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_FIELD)
+	e4:SetRange(LOCATION_SZONE)
+	e4:SetTargetRange(0,LOCATION_MZONE)
+	e4:SetCode(EFFECT_CANNOT_SELECT_BATTLE_TARGET)
+	e4:SetValue(s.atlimit)
+	c:RegisterEffect(e4)
+end
+function s.atlimit(e,c)
+	return c~=e:GetHandler():GetEquipTarget()
+end
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+	local d=Duel.GetAttackTarget()
+	return d and d==e:GetHandler():GetEquipTarget()
+end
+function s.filter(c,e,tp)
+	return c:IsCode(511002145, 511002143, 84430950, 511002144) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
+	if #g>0 and Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)>0 then
+		Duel.ChangeAttackTarget(g:GetFirst())
+            Duel.Equip(tp,c,tc,true)
+            local e1=Effect.CreateEffect(c)
+            e1:SetType(EFFECT_TYPE_SINGLE)
+            e1:SetCode(EFFECT_EQUIP_LIMIT)
+            e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+            e1:SetValue(s.eqlimit)
+            e1:SetLabelObject(tc)
+            c:RegisterEffect(e1)
+    end
+end
+function s.eqlimit(e,c)
+    return c==e:GetLabelObject()
+end
