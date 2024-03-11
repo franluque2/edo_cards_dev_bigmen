e341fc16b594ebca9005ca05a649ae0ac75ac8b4
CaptPurpleHeart <ben.simga@gmail.com>
2024-03-11 22:29:09 +0000

    fix

--- script/c81632568.lua
+++ script/c81632568.lua
@@ -1,6 +1,12 @@
 --Illegal Summon (CT)
 local s,id=GetID()
 function s.initial_effect(c)
+
+    --Activate
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_ACTIVATE)
+	e3:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e3)
 	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
