4ffd5a1484530a16b0171821384dce95cf8e4c14
CaptPurpleHeart <ben.simga@gmail.com>
2024-03-11 11:56:37 +0000

    effect

--- script/c81632561.lua
+++ script/c81632561.lua
@@ -38,7 +38,6 @@ function s.initial_effect(c)
 	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
 	e4:SetCode(EVENT_SUMMON_SUCCESS)
 	e4:SetRange(LOCATION_SZONE)
-    e4:SetCondition(s.condition)
 	e4:SetTarget(s.target)
 	e4:SetOperation(s.operation)
 	c:RegisterEffect(e4)
@@ -72,8 +71,23 @@ function s.tfop(e,tp,eg,ep,ev,re,r,rp)
 								Duel.ActivateFieldSpell(tc,e,tp,eg,ep,ev,re,r,rp)
 							end,
 							aux.Stringid(id,2))
+                            if tc:IsLocation(LOCATION_FZONE) then
+                                local e1=Effect.CreateEffect(e:GetHandler())
+                                e1:SetType(EFFECT_TYPE_FIELD)
+                                e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+                                e1:SetCode(EFFECT_CANNOT_ACTIVATE)
+                                e1:SetTargetRange(1,0)
+                                e1:SetValue(s.aclimit)
+                                e1:SetLabelObject(tc)
+                                e1:SetReset(RESET_PHASE+PHASE_END)
+                                Duel.RegisterEffect(e1,tp)
+                            end
+                    
 end
-
+function s.aclimit(e,re,tp)
+	local tc=e:GetLabelObject()
+	return re:GetHandler():IsCode(tc:GetCode())
+end
 function s.cfilter(c)
 	local np=c:GetPosition()
 	local pp=c:GetPreviousPosition()
@@ -94,9 +108,6 @@ function s.acop(e,tp,eg,ep,ev,re,r,rp)
     Duel.Recover(p,d,REASON_EFFECT)
 end
 
-function s.filter2(c)
-	return c:IsType(TYPE_MONSTER)
-end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return eg:IsExists(Card.IsPosition,1,nil,POS_FACEUP_ATTACK) end
 	Duel.SetTargetCard(eg)
@@ -105,8 +116,4 @@ end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	local g=eg:Filter(Card.IsPosition,nil,POS_FACEUP_ATTACK)
 	Duel.ChangePosition(g,POS_FACEUP_DEFENSE)
-end
-
-function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	return eg:IsExists(s.filter2,1,nil,tp) and #eg==1
 end
