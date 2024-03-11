b78795e1e23d4a9f0bbac518b60d0809dc2cf404
CaptPurpleHeart <ben.simga@gmail.com>
2024-03-11 12:13:36 +0000

    fix

--- script/c81632562.lua
+++ script/c81632562.lua
@@ -46,7 +46,7 @@ function s.nttg(e,c)
 end
 
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetMatchingGroup(s.thfilter,tp,LOCATION_DECK,0,nil)
+	local g=Duel.GetMatchingGroup(s.thfilter,tp,LOCATION_DECK|LOCATION_GRAVE,0,nil)
 	if #g>0 and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
 		local sg=g:Select(tp,1,1,nil)
