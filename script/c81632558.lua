3b2707877d5100292b10066afc273343589bda15
CaptPurpleHeart <ben.simga@gmail.com>
2024-03-11 09:21:52 +0000

    Big Bridge

--- script/c81632558.lua
+++ script/c81632558.lua
@@ -1,6 +1,7 @@
 --Shizuka the Heavenly Dancer (CT)
 local s,id=GetID()
 function s.initial_effect(c)
+    c:SetUniqueOnField(1,0,511002144)
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
