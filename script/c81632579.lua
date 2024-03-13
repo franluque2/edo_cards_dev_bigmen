073f6da4c90b3718479df956d31a1341beb1d942
CaptPurpleHeart <ben.simga@gmail.com>
2024-03-13 18:10:50 +0000

    fix?

--- script/c81632579.lua
+++ script/c81632579.lua
@@ -37,6 +37,14 @@ function s.initial_effect(c)
 	e5:SetTargetRange(0,1)
     e5:SetCondition(s.battlecon)
 	c:RegisterEffect(e5)
+	--destroy
+	local e6=Effect.CreateEffect(c)
+	e6:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
+	e6:SetRange(LOCATION_MZONE)
+	e6:SetCode(EVENT_LEAVE_FIELD)
+	e6:SetCondition(s.sdescon)
+	e6:SetOperation(s.sdesop)
+	c:RegisterEffect(e6)
 end
 local CARD_PANDEMONIUM=94585852
 function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
@@ -54,31 +62,60 @@ function s.battlecon(e)
 	local tp=e:GetHandlerPlayer()
 	return Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsCode,CARD_PANDEMONIUM),tp,LOCATION_ONFIELD,0,1,nil)
 end
-
-function s.operation(e,tp,eg,ep,ev,re,r,rp)
-	local e1=Effect.CreateEffect(e:GetHandler())
-	e1:SetType(EFFECT_TYPE_TRIGGER_F)
-	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_DELAY)
-	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e1:SetRange(LOCATION_MZONE)
-	e1:SetCountLimit(1)
-	e1:SetCondition(s.sdescon)
-	e1:SetOperation(s.sdesop)
-	if Duel.GetTurnPlayer()~=tp then
-		e1:SetLabel(1)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_OPPO_TURN,3)
-	else
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_OPPO_TURN,2)
-	end
-	e:GetHandler():RegisterEffect(e1)
+function s.sfilter(c)
+	return c:IsReason(REASON_DESTROY) and c:IsPreviousPosition(POS_FACEUP) and c:GetPreviousCodeOnField()==94585852 and c:IsPreviousLocation(LOCATION_ONFIELD)
+end
+function s.desfilter(c,tp)
+	return c:IsCode(94585852) and c:IsPreviousPosition(POS_FACEUP)
 end
 function s.sdescon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetTurnPlayer()~=tp
+	return eg:IsExists(s.desfilter,1,nil,tp)
 end
 function s.sdesop(e,tp,eg,ep,ev,re,r,rp)
-	if e:GetLabel()==0 then
-		Duel.Destroy(e:GetHandler(),REASON_EFFECT)
-	else
-		e:SetLabel(0)
+	Duel.Destroy(e:GetHandler(),REASON_EFFECT)
+end
+
+function s.op(e,tp,eg,ep,ev,re,r,rp)
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_PHASE+PHASE_END)
+	e1:SetCountLimit(1)
+	e1:SetLabel(0)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetReset(RESET_PHASE+PHASE_END+RESET_OPPO_TURN,2)
+	e1:SetOperation(s.reg)
+	Duel.RegisterEffect(e1,tp)
+end
+function s.reg(e,tp,eg,ep,ev,re,r,rp)
+	local ct=e:GetLabel()
+	if Duel.GetTurnPlayer()~=tp then
+		ct=ct+1
+		e:SetLabel(ct)
+		if ct>=2 then
+			local e1=Effect.CreateEffect(e:GetHandler())
+			e1:SetCategory(CATEGORY_DESTROY)
+			e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+			e1:SetCode(EVENT_PHASE+PHASE_END)
+			e1:SetCountLimit(1)
+			e1:SetRange(LOCATION_MZONE)
+			e1:SetReset(RESET_PHASE+PHASE_END)
+			e1:SetTarget(s.destg2)
+			e1:SetOperation(s.desop2)
+			e:GetHandler():RegisterEffect(e1)
+		end
 	end
+end
+function s.destg2(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	local sg=Duel.GetMatchingGroup(Card.IsDestructable,tp,LOCATION_MZONE,0,e:GetHandler())
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,e:GetHandler(),1,0,0)
+end
+function s.filter(c,tp)
+	return not c:IsLocation(LOCATION_MZONE) and c:GetOwner()==tp
+end
+function s.desop2(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.Destroy(e:GetHandler(),REASON_EFFECT)~=0 then
+		local sg=Duel.GetMatchingGroup(Card.IsDestructable,tp,LOCATION_MZONE,0,nil)
+		Duel.Destroy(sg,REASON_EFFECT)
+		end
 end
