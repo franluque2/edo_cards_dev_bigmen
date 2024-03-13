b2c35b2e4280d6ce372746af5a782851abc5cb56
CaptPurpleHeart <ben.simga@gmail.com>
2024-03-13 11:41:55 +0000

    wand of aces

---
+++ script/c81632578.lua
@@ -0,0 +1,93 @@
+--Ace of Wand (CT)
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_COIN)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_LEAVE_FIELD)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+
+    	--spsummon
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetCost(aux.bfgcost)
+	e2:SetTarget(s.sptg)
+	e2:SetOperation(s.spop)
+	c:RegisterEffect(e2)
+end
+s.toss_coin=true
+function s.cfilter(c,eg)
+	local tp=c:GetControler()
+	return c:IsReason(REASON_DESTROY) and c:IsReason(REASON_EFFECT) and c:IsPreviousLocation(LOCATION_MZONE)
+		and not eg:IsExists(s.cfilter2,1,nil,tp)
+end
+function s.cfilter2(c,tp)
+	return c:IsReason(REASON_DESTROY) and c:IsReason(REASON_EFFECT) and c:IsPreviousLocation(LOCATION_MZONE)
+		and c:IsControler(1-tp)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.cfilter,1,nil,eg)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_COIN,nil,0,tp,1)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local g=eg:Filter(s.cfilter,nil,eg)
+	local p=g:GetFirst():GetControler()
+	local sum=g:GetSum(Card.GetAttack)/2
+	local res=Duel.TossCoin(tp,1)
+	if res==COIN_HEADS then
+		Duel.Recover(p,sum,REASON_EFFECT)
+	elseif res==COIN_TAILS then
+		Duel.Damage(p,sum,REASON_EFFECT)
+	end
+end
+
+function s.spfilter(c,e,tp)
+	return c:IsCode(02196767, 50593156) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_GRAVE) and s.spfilter(chkc,e,tp) end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingTarget(s.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectTarget(tp,s.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		if Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP) then
+            --Cannot be destroyed by battle or card effect
+            local e1=Effect.CreateEffect(e:GetHandler())
+            e1:SetDescription(3008)
+            e1:SetProperty(EFFECT_FLAG_CLIENT_HINT)
+            e1:SetType(EFFECT_TYPE_SINGLE)
+            e1:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+            e1:SetValue(1)
+            e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,2)
+            g:GetFirst():RegisterEffect(e1)
+            local e2=Effect.CreateEffect(e:GetHandler())
+            e2:SetType(EFFECT_TYPE_SINGLE)
+            e2:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+            e2:SetValue(1)
+            e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,2)
+            g:GetFirst():RegisterEffect(e2)
+            g:GetFirst():RegisterFlagEffect(0,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,EFFECT_FLAG_CLIENT_HINT,2,0,aux.Stringid(id,0))
+        end
+        if tc:IsRelateToEffect(e) and tc:IsFaceup() then
+            Duel.SendtoHand(tc,tp,REASON_EFFECT)
+        end
+    end
+end
