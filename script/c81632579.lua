520a25ffb0d2dd38e68bfed9801b31913630458b
CaptPurpleHeart <ben.simga@gmail.com>
2024-03-13 17:45:02 +0000

    Archfiend Matador

---
+++ script/c81632579.lua
@@ -0,0 +1,79 @@
+--Archfiend Matador (CT)
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	--Cannot attack
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_CANNOT_ATTACK)
+	c:RegisterEffect(e1)
+	--Cannot be destroyed by battle
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+	e2:SetValue(1)
+	c:RegisterEffect(e2)
+	--You take no battle damage
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetCode(EFFECT_AVOID_BATTLE_DAMAGE)
+	e3:SetValue(1)
+	c:RegisterEffect(e3)
+	--Destroy a monster that battles this card
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,0))
+	e4:SetCategory(CATEGORY_DESTROY)
+	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e4:SetCode(EVENT_DAMAGE_STEP_END)
+	e4:SetTarget(s.destg)
+	e4:SetOperation(s.desop)
+	c:RegisterEffect(e4)
+    --Dark Arena Effect
+    local e5=Effect.CreateEffect(e:GetHandler())
+    e5:SetType(EFFECT_TYPE_FIELD)
+    e5:SetCode(EFFECT_PATRICIAN_OF_DARKNESS)
+    e5:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+    e5:SetTargetRange(0,1)
+    e5:SetCondition(s.battlecon)
+    Duel.RegisterEffect(e5,tp)
+    --Send Itself
+    local e6=Effect.CreateEffect(c)
+	e6:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e6:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e6:SetCode(EVENT_SUMMON_SUCCESS+EVENT_PHASE+PHASE_END)
+	e6:SetRange(LOCATION_MZONE)
+	e6:SetCondition(s.sdescon)
+	e6:SetOperation(s.sdesop)
+	c:RegisterEffect(e6)
+    local e7=e6:Clone()
+	e7:SetCode(EVENT_SPSUMMON_SUCCESS+EVENT_PHASE+PHASE_END)
+	c:RegisterEffect(e7)
+end
+local CARD_PANDEMONIUM=94585852
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local bc=e:GetHandler():GetBattleTarget()
+	if chk==0 then return bc and bc:IsRelateToBattle() end
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,bc,1,0,0)
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	local bc=e:GetHandler():GetBattleTarget()
+	if bc:IsRelateToBattle() then
+		Duel.Destroy(bc,REASON_EFFECT)
+	end
+end
+function s.battlecon(e)
+	local tp=e:GetHandlerPlayer()
+	return Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsCode,CARD_PANDEMONIUM),tp,LOCATION_ONFIELD,0,1,nil)
+end
+function s.sdescon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTurnPlayer()==tp
+end
+function s.sdesop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local ct=c:GetTurnCounter()
+	ct=ct+1
+	c:SetTurnCounter(ct)
+	if ct==2 then
+		Duel.Destroy(c,REASON_RULE)
+	end
+end
---
+++ script/c81632580.lua
@@ -0,0 +1,30 @@
+--Ritual of the Matador (CT)
+local s,id=GetID()
+function s.initial_effect(c)
+	Ritual.AddProcGreaterCode(c,6,nil,511000009)
+
+    local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e2:SetCode(EFFECT_DESTROY_REPLACE)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetTarget(s.reptg)
+	e2:SetValue(s.repval)
+	e2:SetOperation(s.repop)
+    e2:SetCountLimit(1,{id,1})
+	c:RegisterEffect(e2)
+end
+function s.repfilter(c,tp)
+	return c:IsFaceup() and c:IsCode(94585852) and c:IsLocation(LOCATION_FZONE)
+		and c:IsControler(tp) and not c:IsReason(REASON_REPLACE)
+end
+function s.reptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsAbleToRemove() and eg:IsExists(s.repfilter,1,nil,tp) end
+	return Duel.SelectEffectYesNo(tp,e:GetHandler(),96)
+end
+function s.repval(e,c)
+	return s.repfilter(c,e:GetHandlerPlayer())
+end
+function s.repop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Remove(e:GetHandler(),POS_FACEUP,REASON_EFFECT)
+end
+
