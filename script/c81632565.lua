617a4397606b19a5441416aae0f2e35d1468e899
CaptPurpleHeart <ben.simga@gmail.com>
2024-03-14 23:27:38 +0000

    fixes

--- script/c81632565.lua
+++ script/c81632565.lua
@@ -61,7 +61,7 @@ end
 function s.atkval(e)
 	local tp=e:GetHandlerPlayer()
 	local g=Duel.GetMatchingGroupCount(s.atkfilter,tp,LOCATION_GRAVE|LOCATION_FZONE,0,nil)
-	return 100*g
+	return 300*g
 end
 
 function s.atkfilter(c)
