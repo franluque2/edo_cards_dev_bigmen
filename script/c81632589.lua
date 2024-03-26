66058059a239dc45ed7455a83afe98ce632c8933
CaptPurpleHeart <ben.simga@gmail.com>
2024-03-26 19:01:10 +0000

    draw

--- script/c81632589.lua
+++ script/c81632589.lua
@@ -64,11 +64,14 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	else
 		local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
 		Duel.Damage(p,d,REASON_EFFECT)
-        if Duel.IsExistingMatchingCard(s.relavfieldfilter, tp, LOCATION_MZONE, 0, 1, nil) then
-            Duel.Draw(tp,1,REASON_EFFECT)
-        end
+		if Duel.IsPlayerCanDraw(tp,1) and Duel.IsExistingMatchingCard(s.DLfilter, tp, LOCATION_MZONE, 0, 1, nil)
+		and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
+			Duel.BreakEffect()
+			Duel.ShuffleDeck(tp)
+			Duel.Draw(tp,1,REASON_EFFECT)
+		end
 	end
 end
-function s.relavfieldfilter(c)
+function s.DLfilter(c)
 	return c:IsCode(810000093) and c:IsFaceup()
 end
