ae963733e2d97a2255e461c6da930c2e19791139
CaptPurpleHeart <ben.simga@gmail.com>
2024-03-28 02:05:29 +0000

    fix

--- script/c81632595.lua
+++ script/c81632595.lua
@@ -101,8 +101,8 @@ function s.desop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 
-function s.atktg(c)
-	return c:IsFaceup() and c:IsCode(511001977, 511001980, 511001979)
+function s.atktg(e,c)
+	return c:GetCode()~=id and c:IsFaceup() and c:IsRace(RACE_FAIRY)
 end
 
 function s.repfilter(c,tp)
