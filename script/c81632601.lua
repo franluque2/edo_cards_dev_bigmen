56d56b56645730cf7acf1ccaf2f2d543662e579a
CaptPurpleHeart <ben.simga@gmail.com>
2024-03-29 10:11:32 +0000

    new effect

--- script/c81632601.lua
+++ script/c81632601.lua
@@ -9,6 +9,18 @@ c:SetUniqueOnField(1,0,511001266)
 	e1:SetHintTiming(0,TIMING_END_PHASE)
 	c:RegisterEffect(e1)
 
+	--Inflict Tuh Damáge
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_DAMAGE)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetCode(EVENT_BATTLE_DESTROYING)
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e2:SetCondition(s.reccon)
+	e2:SetTarget(s.rectg)
+	e2:SetOperation(s.recop)
+	c:RegisterEffect(e2)
+
 	--cannot be target
 	local e3=Effect.CreateEffect(c)
 	e3:SetType(EFFECT_TYPE_FIELD)
@@ -48,64 +60,26 @@ c:SetUniqueOnField(1,0,511001266)
 	c:RegisterEffect(e5)
 
 
-	--
-	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,0))
-	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
-	e2:SetCategory(CATEGORY_DAMAGE)
-	e2:SetRange(LOCATION_SZONE)
-	e2:SetCode(EVENT_PHASE+PHASE_END)
-	e2:SetCountLimit(1)
-	e2:SetTarget(s.damtg)
-	e2:SetOperation(s.damop)
-	c:RegisterEffect(e2)
-	aux.GlobalCheck(s,function()
-		s[0]=0
-		s[1]=0
-		local ge1=Effect.CreateEffect(c)
-		ge1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-		ge1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
-		ge1:SetCode(EVENT_DESTROY)
-		ge1:SetOperation(s.op)
-		Duel.RegisterEffect(ge1,0)
-		local ge2=Effect.CreateEffect(c)
-		ge2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-		ge2:SetCode(EVENT_ADJUST)
-		ge2:SetCountLimit(1)
-		ge2:SetOperation(s.clear)
-		Duel.RegisterEffect(ge2,0)
-	end)
-end
-function s.chkfilter(c,tp)
-	return c:IsMonster() and c:IsPreviousLocation(LOCATION_MZONE) and c:IsLocation(LOCATION_GRAVE) 
-		and c:IsPreviousControler(tp)
-end
-function s.op(e,tp,eg,ep,ev,re,r,rp)
-	local ct1=eg:FilterCount(s.chkfilter,nil,tp)
-	local ct2=eg:FilterCount(s.chkfilter,nil,1-tp)
-	if ct1>0 then
-		s[0]=s[0]+ct1
-	end
-	if ct2>0 then
-		s[1]=s[1]+ct2
-	end
-end
-function s.clear(e,tp,eg,ep,ev,re,r,rp)
-	s[0]=0
-	s[1]=0
-end
-function s.damtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return true end
-	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,tp,s[0]*200)
-	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,tp,s[1]*200)
-end
-function s.damop(e,tp,eg,ep,ev,re,r,rp)
-	if not e:GetHandler():IsRelateToEffect(e) then return end
-	Duel.Damage(tp,s[0]*200,REASON_EFFECT,true)
-	Duel.Damage(1-tp,s[1]*200,REASON_EFFECT,true)
-	Duel.RDComplete()
 end
 
+function s.reccon(e,tp,eg,ep,ev,re,r,rp)
+	local rc=eg:GetFirst()
+	return rc:IsRelateToBattle() and rc:IsFaceup()
+end
+function s.rectg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	local tc=eg:GetFirst():GetBattleTarget()
+	if not tc:IsFaceup() then
+		tc=eg:GetNext():GetBattleTarget()
+	end
+	Duel.SetTargetPlayer(tc:GetPreviousControler())
+	Duel.SetTargetParam(300)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,tc:GetPreviousControler(),300)
+end
+function s.recop(e,tp,eg,ep,ev,re,r,rp)
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Damage(p,d,REASON_EFFECT)
+end
 
 function s.etarget(e,c)
 	return c:IsCode(511001977, 511001978, 511001980, 511001979, 30757396)
