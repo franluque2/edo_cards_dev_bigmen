229011a711e61b05819dcf1360bcac9232c2e7a6
CaptPurpleHeart <ben.simga@gmail.com>
2024-04-01 09:50:25 +0100

    fix

--- script/c81632615.lua
+++ script/c81632615.lua
@@ -74,8 +74,8 @@ function s.operation2(e,tp,eg,ep,ev,re,r,rp)
 	if tc:IsSetCard(0x516) then
 		Duel.DisableShuffleCheck()
 		Duel.SendtoHand(tc,nil,REASON_EFFECT|REASON_EXCAVATE)
-		Duel.SendtoDeck(c,nil,2,REASON_EFFECT)
+		Duel.SendtoDeck(c,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)
 	else
-		Duel.Remove(c,POS_FACEUP,REASON_EFFECT)
+		Duel.Remove(e:GetHandler(),POS_FACEUP,REASON_EFFECT)
 	end
 end
