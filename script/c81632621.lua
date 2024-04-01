c2c355f8ee980dcece611f0cce582c109acc90c3
CaptPurpleHeart <ben.simga@gmail.com>
2024-04-01 16:36:45 +0100

    Scandal Pest

---
+++ script/c81632621.lua
@@ -0,0 +1,133 @@
+--Scandal Snapper (CT)
+local s,id=GetID()
+function s.initial_effect(c)
+	--special summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_SPSUMMON_PROC)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCondition(s.spcon)
+    e1:SetCountLimit(1,{id,1})
+	c:RegisterEffect(e1)
+
+    local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_SUMMON_SUCCESS)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCountLimit(1,{id,2})
+	e2:SetTarget(s.target)
+	e2:SetOperation(s.operation)
+	c:RegisterEffect(e2,false)
+	local e3=e2:Clone()
+	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e3)
+
+    --Can use 1 DARK monster in your hand as material when using this for a "Borrel" monster
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_FIELD)
+	e4:SetRange(LOCATION_HAND)
+	e4:SetCode(EFFECT_EXTRA_MATERIAL)
+	e4:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e4:SetTargetRange(1,0)
+	e4:SetOperation(s.extracon2)
+	e4:SetValue(s.extraval2)
+	local e5=Effect.CreateEffect(c)
+	e5:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_GRANT)
+	e5:SetRange(LOCATION_MZONE)
+	e5:SetTargetRange(LOCATION_HAND,0)
+	e5:SetTarget(s.eftg)
+	e5:SetLabelObject(e4)
+	c:RegisterEffect(e5)
+	aux.GlobalCheck(s,function()
+		s.flagmap2={}
+	end)
+
+    --lvup
+	local e6=Effect.CreateEffect(c)
+	e6:SetDescription(aux.Stringid(19012345,0))
+	e6:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e6:SetCode(EVENT_BATTLE_DAMAGE)
+	e6:SetCondition(s.lvcon)
+	e6:SetOperation(s.lvop)
+	c:RegisterEffect(e6)
+
+    --Can attack directly
+	local e7=Effect.CreateEffect(c)
+	e7:SetType(EFFECT_TYPE_SINGLE)
+	e7:SetCode(EFFECT_DIRECT_ATTACK)
+	c:RegisterEffect(e7)
+
+end
+
+function s.spcon(e,c)
+	if c==nil then return true end
+	return Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.cfilter,c:GetControler(),LOCATION_MZONE,0,1,nil)
+end
+
+function s.filter(c)
+	return c:IsCode(100000262, 511005595, 511002684) and c:IsAbleToHand()
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
+
+function s.eftg(e,c)
+	return c:IsMonster() and c:IsCanBeLinkMaterial()
+end
+function s.extrafilter2(c,tp)
+	return c:IsLocation(LOCATION_MZONE) and c:IsControler(tp)
+end
+function s.extracon2(c,e,tp,sg,mg,lc,og,chk)
+	local ct=sg:FilterCount(Card.HasFlagEffect,nil,id+1)
+	return ct==0 or ((sg+mg):Filter(s.extrafilter2,nil,e:GetHandlerPlayer()):IsExists(Card.IsCode,1,og,id) and ct<2)
+end
+function s.extraval2(chk,summon_type,e,...)
+	local c=e:GetHandler()
+	if chk==0 then
+		local tp,sc=...
+		if summon_type~=SUMMON_TYPE_LINK or not sc:IsCode(64966519) or Duel.GetFlagEffect(tp,id+1)>0 then
+			return Group.CreateGroup()
+		else
+			s.flagmap2[c]=c:RegisterFlagEffect(id+1,0,0,1)
+			return Group.FromCards(c)
+		end
+	elseif chk==1 then
+		local sg,sc,tp=...
+		if summon_type&SUMMON_TYPE_LINK==SUMMON_TYPE_LINK and #sg>0 and Duel.GetFlagEffect(tp,id+1)==0 then
+			Duel.Hint(HINT_CARD,tp,id)
+			Duel.RegisterFlagEffect(tp,id+1,RESET_PHASE|PHASE_END,0,1)
+		end
+	elseif chk==2 then
+		if s.flagmap2[c] then
+			s.flagmap2[c]:Reset()
+			s.flagmap2[c]=nil
+		end
+	end
+end
+function s.lvcon(e,tp,eg,ep,ev,re,r,rp)
+	return ep~=tp and e:GetHandler():GetLevel()<12
+end
+function s.lvop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) or c:IsFacedown() then return end
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_UPDATE_LEVEL)
+	e1:SetValue(2)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
+	c:RegisterEffect(e1)
+	Duel.ChangePosition(c,POS_FACEUP_DEFENSE)
+end
