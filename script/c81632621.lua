47e97dab44b1dfbd3bce7a6dac7c5595385122a5
CaptPurpleHeart <ben.simga@gmail.com>
2024-04-01 16:43:21 +0100

    fix

--- script/c81632621.lua
+++ script/c81632621.lua
@@ -25,15 +25,15 @@ function s.initial_effect(c)
 	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
 	c:RegisterEffect(e3)
 
-    --Can use 1 DARK monster in your hand as material when using this for a "Borrel" monster
+	--Extra Link Material
 	local e4=Effect.CreateEffect(c)
 	e4:SetType(EFFECT_TYPE_FIELD)
 	e4:SetRange(LOCATION_HAND)
 	e4:SetCode(EFFECT_EXTRA_MATERIAL)
 	e4:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e4:SetTargetRange(1,0)
-	e4:SetOperation(s.extracon2)
-	e4:SetValue(s.extraval2)
+	e4:SetOperation(s.extracon)
+	e4:SetValue(s.extraval)
 	local e5=Effect.CreateEffect(c)
 	e5:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_GRANT)
 	e5:SetRange(LOCATION_MZONE)
@@ -41,9 +41,6 @@ function s.initial_effect(c)
 	e5:SetTarget(s.eftg)
 	e5:SetLabelObject(e4)
 	c:RegisterEffect(e5)
-	aux.GlobalCheck(s,function()
-		s.flagmap2={}
-	end)
 
     --lvup
 	local e6=Effect.CreateEffect(c)
@@ -85,35 +82,35 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 end
 
 function s.eftg(e,c)
-	return c:IsMonster() and c:IsCanBeLinkMaterial()
+	return c:IsType(TYPE_MONSTER) and c:IsCanBeLinkMaterial()
 end
-function s.extrafilter2(c,tp)
+function s.extrafilter(c,tp)
 	return c:IsLocation(LOCATION_MZONE) and c:IsControler(tp)
 end
-function s.extracon2(c,e,tp,sg,mg,lc,og,chk)
-	local ct=sg:FilterCount(Card.HasFlagEffect,nil,id+1)
-	return ct==0 or ((sg+mg):Filter(s.extrafilter2,nil,e:GetHandlerPlayer()):IsExists(Card.IsCode,1,og,id) and ct<2)
+function s.extracon(c,e,tp,sg,mg,lc,og,chk)
+	local ct=sg:FilterCount(Card.HasFlagEffect,nil,id)
+	return ct==0 or ((sg+mg):Filter(s.extrafilter,nil,e:GetHandlerPlayer()):IsExists(Card.IsCode,1,og,id) and ct<2)
 end
-function s.extraval2(chk,summon_type,e,...)
+function s.extraval(chk,summon_type,e,...)
 	local c=e:GetHandler()
 	if chk==0 then
 		local tp,sc=...
-		if summon_type~=SUMMON_TYPE_LINK or not sc:IsCode(64966519) or Duel.GetFlagEffect(tp,id+1)>0 then
+		if summon_type~=SUMMON_TYPE_LINK or not sc:IsCode(64966519) or Duel.GetFlagEffect(tp,id)>0 then
 			return Group.CreateGroup()
 		else
-			s.flagmap2[c]=c:RegisterFlagEffect(id+1,0,0,1)
+			s.flagmap[c]=c:RegisterFlagEffect(id,0,0,1)
 			return Group.FromCards(c)
 		end
 	elseif chk==1 then
 		local sg,sc,tp=...
-		if summon_type&SUMMON_TYPE_LINK==SUMMON_TYPE_LINK and #sg>0 and Duel.GetFlagEffect(tp,id+1)==0 then
+		if summon_type&SUMMON_TYPE_LINK==SUMMON_TYPE_LINK and #sg>0 and Duel.GetFlagEffect(tp,id)==0 then
 			Duel.Hint(HINT_CARD,tp,id)
-			Duel.RegisterFlagEffect(tp,id+1,RESET_PHASE|PHASE_END,0,1)
+			Duel.RegisterFlagEffect(tp,id,RESET_PHASE|PHASE_END,0,1)
 		end
 	elseif chk==2 then
-		if s.flagmap2[c] then
-			s.flagmap2[c]:Reset()
-			s.flagmap2[c]=nil
+		if s.flagmap[c] then
+			s.flagmap[c]:Reset()
+			s.flagmap[c]=nil
 		end
 	end
 end
