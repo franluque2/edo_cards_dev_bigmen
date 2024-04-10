9cab68a43de0a2e5bb721ae68b1ebae3fb5c1e65
Fran Luque <franluque2@gmail.com>
2024-04-10 03:54:34 -0300

    Fix

--- script/c81632643.lua
+++ script/c81632643.lua
@@ -71,8 +71,12 @@ end
 function s.cfilter(c)
 	return c:IsFaceup() and c:IsType(TYPE_XYZ)
 end
+
+function s.fuxyzfilter(c)
+	return c:IsFaceup() and c:IsType(TYPE_XYZ)
+end
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsType,TYPE_XYZ),tp,LOCATION_MZONE,0,1,nil)
+	return Duel.IsExistingMatchingCard(s.fuxyzfilter,e:GetHandlerPlayer(),LOCATION_MZONE,0,1,nil)
 end
 
 function s.damcon(e,tp,eg,ep,ev,re,r,rp)
