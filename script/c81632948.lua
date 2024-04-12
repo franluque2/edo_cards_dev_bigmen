f6a215054634247577c87d9390f7c041eea913f4
Fran Luque <franluque2@gmail.com>
2024-04-11 21:23:07 -0300

    Finish Grady

--- script/c81632948.lua
+++ script/c81632948.lua
@@ -33,8 +33,39 @@ function s.op(e, tp, eg, ep, ev, re, r, rp)
 		e2:SetCondition(s.adcon)
 		e2:SetOperation(s.adop)
 		Duel.RegisterEffect(e2, tp)
+
+		local e5=Effect.CreateEffect(e:GetHandler())
+        e5:SetType(EFFECT_TYPE_FIELD)
+        e5:SetCode(EFFECT_ADD_SETCODE)
+        e5:SetTargetRange(LOCATION_DECK,0)
+        e5:SetTarget(function(_,c)  return c:IsSetCard(0x600) end)
+        e5:SetValue(0x1017)
+        Duel.RegisterEffect(e5,tp)
+
+		local e6=Effect.CreateEffect(e:GetHandler())
+        e6:SetType(EFFECT_TYPE_FIELD)
+        e6:SetCode(EFFECT_ADD_RACE)
+        e6:SetTargetRange(LOCATION_GRAVE,0)
+        e6:SetTarget(function(_,c)  return c:IsSetCard(0x600) end)
+		e6:SetCondition(s.changcon)
+        e6:SetValue(RACE_MACHINE)
+        Duel.RegisterEffect(e6,tp)
+		
+
+		local e7=e6:Clone()
+		e7:SetCode(EFFECT_CHANGE_LEVEL)
+		e7:SetValue(8)
+		Duel.RegisterEffect(e7, tp)
 	end
 	e:SetLabel(1)
+end
+
+function s.changcon(e, tp, eg, ep, ev, re, r, rp)
+	return Duel.IsExistingMatchingCard(s.fudarkflattopfilter, e:GetHandlerPlayer(), LOCATION_ONFIELD, 0, 1, nil)
+end
+
+function s.fudarkflattopfilter(c)
+	return c:IsFaceup() and c:IsCode(67904682)
 end
 
 function s.flipcon(e, tp, eg, ep, ev, re, r, rp)
@@ -59,102 +90,111 @@ function s.generatedtuners(e, tp, eg, ep, ev, re, r, rp)
 	end
 end
 
-function s.catastrogueaddfilter(c)
-	return c:IsCode(100000143) and c:IsAbleToHand()
+
+function s.blizzardfilter(c)
+	return c:IsCode(100000157)
+		and c:IsAbleToHand()
 end
 
-function s.icemirrorshowfilter(c)
-	return c:IsCode(69492187) and not c:IsPublic()
+function s.spsumdtunerfilter(c,e,tp)
+	return c:IsMonster() and c:IsSetCard(setname) and c:IsCanBeSpecialSummoned(e, SUMMON_TYPE_SPECIAL, tp, false,false, POS_FACEUP)
 end
 
-function s.blizzardfilter(c, e, tp)
-	return c:IsType(TYPE_MONSTER)
-		and c:IsCode(100000157)
-		and c:IsCanBeSpecialSummoned(e, 0, tp, false, false)
-end
-
-function s.monsterfilter(c)
-	return c:IsFaceup() and c:HasLevel()
-end
-
-function s.catastroguetributefilter(c, e, tp)
-	return c:IsType(TYPE_MONSTER)
-		and (c:GetFlagEffect(id) > 0)
-		and c:IsCanBeSpecialSummoned(e, 0, tp, false, false)
-end
 
 function s.dtunerfilter(c)
-	return c:IsSetCard(0x600) and not c:IsPublic()
+	return c:IsSetCard(0x600) and not c:IsPublic() and c:IsAbleToDeck()
 end
 
+function s.addwaterfilter(c)
+	return c:IsMonster() and c:IsAttribute(ATTRIBUTE_WATER) and c:IsLevelBelow(3)
+end
+
+function s.darkmirrorshufflefilter(c)
+	return c:IsCode(69492187) and c:IsAbleToDeck()
+end
+
+function s.spcheck(sg,e,tp,mg)
+	return sg:GetClassCount(Card.GetCode)==1
+end
+
+function s.spfilter(c)
+	return c:IsAttribute(ATTRIBUTE_WATER) and c:IsAbleToDeck()
+end
 function s.flipcon2(e, tp, eg, ep, ev, re, r, rp)
+
+	local g=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_GRAVE,0,nil)
+
 	--OPT check
 	if Duel.GetFlagEffect(tp, id + 2) > 0 and Duel.GetFlagEffect(tp, id + 6) > 0 and Duel.GetFlagEffect(tp, id + 4) > 0
-		and Duel.GetFlagEffect(tp, id + 5) > 0 and Duel.GetFlagEffect(tp, id + 7) > 0 then
+		and Duel.GetFlagEffect(tp, id + 5) > 0 then
 		return
 	end
 	local b1 = Duel.GetFlagEffect(tp, id + 2) == 0
-		and Duel.IsExistingMatchingCard(s.monsterfilter, tp, LOCATION_MZONE, 0, 1, nil)
+	 and Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,nil)
+		and Duel.IsExistingMatchingCard(s.addwaterfilter, tp, LOCATION_DECK, 0, 1, nil)
 
 	local b2 = Duel.GetFlagEffect(tp, id + 6) == 0
 		and Duel.IsExistingMatchingCard(aux.TRUE, tp, LOCATION_MZONE, 0, 2, nil)
-		and Duel.IsExistingMatchingCard(s.catastrogueaddfilter, tp, LOCATION_DECK, 0, 1, nil)
+		and Duel.IsExistingMatchingCard(s.spsumdtunerfilter, tp, LOCATION_HAND, 0, 1, nil,e,tp)
 
 	local b3 = Duel.GetFlagEffect(tp, id + 4) == 0
-		and Duel.IsExistingMatchingCard(s.icemirrorshowfilter, tp, LOCATION_HAND, 0, 1, nil)
-		and Duel.IsExistingMatchingCard(s.blizzardfilter, tp, LOCATION_HAND + LOCATION_DECK + LOCATION_GRAVE, 0, 1, nil,
-			e, tp)
-	local b4 = Duel.GetFlagEffect(tp, id + 5) == 0
-		and Duel.IsExistingMatchingCard(s.catastroguetributefilter, tp, LOCATION_GRAVE + LOCATION_REMOVED, 0, 1, nil, e,
-			tp)
-
-	local b5 = Duel.GetFlagEffect(tp, id + 7) == 0
 		and Duel.IsExistingMatchingCard(s.dtunerfilter, tp, LOCATION_HAND, 0, 1, nil)
-		and (Duel.GetLocationCount(tp, LOCATION_MZONE) > 0)
 
-	return aux.CanActivateSkill(tp) and (b1 or b2 or b3 or b4 or b5)
+	local b4 = Duel.GetFlagEffect(tp, id + 5) == 0
+		and aux.SelectUnselectGroup(g,e,tp,2,2,s.spcheck,0)
+		and Duel.IsExistingMatchingCard(s.darkmirrorshufflefilter, tp, LOCATION_GRAVE, 0, 1, nil)
+
+	return aux.CanActivateSkill(tp) and (b1 or b2 or b3 or b4)
 end
 
 function s.flipop2(e, tp, eg, ep, ev, re, r, rp)
 	Duel.Hint(HINT_CARD, tp, id)
+	local g3=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_GRAVE,0,nil)
+
 	local b1 = Duel.GetFlagEffect(tp, id + 2) == 0
-		and Duel.IsExistingMatchingCard(s.monsterfilter, tp, LOCATION_MZONE, 0, 1, nil)
+	 and Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,nil)
+		and Duel.IsExistingMatchingCard(s.addwaterfilter, tp, LOCATION_DECK, 0, 1, nil)
 
 	local b2 = Duel.GetFlagEffect(tp, id + 6) == 0
 		and Duel.IsExistingMatchingCard(aux.TRUE, tp, LOCATION_MZONE, 0, 2, nil)
-		and Duel.IsExistingMatchingCard(s.catastrogueaddfilter, tp, LOCATION_DECK, 0, 1, nil)
+		and Duel.IsExistingMatchingCard(s.spsumdtunerfilter, tp, LOCATION_HAND, 0, 1, nil,e,tp)
 
 	local b3 = Duel.GetFlagEffect(tp, id + 4) == 0
-		and Duel.IsExistingMatchingCard(s.icemirrorshowfilter, tp, LOCATION_HAND, 0, 1, nil)
-		and Duel.IsExistingMatchingCard(s.blizzardfilter, tp, LOCATION_HAND + LOCATION_DECK + LOCATION_GRAVE, 0, 1, nil,
-			e, tp)
-
+		and Duel.IsExistingMatchingCard(s.dtunerfilter, tp, LOCATION_HAND, 0, 1, nil)
 
 	local b4 = Duel.GetFlagEffect(tp, id + 5) == 0
-		and Duel.IsExistingMatchingCard(s.catastroguetributefilter, tp, LOCATION_GRAVE + LOCATION_REMOVED, 0, 1, nil, e,
-			tp)
+		and aux.SelectUnselectGroup(g3,e,tp,2,2,s.spcheck,0)
+		and Duel.IsExistingMatchingCard(s.darkmirrorshufflefilter, tp, LOCATION_GRAVE, 0, 1, nil)
 
-
-	local b5 = Duel.GetFlagEffect(tp, id + 7) == 0
-		and Duel.IsExistingMatchingCard(s.dtunerfilter, tp, LOCATION_HAND, 0, 1, nil)
-		and (Duel.GetLocationCount(tp, LOCATION_MZONE) > 0)
 
 	local op = Duel.SelectEffect(tp, { b1, aux.Stringid(id, 0) },
-		{ b2, aux.Stringid(id, 2) },
-		{ b3, aux.Stringid(id, 3) },
-		{ b4, aux.Stringid(id, 4) },
-		{ b5, aux.Stringid(id, 5) })
+		{ b2, aux.Stringid(id, 1) },
+		{ b3, aux.Stringid(id, 2) },
+		{ b4, aux.Stringid(id, 3) })
 	op = op - 1
 
 	if op == 0 then
-		Duel.RegisterFlagEffect(tp, id + 2, RESET_EVENT + RESETS_STANDARD + RESET_PHASE + PHASE_END, 0, 0)
+		Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_COST+REASON_DISCARD,nil)
+		Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_ATOHAND)
+
+		local tc=Duel.SelectMatchingCard(tp, s.addwaterfilter, tp, LOCATION_DECK, 0, 1,1,false,nil)
+		if tc then
+			Duel.SendtoHand(tc, tp, REASON_RULE)
+			Duel.ConfirmCards(1-tp, tc)
+		end
+
+		Duel.RegisterFlagEffect(tp, id + 2, 0, 0, 0)
 	elseif op == 1 then
+		Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_SPSUMMON)
+
+		local tc=Duel.SelectMatchingCard(tp, s.spsumdtunerfilter, tp, LOCATION_HAND, 0, 1,1,false,nil,e,tp)
+		if tc then
+			Duel.SpecialSummon(tc, 0, tp, tp, false,false,POS_FACEUP)
+		end
+
 		Duel.RegisterFlagEffect(tp, id + 6, RESET_EVENT + RESETS_STANDARD + RESET_PHASE + PHASE_END, 0, 0)
 	elseif op == 2 then
-		Duel.RegisterFlagEffect(tp, id + 4, RESET_EVENT + RESETS_STANDARD + RESET_PHASE + PHASE_END, 0, 0)
-	elseif op == 3 then
-		Duel.RegisterFlagEffect(tp, id + 5, RESET_EVENT + RESETS_STANDARD + RESET_PHASE + PHASE_END, 0, 0)
-	elseif op == 4 then
+		
 		local g = Duel.SelectMatchingCard(tp, s.dtunerfilter, tp, LOCATION_HAND, 0, 1, 1, nil)
 		if #g > 0 then
 			Duel.SendtoDeck(g, tp, SEQ_DECKBOTTOM, REASON_EFFECT)
@@ -171,7 +211,26 @@ function s.flipop2(e, tp, eg, ep, ev, re, r, rp)
 			local newcard=Duel.CreateToken(tp, sg:GetFirst():GetCode())
 			Duel.SendtoHand(newcard, tp, REASON_RULE)
 		end
-		Duel.RegisterFlagEffect(tp, id + 7, RESET_EVENT + RESETS_STANDARD + RESET_PHASE + PHASE_END, 0, 0)
+		Duel.RegisterFlagEffect(tp, id + 4, RESET_EVENT + RESETS_STANDARD + RESET_PHASE + PHASE_END, 0, 0)
+	elseif op == 3 then
+		Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_TODECK)
+		local g=Duel.SelectMatchingCard(tp, s.darkmirrorshufflefilter, tp, LOCATION_GRAVE, 0, 1,1,false,nil)
+		local g2=aux.SelectUnselectGroup(g3,e,tp,2,2,s.spcheck,0)
+
+		Group.Merge(g, g2)
+
+		if Duel.SendtoDeck(g, tp,SEQ_DECKSHUFFLE, REASON_RULE) and Duel.IsExistingMatchingCard(s.blizzardfilter, tp, LOCATION_DECK|LOCATION_GRAVE, 0, 1, nil) and Duel.SelectYesNo(tp, aux.Stringid(id, 4)) then
+			Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_ATOHAND)
+
+			local tc=Duel.SelectMatchingCard(tp, s.blizzardfilter, tp, LOCATION_DECK|LOCATION_GRAVE, 0, 1,1,false,nil)
+			if tc then
+				Duel.SendtoHand(tc, tp, REASON_RULE)
+				Duel.ConfirmCards(1-tp, tc)
+			end
+	
+		end
+
+		Duel.RegisterFlagEffect(tp, id + 5, RESET_EVENT + RESETS_STANDARD + RESET_PHASE + PHASE_END, 0, 0)
 	end
 end
 
