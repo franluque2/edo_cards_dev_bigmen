f89562eef8160915dedc81b4f3ca2a60d015e45c
CaptPurpleHeart <ben.simga@gmail.com>
2024-04-15 06:41:32 +0100

    fix

--- script/c81632658.lua
+++ script/c81632658.lua
@@ -41,8 +41,8 @@ function s.tdtg(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function s.tdop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,3))
+    if Duel.SelectYesNo(tp,aux.Stringid(id,0)) then return end
 	local g=Duel.SelectMatchingCard(tp,s.tdfilter,tp,LOCATION_DECK,0,1,1,nil)
-    if #g==0 or not Duel.SelectYesNo(tp,aux.Stringid(id,0)) then return end
 	local tc=g:GetFirst()
 	if tc then
 		Duel.ShuffleDeck(tp)
