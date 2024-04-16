8aca999e376f2190bb993001b5779de9b13a414a
CaptPurpleHeart <ben.simga@gmail.com>
2024-04-16 23:02:40 +0100

    fix

--- script/c81632661.lua
+++ script/c81632661.lua
@@ -31,9 +31,7 @@ function s.condition(e,tp,eg,ep,ev,re,r,rp)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
     local tc=Duel.GetAttacker()
-    if tc:IsControler(tp) then tc=Duel.GetAttackTarget() end
-    Debug.Message(tc)
-    if tc:IsAbletoHand() then
+    if tc then
 		Duel.SendtoHand(tc,nil,REASON_EFFECT)
 	end
 end
