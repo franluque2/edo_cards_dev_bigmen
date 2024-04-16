80354f0a5f8f70daceadaa0be411d172215a95de
CaptPurpleHeart <ben.simga@gmail.com>
2024-04-16 22:37:13 +0100

    fix

--- script/c81632661.lua
+++ script/c81632661.lua
@@ -3,6 +3,7 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
+    e1:SetCategory(CATEGORY_TOHAND)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_BATTLE_START)
 	e1:SetCondition(s.condition)
