93a7ab594fdda277dfd69444954df73e879dc669
CaptPurpleHeart <ben.simga@gmail.com>
2024-04-23 17:14:20 +0100

    o

--- script/c81632680.lua
+++ script/c81632680.lua
@@ -6,9 +6,8 @@ function s.initial_effect(c)
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetCost(s.cost)
-	e1:SetTarget(s.target)
-	e1:SetOperation(s.activate)
+	e1:SetTarget(s.tdtg)
+	e1:SetOperation(s.tdop)
 	c:RegisterEffect(e1)
 end
 
