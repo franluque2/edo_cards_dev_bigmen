82be4127a8ad9519a7fdf78c9f3b7a3a7a533716
CaptPurpleHeart <ben.simga@gmail.com>
2024-04-23 10:05:24 +0100

    fix

--- script/c81632676.lua
+++ script/c81632676.lua
@@ -48,7 +48,7 @@ function s.desop(e,tp,eg,ep,ev,re,r,rp)
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_UPDATE_ATTACK)
-		e1:SetValue(#(Duel.GetOperatedGroup())*200)
+		e1:SetValue(ct*200)
 		e1:SetReset(RESET_EVENT|RESETS_STANDARD_DISABLE)
 		c:RegisterEffect(e1)
 	end
