6ca72097511356959bd08b494e140e06d9647ef4
CaptPurpleHeart <ben.simga@gmail.com>
2024-04-23 17:13:10 +0100

    fix

---
+++ script/c81632680.lua
@@ -0,0 +1,56 @@
+--Cat Food (CT)
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+
+
+function s.tdfilter(c,tp)
+	return (c:IsSetCard(0x1538) or c:IsSetCard(0x150e)) and (c:IsAbleToGrave())
+end
+
+function s.filter(c)
+	return c:IsSetCard(0x150e)
+end
+
+function s.filter2(c)
+	return c:IsSetCard(0x1538)
+end
+
+function s.tdtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE) and s.tdfilter(chkc,tp) end
+	if chk==0 then return Duel.IsExistingTarget(s.tdfilter,tp,LOCATION_MZONE,0,1,nil,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local tc=Duel.SelectTarget(tp,s.tdfilter,tp,LOCATION_MZONE,0,1,1,nil,tp):GetFirst()
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,tc,1,tp,LOCATION_MZONE)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,0x13)
+end
+function s.tdop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if not tc:IsRelateToEffect(e) or Duel.SendtoGrave(tc,REASON_EFFECT)==0 then return end
+	local c=e:GetHandler()
+	if tc:IsSetCard(0x1538) and tc:IsLocation(LOCATION_GRAVE) then
+		Duel.BreakEffect()
+        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+        local g=Duel.SelectMatchingCard(tp,s.filter,tp,0x13,0,1,1,nil,e,tp)
+        if #g>0 then
+            Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP_ATTACK)
+        end
+	elseif tc:IsSetCard(0x150e) and tc:IsLocation(LOCATION_GRAVE)
+		and c:IsFaceup() and c:IsRelateToEffect(e) then
+		Duel.BreakEffect()
+        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+        local g=Duel.SelectMatchingCard(tp,s.filter2,tp,0x13,0,1,1,nil,e,tp)
+        if #g>0 then
+            Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP_ATTACK|POS_FACEDOWN_DEFENSE)
+        end
+	end
+end
