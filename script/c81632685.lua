89dc04b944c0ad4ec677793ca6d8a226a4b08eff
CaptPurpleHeart <ben.simga@gmail.com>
2024-04-24 08:09:32 +0100

    new effect

--- script/c81632685.lua
+++ script/c81632685.lua
@@ -16,7 +16,7 @@ function s.initial_effect(c)
 	e2:SetRange(LOCATION_SZONE)
 	e2:SetTargetRange(LOCATION_MZONE,0)
 	e2:SetTarget(s.tg)
-	e2:SetValue(s.val)
+	e2:SetValue(1000)
 	c:RegisterEffect(e2)
 
 	--def up
