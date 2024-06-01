e5d26bf4d18459d6f627becfb2ec05bd34ed660f
Fran Luque <franluque2@gmail.com>
2024-06-01 10:15:39 -0300

    Add "Tower of Hanoi" and "Storm Access"

---
+++ script/c81633150.lua
@@ -0,0 +1,73 @@
+--Storm Access
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate Skill
+	local e1=Effect.CreateEffect(c)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_STARTUP)
+	e1:SetCountLimit(1)
+	e1:SetRange(0x5f)
+	e1:SetLabel(0)
+	e1:SetOperation(s.op)
+	c:RegisterEffect(e1)
+
+end
+
+
+
+function s.op(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetLabel()==0 then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e1:SetCode(EVENT_PREDRAW)
+		e1:SetCondition(s.flipcon)
+		e1:SetOperation(s.flipop)
+		Duel.RegisterEffect(e1,tp)
+
+
+        local c=e:GetHandler()
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCountLimit(1)
+	e1:SetCode(EVENT_PHASE+PHASE_STANDBY)
+	e1:SetCondition(s.adcon)
+	e1:SetOperation(s.adop)
+	Duel.RegisterEffect(e1,tp)
+
+
+	end
+	e:SetLabel(1)
+end
+
+
+function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
+	aux.RegisterClientHint(e:GetHandler(),nil,tp,1,0,aux.Stringid(id,0),nil)
+	return Duel.GetCurrentChain()==0 and Duel.GetTurnCount()==1
+end
+function s.flipop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_CARD,tp,id)
+	Duel.RegisterFlagEffect(tp,id,0,0,0)
+	Duel.SendtoDeck(e:GetHandler(), tp, -2, REASON_EFFECT)
+	if e:GetHandler():GetPreviousLocation()==LOCATION_HAND then
+		Duel.Draw(tp, 1, REASON_EFFECT)
+	end
+end
+
+
+function s.adcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTurnPlayer()==tp
+end
+
+local cyberselinks={5821478,22593417,66403530,67288539,72529749,5043010,58036229,86066372,1861629,6622715,15844566,33897356,46947713,61245672}
+
+function s.getcard()
+return cyberselinks[ Duel.GetRandomNumber(1, #cyberselinks ) ]
+end
+
+function s.adop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_CARD,tp,id)
+	local g=Duel.CreateToken(tp,s.getcard())
+	Duel.SendtoDeck(g,tp,SEQ_DECKTOP,REASON_EFFECT)
+end
+
