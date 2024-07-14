95cf24ed6d8bc89536f0392064f86db02f08d961
CaptPurpleHeart <ben.simga@gmail.com>
2024-07-14 17:29:03 +0100

    fix

--- script/c81632693.lua
+++ script/c81632693.lua
@@ -35,19 +35,6 @@
 	e3:SetValue(400)
 	c:RegisterEffect(e3)
     
-    local e4=Effect.CreateEffect(c)
-	e4:SetDescription(aux.Stringid(id,0))
-	e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e4:SetCode(EVENT_BATTLE_START)
-	e4:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e4:SetRange(LOCATION_GRAVE)
-	e4:SetCountLimit(1,id)
-	e4:SetCondition(s.spcon)
-	e4:SetCost(aux.bfgcost)
-	e4:SetTarget(s.sptg3)
-	e4:SetOperation(s.spop3)
-	c:RegisterEffect(e4)
     end
 
 function s.spcfilter(c)
@@ -89,35 +76,4 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 end
 function s.tg(e,c)
 	return c:IsRace(RACE_FAIRY+RACE_MACHINE+RACE_WARRIOR)
-end
-function s.spcon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetAttacker():IsControler(1-tp)
-end
-function s.filter(c,e,tp)
-	return c:IsCode(01412158) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
-		and (e:GetHandler():IsLocation(LOCATION_MZONE) and e:GetHandler():GetSequence()<5 or (Duel.GetLocationCount(tp,LOCATION_MZONE)>0))
-end
-function s.sptg3(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_GRAVE) and s.filter(chkc,e,tp) end
-	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectTarget(tp,s.filter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
-end
-function s.spop3(e,tp,eg,ep,ev,re,r,rp)
-	local tc=Duel.GetFirstTarget()
-	if tc and tc:IsRelateToEffect(e) and Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP) then
-		local e1=Effect.CreateEffect(e:GetHandler())
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_UPDATE_ATTACK)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-		e1:SetValue(1000)
-		tc:RegisterEffect(e1)
-		Duel.SpecialSummonComplete()
-		local a=Duel.GetAttacker()
-		if a:CanAttack() and not a:IsImmuneToEffect(e) then
-			Duel.ChangeAttackTarget(tc)
-			Duel.CalculateDamage(a,tc)
-		end
-	end
 end
