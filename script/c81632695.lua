f9c5ff8e6a1cdd90a71e75fa7b1b3595c1af3447
CaptPurpleHeart <ben.simga@gmail.com>
2024-07-14 01:17:26 +0100

    fix

--- script/c81632695.lua
+++ script/c81632695.lua
@@ -3,7 +3,7 @@ local s,id=GetID()
 function s.initial_effect(c)
 	c:EnableReviveLimit()
 	-- "Cyber Falcon" + "Cyber Falcon"
-	Fusion.AddProcMix(c,true,true,30655537,30655537)
+	Fusion.AddProcMix(c,true,true,30655537,aux.FilterBoolFunctionEx(Card.IsRace,RACE_MACHINE))
 	-- Gain LP
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_IGNITION)
