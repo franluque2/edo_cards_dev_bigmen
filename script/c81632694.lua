fd43f9b5f07615a619b4a1885a40fdf8f16297fc
CaptPurpleHeart <ben.simga@gmail.com>
2024-07-14 17:09:10 +0100

    rare

---
+++ script/c81632694.lua
@@ -0,0 +1,56 @@
+--Rare Metal Soul (CT)
+local s,id=GetID()
+Duel.LoadScript("c420.lua")
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_ATKCHANGE)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetHintTiming(TIMING_DAMAGE_STEP)
+    e1:SetCountLimit(1,{id,2})
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetCurrentPhase()~=PHASE_DAMAGE or not Duel.IsDamageCalculated() 
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.Robos,tp,LOCATION_MZONE,0,1,nil) end
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(s.Robos,tp,LOCATION_MZONE,0,nil)
+	local c=e:GetHandler()
+	local tc=g:GetFirst()
+	while tc do
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		if Duel.GetTurnPlayer()==tp then
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END+RESET_SELF_TURN,2)
+		else
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END+RESET_SELF_TURN)
+		end
+		e1:SetValue(1000)
+		tc:RegisterEffect(e1)
+		tc=g:GetNext()
+        local c=e:GetHandler()
+        --"Allure Queen" monsters you control cannot be destroyed by card effects for the rest of this turn
+        local e1=Effect.CreateEffect(c)
+        e1:SetType(EFFECT_TYPE_FIELD)
+        e1:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+        e1:SetTargetRange(LOCATION_MZONE,0)
+        e1:SetTarget(s.Robos)
+        e1:SetValue(1)
+        e1:SetReset(RESET_PHASE|PHASE_END)
+        Duel.RegisterEffect(e1,tp)
+        aux.RegisterClientHint(c,nil,tp,1,0,aux.Stringid(id,2))
+	end
+end
+function s.Robos(c)
+    return c:IsCode(01412158, 75923050) and c:IsFaceup()
+end
