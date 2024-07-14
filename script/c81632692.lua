a153d4ee6434279756a7c176ba011d11a35d6a3c
CaptPurpleHeart <ben.simga@gmail.com>
2024-07-14 16:30:55 +0100

    tristan

---
+++ script/c81632692.lua
@@ -0,0 +1,93 @@
+--Robomonkey Tristan (CT)
+local s,id=GetID()
+function s.initial_effect(c)
+	--Add 1 "Fiendsmith" Spell/Trap from your Deck to your hand
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCountLimit(1,{id,1})
+	e1:SetCost(s.thcost)
+	e1:SetTarget(s.thtg)
+	e1:SetOperation(s.thop)
+	c:RegisterEffect(e1)
+
+    --Special Summon this card
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetCountLimit(1,{id,2})
+    e2:SetCost(s.sscost)
+	e2:SetTarget(s.sstg)
+	e2:SetOperation(s.ssop)
+	c:RegisterEffect(e2)
+
+    -- Fusion Summon
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,0))
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCountLimit(1,{id,3})
+	e3:SetCost(s.cost)
+	e3:SetTarget(Fusion.SummonEffTG())
+	e3:SetOperation(Fusion.SummonEffOP())
+	c:RegisterEffect(e3)
+end
+function s.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return c:IsDiscardable() end
+	Duel.SendtoGrave(c,REASON_COST|REASON_DISCARD)
+end
+function s.thfilter(c)
+	return ((((c:IsRace(RACE_MACHINE) and c:IsType(TYPE_NORMAL)) and c:IsLevelBelow(3))) or c:IsSetCard(0x2178)) and c:IsAbleToHand() and not c:IsCode(id)
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
+	--Tribute 2 monsters as cost
+    function s.sscost(e,tp,eg,ep,ev,re,r,rp,chk)
+        if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_HAND,0,1,nil)
+            and Duel.IsExistingMatchingCard(s.cfilter2,tp,LOCATION_HAND,0,1,nil,tp) end
+        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONFIRM)
+        local g1=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_HAND,0,1,1,nil)
+        local g2=Duel.SelectMatchingCard(tp,s.cfilter2,tp,LOCATION_HAND,0,1,1,nil,tp)
+        e:SetLabel(g2:GetFirst():GetCode())
+        g1:Merge(g2)
+        Duel.ConfirmCards(1-tp,g1)
+        Duel.ShuffleHand(tp)
+    end
+        --Activation legality
+    function s.sstg(e,tp,eg,ep,ev,re,r,rp,chk)
+        if chk==0 then return e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
+        Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+    end
+        --Special summon itself from GY
+    function s.ssop(e,tp,eg,ep,ev,re,r,rp)
+        local c=e:GetHandler()
+        if c:IsRelateToEffect(e) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 then
+            Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+        end
+    end
+    function s.cfilter(c)
+        return c:IsCode(38916461) and not c:IsPublic()
+    end
+    function s.cfilter2(c,tp)
+        return c:IsCode(92421852) and not c:IsPublic()
+    end
+
+    function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+        if chk==0 then return e:GetHandler():IsReleasable() end
+        Duel.Release(e:GetHandler(),REASON_COST)
+    end
