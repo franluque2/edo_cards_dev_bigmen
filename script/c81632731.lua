6c1cce930cf10bc76ada46cbe2a9fdc07e22a983
Fran Luque <franluque2@gmail.com>
2024-08-23 18:15:01 -0300

    Doodle Beast Fusions

--- script/c81632731.lua
+++ script/c81632731.lua
@@ -1,5 +1,98 @@
 --Doodle Beast - Rex Raptor
 local s,id=GetID()
 function s.initial_effect(c)
-	
+    c:EnableReviveLimit()
+    Fusion.AddProcMix(c,true,true,67725394,{81632727,s.ffilter})
+
+    -- must be fusion summoned
+    local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
+	e1:SetValue(aux.fuslimit)
+	c:RegisterEffect(e1)
+
+	-- Destroy
+    local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_DESTROY)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+    e2:SetDescription(aux.Stringid(id,0))
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCondition(s.descon)
+	e2:SetTarget(s.destg)
+	e2:SetOperation(s.desop)
+	c:RegisterEffect(e2)
+
+    local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetCode(EFFECT_MATERIAL_CHECK)
+	e3:SetValue(s.valcheck)
+	e3:SetLabelObject(e2)
+	c:RegisterEffect(e3)
+
+    --Special Summon 1 Doodle Beast monster from your GY
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,1))
+	e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e4:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
+	e4:SetCode(EVENT_DESTROYED)
+	e4:SetCondition(function(e,tp,eg,ep,ev,re,r,rp) return rp==1-tp end)
+	e4:SetTarget(s.sptg)
+	e4:SetOperation(s.spop)
+	c:RegisterEffect(e4)
+end
+
+s.material={67725394,81632727}
+s.listed_names={67725394,81632727}
+s.material_setcode={SET_DOODLE_BEAST}
+function s.ffilter(c,fc,sumtype,tp)
+	return c:IsRace(RACE_DINOSAUR,fc,sumtype,tp)
+end
+
+function s.spfilter(c,e,tp)
+	return c:IsSetCard(SET_DOODLE_BEAST) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_GRAVE) and s.spfilter(chkc,e,tp) end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingTarget(s.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectTarget(tp,s.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,tp,0)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
+
+function s.valcheck(e,c)
+	local g=c:GetMaterial()
+	if g:IsExists(Card.IsCode, 1, nil, 81632727) then
+
+        local g2=g:Filter(Card.IsLocation, nil, LOCATION_ONFIELD)
+        e:GetLabelObject():SetLabel(#g2) 
+    end
+end
+
+function s.descon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsSummonType(SUMMON_TYPE_FUSION) and e:GetLabel()>0
+end
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g=Duel.GetFieldGroup(tp,0,LOCATION_MZONE)
+	if chk==0 then return (#g>0) and #g<=e:GetLabel() end
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,e:GetLabel(),0,0)
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetFieldGroup(tp,0,LOCATION_MZONE)
+	if #g>0 then
+		
+        Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_DESTROY)
+        local g2=g:Select(tp, e:GetLabel(), e:GetLabel(), nil)
+        Duel.HintSelection(g2)
+        Duel.Destroy(g2, REASON_EFFECT)
+	end
 end
