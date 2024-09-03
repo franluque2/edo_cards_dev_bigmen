94c8804ba94036b6b52319b5994c88274dfb9de5
CaptPurpleHeart <ben.simga@gmail.com>
2024-09-03 03:51:57 +0100

    test

--- script/c81632724.lua
+++ script/c81632724.lua
@@ -1,4 +1,64 @@
 --Call of the Haunting (CT)
-function c81632724.initial_effect(c)
-	
+local s,id=GetID()
+function s.initial_effect(c)
+	--activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetTarget(s.tg)
+	e1:SetOperation(s.op)
+    e1:SetCountLimit(1,{id,0})
+	c:RegisterEffect(e1)
+	--draw
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetCode(EVENT_DESTROYED)
+	e2:SetTarget(s.target)
+	e2:SetOperation(s.operation)
+	c:RegisterEffect(e2)
+
+ end
+ function s.fil(c,e,tp)
+	return c:IsCode(81632718, 81632719, 81632720, 81632721) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+ end
+ function s.settg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 
+		and Duel.IsExistingMatchingCard(s.fil,tp,LOCATION_GRAVE,0,1,nil,e,tp)
+	end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
 end
+function s.setop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	if ft<=0 then return end
+	local g=Duel.GetMatchingGroup(s.fil,tp,LOCATION_GRAVE,0,nil,e,tp)
+	if #g==0 then return end
+	local sg=aux.SelectUnselectGroup(g,e,tp,1,ft,aux.dncheck,1,tp,HINTMSG_SPSUMMON)
+	Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
+end
+ function s.filter(c,e,tp)
+	return c:IsLocation(LOCATION_GRAVE) and c:GetPreviousControler()==tp and c:IsReason(REASON_DESTROY) and c:GetOriginalRace()~=RACE_ZOMBIE
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+ end
+ function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and eg:IsExists(s.filter,1,nil,e,tp) end
+	eg:Filter(s.filter,nil,e,tp):GetFirst():CreateEffectRelation(e)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,eg:Filter(s.filter,nil,e,tp),1,0,0)
+ end
+ function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local tc=eg:Filter(s.filter,nil,e,tp):GetFirst()
+	if tc and tc:IsRelateToEffect(e) then
+		Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP_ATTACK)
+		local e3=Effect.CreateEffect(e:GetHandler())
+		e3:SetType(EFFECT_TYPE_SINGLE)
+		e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e3:SetCode(EFFECT_UPDATE_ATTACK)
+		e3:SetValue(tc:GetAttack()/2)
+		tc:RegisterEffect(e3)
+		Duel.SpecialSummonComplete()
+	end
+ end
