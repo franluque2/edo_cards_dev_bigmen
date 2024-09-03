f79a366f41b4df2f88c8e8136d6fb9f2c19d682e
CaptPurpleHeart <ben.simga@gmail.com>
2024-09-03 10:31:57 +0100

    fix

--- script/c81632719.lua
+++ script/c81632719.lua
@@ -78,7 +78,7 @@ end
 
 function s.descon(e,tp,eg,ep,ev,re,r,rp)
 	local d=Duel.GetAttackTarget()
-	return e:GetHandler()==Duel.GetAttacker()
+	return e:GetHandler()==Duel.GetAttacker() and d and d:IsFaceup()
 end
 function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():IsRace(RACE_ZOMBIE) end
