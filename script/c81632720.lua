69680b6b3db87105eedfa83df595d11a051378f5
CaptPurpleHeart <ben.simga@gmail.com>
2024-09-03 04:41:35 +0100

    fix

--- script/c81632720.lua
+++ script/c81632720.lua
@@ -19,6 +19,31 @@ function s.initial_effect(c)
 	e2:SetOperation(s.tnop)
 	c:RegisterEffect(e2)
 
+	--search
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,0))
+	e3:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e3:SetType(EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_SINGLE)
+	e3:SetCode(EVENT_SUMMON_SUCCESS)
+	e3:SetProperty(EFFECT_FLAG_DELAY)
+	e3:SetTarget(s.thtg)
+	e3:SetOperation(s.thop)
+	c:RegisterEffect(e3)
+
+	--Negate activation during BP and halve LP
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,1))
+	e4:SetCategory(CATEGORY_NEGATE)
+	e4:SetType(EFFECT_TYPE_QUICK_O)
+	e4:SetCode(EVENT_CHAINING)
+	e4:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetCountLimit(1,id)
+	e4:SetCondition(s.discon)
+	e4:SetTarget(s.distg)
+	e4:SetOperation(s.disop)
+	c:RegisterEffect(e4)
+
 end
 
 function s.tnop(e,tp,eg,ep,ev,re,r,rp)
@@ -39,5 +64,36 @@ function s.tnop(e,tp,eg,ep,ev,re,r,rp)
 		e2:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
 		e2:SetValue(0)
 		c:RegisterEffect(e2)
+	end
+end
+
+function s.thfilter(c)
+	return c:IsCode(81632717, 81632718, 81632719, 81632721, 81632722) and c:IsAbleToHand()
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
+
+function s.discon(e,tp,eg,ep,ev,re,r,rp)
+	return rp~=tp and Duel.IsBattlePhase() and re:IsActiveType(TYPE_TRAP) and Duel.IsChainNegatable(ev)
+		and not e:GetHandler():IsStatus(STATUS_BATTLE_DESTROYED)
+end
+function s.distg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsRace(RACE_ZOMBIE) end
+	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
+end
+function s.disop(e,tp,eg,ep,ev,re,r,rp)
+	if not Duel.NegateActivation(ev) or e:GetHandler():IsRace(RACE_ZOMBIE) then return end
+	if re:IsHasType(EFFECT_TYPE_ACTIVATE) and re:GetHandler():IsRelateToEffect(re) then
+		Duel.SendtoGrave(eg,REASON_EFFECT)
 	end
 end
