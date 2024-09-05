2e0b216c1540bfe2a55baff887b7079d64457364
Fran Luque <franluque2@gmail.com>
2024-09-04 21:43:10 -0300

    Balance Change

--- script/c851632029.lua
+++ script/c851632029.lua
@@ -4,7 +4,7 @@ local s,id=GetID()
 function s.initial_effect(c)
     c:EnableReviveLimit()
     c:SetSPSummonOnce(id)
-	Fusion.AddProcMixN(c,true,true,CARD_SPIDERITELING,3)
+	Fusion.AddProcMixN(c,true,true,CARD_SPIDERITELING,5)
 	Fusion.AddContactProc(c,s.contactfil,s.contactop,s.splimit)
 
     local e1=Effect.CreateEffect(c)
