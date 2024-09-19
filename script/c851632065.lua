8d31e17ccfc1eed6ad74ef41d94cc0956017393c
Fran Luque <franluque2@gmail.com>
2024-09-19 00:45:10 -0300

    Add Iron Chain Minotaur

---
+++ script/c851632065.lua
@@ -0,0 +1,101 @@
+--Iron Chain Minotaur
+Duel.LoadScript ("wb_aux.lua")
+local s,id=GetID()
+function s.initial_effect(c)
+    	--spsummon
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+    e1:SetCountLimit(1,id)
+	e1:SetCost(s.spcost)
+	e1:SetTarget(s.sptg)
+	e1:SetOperation(s.spop)
+	c:RegisterEffect(e1)
+	
+    local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e3:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+	e3:SetCode(EVENT_BE_MATERIAL)
+	e3:SetCondition(function(e,tp,eg,ep,ev,re,r,rp) return (r&REASON_SYNCHRO)==REASON_SYNCHRO and e:GetHandler():GetReasonCard():IsAttribute(ATTRIBUTE_EARTH) end)
+	e3:SetOperation(s.effop)
+	c:RegisterEffect(e3)
+end
+
+function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckLPCost(tp,1000) end
+	Duel.PayLPCost(tp,1000)
+end
+function s.filter(c,e,tp)
+	return c:IsSetCard(0x25) and c:IsCanBeSpecialSummoned(e,0,tp,false,false) and not c:IsCode(id)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
+	if #g>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
+
+function s.effop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local rc=c:GetReasonCard()
+	local e1=Effect.CreateEffect(rc)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_CHANGE_BATTLE_DAMAGE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetValue(s.value)
+	e1:SetReset(RESET_EVENT|RESETS_STANDARD)
+	rc:RegisterEffect(e1,true)
+	if not rc:IsType(TYPE_EFFECT) then
+		local e2=Effect.CreateEffect(c)
+		e2:SetType(EFFECT_TYPE_SINGLE)
+		e2:SetCode(EFFECT_ADD_TYPE)
+		e2:SetValue(TYPE_EFFECT)
+		e2:SetReset(RESET_EVENT|RESETS_STANDARD)
+		rc:RegisterEffect(e2,true)
+	end
+	rc:RegisterFlagEffect(id,RESET_EVENT|RESETS_STANDARD,EFFECT_FLAG_CLIENT_HINT,1,0,aux.Stringid(id,1))
+end
+
+
+function s.value(e, damp)
+    if e:GetOwnerPlayer()~=damp then
+		local val=Duel.GetBattleDamage(damp)
+		if val>100 then
+			if Duel.GetFlagEffect(damp, id)==0 then
+			Duel.RegisterFlagEffect(damp, id, RESET_PHASE+PHASE_DAMAGE, 0, 0)
+			local tomill=math.floor((val-100)/500)
+			local e1=Effect.CreateEffect(e:GetHandler())
+			e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+			e1:SetCode(EVENT_ADJUST)
+			e1:SetCountLimit(1,0,EFFECT_COUNT_CODE_CHAIN)
+			e1:SetLabel(tomill)
+			e1:SetOperation(s.millop)
+			Duel.RegisterEffect(e1,damp)
+		end
+
+		end
+
+		return 100
+	else
+		return -1
+	end
+end
+
+function s.millop(e,tp,eg,ep,ev,re,r,rp)
+	local val=e:GetLabel()
+	if val>0 then
+		e:SetLabel(0)
+		Duel.DiscardDeck(tp, val, REASON_EFFECT)
+		e:Reset()
+
+	end
+end
