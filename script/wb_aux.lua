19b324cdff27f05703741138b718c16f5845ca84
Fran Luque <franluque2@gmail.com>
2024-09-22 22:22:57 -0300

    Bugfix

--- script/wb_aux.lua
+++ script/wb_aux.lua
@@ -35,11 +35,11 @@ end
 
 function WbAux.CanPlayerSummonDredge(tp)
     return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-    and Duel.IsPlayerCanSpecialSummonMonster(tp,CARD_ABYSSAL_DREDGE,0,TYPE_EFFECT,0,0,4,RACE_AQUA,ATTRIBUTE_DARK)
+    and Duel.IsPlayerCanSpecialSummonMonster(tp,CARD_ABYSSAL_DREDGE,0,TYPE_EFFECT,0,0,4,RACE_REPTILE,ATTRIBUTE_DARK)
 end
 
 function WbAux.SpecialSummonDredge(tp, pos)
-    if not Duel.IsPlayerCanSpecialSummonMonster(tp,CARD_ABYSSAL_DREDGE,0,TYPE_EFFECT,0,0,4,RACE_AQUA,ATTRIBUTE_DARK) then return false end
+    if not Duel.IsPlayerCanSpecialSummonMonster(tp,CARD_ABYSSAL_DREDGE,0,TYPE_EFFECT,0,0,4,RACE_REPTILE,ATTRIBUTE_DARK) then return false end
     local dredge=Duel.CreateToken(tp, CARD_ABYSSAL_DREDGE)
     if not pos then pos=POS_FACEUP end
     return Duel.SpecialSummon(dredge, SUMMON_TYPE_SPECIAL, tp, tp, false,false, pos)
