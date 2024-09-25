196d7a10c968fc86bd6bd75f2f404cc783d9ca48
CaptPurpleHeart <ben.simga@gmail.com>
2024-09-25 22:26:27 +0100

    fix

--- script/c81632742.lua
+++ script/c81632742.lua
@@ -37,25 +37,26 @@ function s.filter(c)
 	return c:IsFaceup() and c:IsAttackPos() and c:IsRace(RACE_DINOSAUR) and c:IsCanChangePositionRush()
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	--Requirement
 	--Effect
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-	local g2=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_MZONE,0,1,2,nil)
-	if #g2>0 then
-		Duel.HintSelection(g2)
-		for tc in g2:Iter() do
-			Duel.ChangePosition(tc,POS_FACEUP_DEFENSE)
-		end
-			local ge0=Effect.CreateEffect(e:GetHandler())
-            ge0:SetCode(EFFECT_UPDATE_ATTACK)
-            ge0:SetType(EFFECT_TYPE_SINGLE)
-            ge0:SetValue(300)
-            ge0:SetReset(RESET_PHASE+PHASE_END)
-            local ge1=Effect.CreateEffect(e:GetHandler())
-            ge1:SetCode(EFFECT_UPDATE_DEFENSE)
-            ge1:SetType(EFFECT_TYPE_SINGLE)
-            ge1:SetValue(300)
-            ge1:SetReset(RESET_PHASE+PHASE_END)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_POSCHANGE)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_MZONE,0,1,2,nil)
+	if #g==0 then return end
+	Duel.HintSelection(g)
+	if Duel.ChangePosition(g,POS_FACEUP_DEFENSE)~=#g then return end
+	local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_MZONE,0,nil)
+	for tc in g:Iter() do
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_DEFENSE)
+		e1:SetValue(300)
+		e1:SetReset(RESETS_STANDARD_PHASE_END)
+		tc:RegisterEffect(e1)
+        local e2=Effect.CreateEffect(e:GetHandler())
+		e2:SetType(EFFECT_TYPE_SINGLE)
+		e2:SetCode(EFFECT_UPDATE_ATTACK)
+		e2:SetValue(300)
+		e2:SetReset(RESETS_STANDARD_PHASE_END)
+		tc:RegisterEffect(e2)
 	end
 end
 
