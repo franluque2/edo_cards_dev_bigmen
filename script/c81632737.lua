81de6d0b696546dda620dba7f34120624834ec9c
CaptPurpleHeart <ben.simga@gmail.com>
2024-09-25 21:35:31 +0100

    test

---
+++ script/c81632737.lua
@@ -0,0 +1,33 @@
+--Dynakit Stegostickus
+local s,id=GetID()
+function s.initial_effect(c)
+   	--Send the top 2 cards of deck to GY
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_DECKDES)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1)
+	e2:SetTarget(s.target)
+	e2:SetOperation(s.operation)
+	c:RegisterEffect(e2)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDiscardDeck(tp,4) end
+	Duel.SetOperationInfo(0,CATEGORY_DECKDES,nil,0,tp,4)
+end
+function s.thfilter(c)
+	return c:IsRace(RACE_DINOSAUR) or c:IsCode(81632741) and c:IsAbleToHand()
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	--Effect
+	Duel.DiscardDeck(tp,4,REASON_EFFECT)
+	if Duel.IsExistingMatchingCard(aux.NecroValleyFilter(s.thfilter),tp,LOCATION_GRAVE,0,1,nil) and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+		if #g>0 then
+			Duel.SendtoHand(g,nil,REASON_EFFECT)
+			Duel.ConfirmCards(1-tp,g)
+		end
+	end
+end
