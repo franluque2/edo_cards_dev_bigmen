cf484bf38ddd73f545112ac8f6b9c6b2c9ed6d4f
CaptPurpleHeart <ben.simga@gmail.com>
2024-09-25 22:52:12 +0100

    paint

---
+++ script/c81632743.lua
@@ -0,0 +1,52 @@
+--Protective Dynakit Paint
+local s,id=GetID()
+function s.initial_effect(c)
+	--Your attacked monster gains 500 ATK
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCategory(CATEGORY_ATKCHANGE)
+	e1:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCondition(s.condition)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTurnPlayer()==1-tp
+end
+function s.cfilter(c)
+	return c:IsRace(RACE_DINOSAUR) and c:IsMonster() and c:IsAbleToDeckOrExtraAsCost()
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_GRAVE,0,3,nil) end
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsFaceup,tp,0,LOCATION_MZONE,1,nil) end
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	--Requirement
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+	local g=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_GRAVE,0,3,3,nil)
+	Duel.HintSelection(g)
+	if #g>0 and Duel.SendtoDeck(g,nil,SEQ_DECKSHUFFLE,REASON_COST)>0 then
+		local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,0,LOCATION_MZONE,nil)
+		if #g==0 then return end
+        local tc=Duel.GetAttackTarget()
+        if tc and tc:IsRelateToBattle() and tc:IsFaceup() then
+            local e1=Effect.CreateEffect(e:GetHandler())
+            e1:SetType(EFFECT_TYPE_SINGLE)
+            e1:SetCode(EFFECT_UPDATE_ATTACK)
+            e1:SetValue(tc:GetLevel()*100)
+            e1:SetReset(RESETS_STANDARD_PHASE_END)
+            tc:RegisterEffect(e1)
+            local e2=Effect.CreateEffect(e:GetHandler())
+            e2:SetType(EFFECT_TYPE_SINGLE)
+            e2:SetCode(EFFECT_UPDATE_DEFENSE)
+            e2:SetValue(tc:GetLevel()*100)
+            e2:SetReset(RESETS_STANDARD_PHASE_END)
+            tc:RegisterEffect(e2)
+        end
+end
+end
