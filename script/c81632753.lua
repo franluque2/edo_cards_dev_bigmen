7aa245689ffdab87ab5f49c803a4c48a22da04b6
CaptPurpleHeart <ben.simga@gmail.com>
2024-09-26 04:42:10 +0100

    Interesting Manga

--- script/c81632753.lua
+++ script/c81632753.lua
@@ -1,4 +1,49 @@
 --Interesting Manga You Would Read Again (CT)
-function c81632753.initial_effect(c)
-	
+local s,id=GetID()
+function s.initial_effect(c)
+	--Increase ATK
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_ATKCHANGE)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+    e1:SetCondition(s.condition)
+	e1:SetCost(s.cost)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+function s.filter(c)
+	return c:IsFaceup() and c:IsAttribute(ATTRIBUTE_WATER)
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToGraveAsCost,tp,LOCATION_HAND,0,1,e:GetHandler())
+		and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_MZONE,0,1,nil) end
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(s.tdfilter,tp,LOCATION_GRAVE,0,nil)
+	if #g>0 then
+		--Effect
+        local ct=Duel.SendtoDeck(g,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)
+        if ct>0 then
+            Duel.Recover(tp,ct*300,REASON_EFFECT)
+            Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_MZONE,0,1,1,nil)
+            Duel.HintSelection(g)
+            local e1=Effect.CreateEffect(c)
+            e1:SetType(EFFECT_TYPE_SINGLE)
+            e1:SetCode(EFFECT_UPDATE_ATTACK)
+            e1:SetValue(4600)
+            e1:SetReset(RESETS_STANDARD_PHASE_END)
+            g:GetFirst():RegisterEffect(e1)
+        end
+end
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(s.Manga,tp,LOCATION_GRAVE,0,5,nil,nil)
+end
+
+function s.Manga(c)
+	return c:IsCode(81632751, 81632754, 81632755)
+end
+
+function s.tdfilter(c)
+	return c:IsCode(81632751, 81632754, 81632755) and c:IsAbleToDeckAsCost()
 end
