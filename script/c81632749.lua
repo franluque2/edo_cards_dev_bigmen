64861014af3143156a4f6ae85614f07e5c4d6638
CaptPurpleHeart <ben.simga@gmail.com>
2024-09-26 04:03:08 +0100

    fix

--- script/c81632749.lua
+++ script/c81632749.lua
@@ -17,7 +17,7 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.SetOperationInfo(0,CATEGORY_DECKDES,nil,0,tp,3)
 end
 function s.cfilter(c)
-	return c:IsLocation(LOCATION_GRAVE) and c:IsRace(RACE_BEAST) and c:IsAttribute(ATTRIBUTE_WATER) and c:IsMonster()
+	return c:IsLocation(LOCATION_GRAVE) and (c:IsRace(RACE_BEAST) and c:IsAttribute(ATTRIBUTE_WATER)) and c:IsMonster()
 end
 function s.spfilter(c,e,tp)
 	return c:IsRace(RACE_BEAST) and c:IsAttribute(ATTRIBUTE_WATER) and c:IsMonster() and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
@@ -27,7 +27,7 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	Duel.DiscardDeck(tp,3,REASON_EFFECT)
 	local g=Duel.GetOperatedGroup()
 	local ct=g:FilterCount(s.cfilter,nil)
-	if ct>0 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 then
+	if ct>0 then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
 		local g=Duel.GetMatchingGroup(aux.NecroValleyFilter(s.Manga),tp,LOCATION_GRAVE,0,nil,e,tp)
 		if #g>0 and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
