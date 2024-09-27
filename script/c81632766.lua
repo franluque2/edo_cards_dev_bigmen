d6f6c3bd6d25d199e7436e05528d63b2d79a0413
CaptPurpleHeart <ben.simga@gmail.com>
2024-09-27 05:32:20 +0100

    equips

---
+++ script/c81632766.lua
@@ -0,0 +1,24 @@
+--Noodle Mask of Gluttony (CT)
+local s,id=GetID()
+function s.initial_effect(c)
+	aux.AddEquipProcedure(c,nil,s.filter)
+	--Atk,def
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_EQUIP)
+	e2:SetCode(EFFECT_UPDATE_ATTACK)
+    e2:SetCost(s.cost)
+	e2:SetValue(1000)
+	c:RegisterEffect(e2)
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_EQUIP)
+	e3:SetCode(EFFECT_UPDATE_DEFENSE)
+	e3:SetValue(-1000)
+	c:RegisterEffect(e3)
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckLPCost(tp,1000) end
+	Duel.PayLPCost(tp,1000)
+end
+function s.filter(c)
+	return c:IsRace(RACE_PYRO) and c:IsLevelAbove(7)
+end
---
+++ script/c81632767.lua
@@ -0,0 +1,20 @@
+--The Noodle Mask of Ghost Peppers (CT)
+local s,id=GetID()
+function s.initial_effect(c)
+	aux.AddEquipProcedure(c,nil,s.filter)
+	--Atk,def
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_EQUIP)
+	e2:SetCode(EFFECT_UPDATE_ATTACK)
+	e2:SetValue(800)
+	c:RegisterEffect(e2)
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_EQUIP)
+	e3:SetCode(EFFECT_UPDATE_DEFENSE)
+	e3:SetValue(800)
+	c:RegisterEffect(e3)
+end
+
+function s.filter(c)
+	return c:IsRace(RACE_PYRO)
+end
