749f74a2f065feba54f92ed190cd7131a08ce36f
Fran Luque <franluque2@gmail.com>
2024-10-29 18:31:04 -0300

    Text Change

--- script/c851632088.lua
+++ script/c851632088.lua
@@ -7,7 +7,7 @@ function s.initial_effect(c)
 
     local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetCode(EFFECT_INDESTRUCTABLE_COUNT)
+	e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetTargetRange(LOCATION_MZONE,0)
 	e1:SetTarget(function(_,c) return c:GetMutualLinkedGroupCount()>0 end)
