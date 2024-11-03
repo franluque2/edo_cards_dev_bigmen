82c9399bad1a56626ad7889d0fa748f1674d38bc
Fran Luque <franluque2@gmail.com>
2024-11-03 02:11:18 -0300

    Guard Bugfix

--- script/c851632088.lua
+++ script/c851632088.lua
@@ -7,10 +7,11 @@ function s.initial_effect(c)
 
     local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+	e1:SetCode(EFFECT_INDESTRUCTABLE_COUNT)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetTargetRange(LOCATION_MZONE,0)
 	e1:SetTarget(function(_,c) return c:GetMutualLinkedGroupCount()>0 end)
+	e1:SetValue(function(_,_,r) return r&REASON_BATTLE==REASON_BATTLE end)
 	e1:SetCountLimit(1)
 	c:RegisterEffect(e1)
 
