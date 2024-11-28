087f88f30132ae230a4b6e4585501bac192e5456
CaptPurpleHeart <ben.simga@gmail.com>
2024-11-28 18:13:03 +0000

    fix

--- script/c81632802.lua
+++ script/c81632802.lua
@@ -24,6 +24,7 @@ function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsPlayerCanDiscardDeck(tp,1) end
 end
 function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
 	--Requirement
 	if Duel.DiscardDeck(tp,1,REASON_COST)<1 then return end
 	--Effect
