a59a1758f7f66a8d0efb977c6c80becb56a4dec2
fran L <franluque2@gmail.com>
2024-12-28 18:13:26 -0300

    Fix

--- script/c81633218.lua
+++ script/c81633218.lua
@@ -30,7 +30,7 @@ function s.op(e,tp,eg,ep,ev,re,r,rp)
 		Duel.RegisterEffect(e1,tp)
 
         local e2=Effect.CreateEffect(e:GetHandler())
-		e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e2:SetType(EFFECT_TYPE_FIELD)
         e2:SetCode(EFFECT_ADD_CODE)
         e2:SetTargetRange(LOCATION_MZONE,0)
         e2:SetTarget(function(_,c) return c:IsOriginalCode(TOKEN_ADVENTURER) end)
