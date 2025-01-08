2f256a6d68ee00120abcf1a8c7b47f5a67ab7f9f
CaptPurpleHeart <ben.simga@gmail.com>
2025-01-08 10:20:31 +0000

    fix

--- script/c81632815.lua
+++ script/c81632815.lua
@@ -1,6 +1,7 @@
 --Ninja Master Shogun (CT)
 local s,id=GetID()
 function s.initial_effect(c)
+	c:SetUniqueOnField(1,0,id)
 	--special summon
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
