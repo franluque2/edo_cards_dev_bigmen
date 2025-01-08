ef99058aac92b1dbeecb1ea8531ff5f0fc822bfd
CaptPurpleHeart <ben.simga@gmail.com>
2025-01-08 13:59:28 +0000

    corn

---
+++ script/c81632826.lua
@@ -0,0 +1,4 @@
+--Cornditioning (CT)
+function c81632826.initial_effect(c)
+	
+end
---
+++ script/c81632828.lua
@@ -0,0 +1,27 @@
+--Cornfused (CT)
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DISABLE)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_CHAINING)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return ep~=tp and re:IsActiveType(TYPE_EFFECT) and Duel.IsChainDisablable(ev)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_DISABLE,eg,1,0,0)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.NegateEffect(ev) and Duel.GetLocationCount(tp,LOCATION_MZONE)>1 then
+    local token=Duel.CreateToken(tp,74983882)
+    Duel.SpecialSummon(token,0,tp,tp,false,false,POS_FACEUP)
+    Duel.SpecialSummon(token,0,tp,tp,false,false,POS_FACEUP)
+end
+end
