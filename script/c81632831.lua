a137b8aacedfe0950bed9a4d6828b2b7da61de9f
CaptPurpleHeart <ben.simga@gmail.com>
2025-01-10 19:46:54 +0000

    tragoedia

---
+++ script/c81632831.lua
@@ -0,0 +1,89 @@
+--A World of True Darkness (CT)
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH+CATEGORY_DECKDES)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,{id,0})
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+    --no damage
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_FIELD)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetCode(EVENT_PRE_BATTLE_DAMAGE)
+    e2:SetProperty(EFFECT_FLAG_DELAY)
+    e2:SetCountLimit(1,{id,2})
+	e2:SetOperation(s.op)
+	c:RegisterEffect(e2)
+    local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,1))
+	e4:SetCategory(CATEGORY_TODECK+CATEGORY_TOHAND)
+	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e4:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e4:SetCode(EVENT_PHASE+PHASE_END)
+	e4:SetRange(LOCATION_SZONE)
+	e4:SetCountLimit(1,{id,1})
+	e4:SetTarget(s.settg)
+	e4:SetOperation(s.setop)
+	c:RegisterEffect(e4)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		if Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)<5 then return false end
+		local g=Duel.GetDecktopGroup(tp,5)
+		local result=g:FilterCount(Card.IsAbleToHand,nil)>0
+		return result and Duel.IsPlayerCanDiscardDeck(tp,5)
+	end
+end
+function s.filter(c)
+	return c:IsSpellTrap()
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	if not Duel.IsPlayerCanDiscardDeck(tp,5) then return end
+	Duel.ConfirmDecktop(tp,5)
+	local g=Duel.GetDecktopGroup(tp,5)
+	if #g>0 then
+		Duel.DisableShuffleCheck()
+		if g:IsExists(s.filter,1,nil) then
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+			local sg=g:FilterSelect(tp,s.filter,1,1,nil)
+			if sg:GetFirst():IsAbleToHand() then
+				Duel.SendtoHand(sg,nil,REASON_EFFECT)
+				Duel.ConfirmCards(1-tp,sg)
+				Duel.ShuffleHand(tp)
+				g:Sub(sg)
+				Duel.SendtoGrave(g,REASON_EFFECT|REASON_EXCAVATE)
+			else
+				Duel.SendtoGrave(g,REASON_EFFECT|REASON_EXCAVATE)
+			end
+	    end
+    end
+end
+function s.op(e,tp,eg,ep,ev,re,r,rp)
+	if ep==1-tp then
+		Duel.Hint(HINT_CARD,0,id)
+		Duel.ChangeBattleDamage(1-tp,0)
+		Duel.Recover(tp,ev,REASON_EFFECT)
+	end
+end
+
+function s.setfilter(c)
+	return (c:IsAttribute(ATTRIBUTE_DARK) and c:IsLevel(10)) and c:IsAbleToDeck()
+end
+function s.settg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.setfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.setfilter,tp,LOCATION_GRAVE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+	local g=Duel.SelectTarget(tp,s.setfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,g,1,0,0)
+end
+function s.setop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) then
+		Duel.SendtoDeck(tc,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)
+	end
+end
