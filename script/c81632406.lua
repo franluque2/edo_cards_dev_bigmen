30389c8bc03474046eea22cfc7fe783f9f75b9dd
CaptPurpleHeart <ben.simga@gmail.com>
2025-01-12 05:29:15 +0000

    fix

--- script/c81632406.lua
+++ script/c81632406.lua
@@ -72,7 +72,7 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 			e1:SetType(EFFECT_TYPE_SINGLE)
 			e1:SetCode(EFFECT_UPDATE_DEFENSE)
 			e1:SetValue(1000)
-			e1:SetReset(RESET_EVENT|RESETS_STANDARD)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
 			tc:RegisterEffect(e1)
 		end
 end
