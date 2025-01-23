c276bb01882a204a9490612603620df41313b621
CaptPurpleHeart <ben.simga@gmail.com>
2025-01-23 19:15:51 +0000

    test

--- script/c81632839.lua
+++ script/c81632839.lua
@@ -1,4 +1,56 @@
 --Duel Sanctuary (CT)
-function c81632839.initial_effect(c)
-	
+local s,id=GetID()
+function s.initial_effect(c)
+	-- Activate
+    local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+    --destroy replace yours
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e2:SetCode(EFFECT_DESTROY_REPLACE)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetTarget(s.reptg)
+	e2:SetValue(s.repval)
+	e2:SetOperation(s.repop)
+	c:RegisterEffect(e2)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(s.cardfilter,tp,LOCATION_DECK,0,nil)
+	if #g>0 and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local sg=g:Select(tp,1,1,nil)
+		Duel.SendtoHand(sg,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,sg)
+	end
+end
+function s.cardfilter(c)
+	return (c:IsCode(511000706)) and c:IsAbleToHand()
+end
+
+function s.repfilter(c,tp)
+	return (c:IsControler(tp) and c:IsCode(511000706)) and (c:IsReason(REASON_EFFECT) and c:GetReasonPlayer()==1-tp)
+end
+function s.reptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return Duel.GetFlagEffect(tp,id)==0 and c:IsDestructable(e) and not c:IsStatus(STATUS_DESTROY_CONFIRMED+STATUS_BATTLE_DESTROYED) 
+		and eg:IsExists(s.repfilter,1,nil,tp) end
+		local g=eg:Filter(s.repfilter,nil,tp)
+		if #g==1 then
+			e:SetLabelObject(g:GetFirst())
+		c:SetStatus(STATUS_DESTROY_CONFIRMED,true)
+		Duel.RegisterFlagEffect(tp,id,RESET_PHASE+PHASE_END,0,1)
+		return true
+	else return false end
+end
+function s.repval(e,c)
+	return c==e:GetLabelObject()
+end
+function s.repop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	c:SetStatus(STATUS_DESTROY_CONFIRMED,false)
+	Duel.Destroy(c,REASON_EFFECT+REASON_REPLACE)
 end
