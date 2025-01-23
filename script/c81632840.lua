a113a8098592197065036cf7d245d35bbe5e3354
CaptPurpleHeart <ben.simga@gmail.com>
2025-01-23 17:46:57 +0000

    fix

--- script/c81632840.lua
+++ script/c81632840.lua
@@ -40,7 +40,7 @@ function s.spcon(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)==0
 end
 function s.spfilter(c,e,tp)
-	return (c:IsSetCard(0x550) and c:IsRace(RACE_FAIRY) and c:IsAttribute(ATTRIBUTE_EARTH)) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+	return (c:IsSetCard(0x550) and c:IsRace(RACE_FAIRY) and c:IsAttribute(ATTRIBUTE_EARTH))
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then
