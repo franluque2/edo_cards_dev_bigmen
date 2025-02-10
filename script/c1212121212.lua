7a805dd185e6b44149887c0464b5d4f662bfd4c3
TotallyNotASpy8 <98987756+TotallyNotASpy8@users.noreply.github.com>
2025-02-10 17:51:42 -0600

    Vanilla+

---
+++ script/c1212121212.lua
@@ -0,0 +1,39 @@
+--Zombie World (WB)
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	--race
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetRange(LOCATION_FZONE)
+	e2:SetTargetRange(LOCATION_MZONE+LOCATION_GRAVE,LOCATION_MZONE+LOCATION_GRAVE)
+	e2:SetCode(EFFECT_ADD_RACE)
+	e2:SetValue(RACE_ZOMBIE)
+	e2:SetTarget(s.tg)
+	c:RegisterEffect(e2)
+end
+function s.tg(e,c)
+	if c:GetFlagEffect(1)==0 then
+		c:RegisterFlagEffect(1,0,0,0)
+		local eff
+		if c:IsLocation(LOCATION_MZONE) then
+			eff={Duel.GetPlayerEffect(c:GetControler(),EFFECT_NECRO_VALLEY)}
+		else
+			eff={c:GetCardEffect(EFFECT_NECRO_VALLEY)}
+		end
+		c:ResetFlagEffect(1)
+		for _,te in ipairs(eff) do
+			local op=te:GetOperation()
+			if not op or op(e,c) then return false end
+		end
+	end
+	return true
+end
+function s.val(e,c,re,chk)
+	if chk==0 then return true end
+	return RACE_ZOMBIE
+end
