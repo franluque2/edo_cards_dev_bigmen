3ba7e1638ab28c7ec4b2d658678c80487d5e536e
Fran Luque <franluque2@gmail.com>
2025-02-24 02:26:44 -0300

    Add Crack the Whip

--- script/c851632134.lua
+++ script/c851632134.lua
@@ -2,5 +2,51 @@
 Duel.LoadScript ("wb_aux.lua")
 local s,id=GetID()
 function s.initial_effect(c)
+    local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+
+    local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetTargetRange(LOCATION_MZONE,0)
+	e2:SetCode(EFFECT_EXTRA_ATTACK)
+    e2:SetTarget(function (_,c) return c:GetControler()~=c:GetOwner() end)
+	e2:SetValue(1)
+	c:RegisterEffect(e2)
+
+    local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetCategory(CATEGORY_REMOVE+CATEGORY_CONJURE+CATEGORY_SPECIAL_SUMMON)
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e3:SetRange(LOCATION_SZONE)
+	e3:SetCountLimit(1,{id,1})
+	e3:SetTarget(s.lvtg)
+	e3:SetOperation(s.lvop)
+	c:RegisterEffect(e3)
+
 	
+end
+
+function s.tgfilter(c)
+	return c:IsAbleToRemove() and c:IsControler(1-c:GetOwner())
+end
+function s.lvtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsFaceup() end
+	if chk==0 then return Duel.IsExistingTarget(s.tgfilter,tp,LOCATION_MZONE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	Duel.SelectTarget(tp,s.tgfilter,tp,LOCATION_MZONE,0,1,1,nil)
+end
+function s.lvop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsFaceup() and tc:IsRelateToEffect(e) then
+        if Duel.Remove(tc, POS_FACEUP, REASON_EFFECT)>0 then
+            local tosum=Duel.CreateToken(tp,tc:GetOriginalCode())
+            if tosum then
+                Duel.SpecialSummon(tosum, SUMMON_TYPE_SPECIAL, tp, tp, true, true, POS_FACEUP)
+            end
+        end
+	end
 end
