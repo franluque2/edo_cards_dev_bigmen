7d44b929d3d7298a1e682b9167a183e0efb07d3e
Fran Luque <franluque2@gmail.com>
2025-03-03 16:19:24 -0300

    Fix

--- script/c81633145.lua
+++ script/c81633145.lua
@@ -81,6 +81,16 @@ function s.flipop(e,tp,eg,ep,ev,re,r,rp)
             e3:SetValue(s.atktg)
             tc:RegisterEffect(e3)
 			
+			local e2=Effect.CreateEffect(tc)
+			e2:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_DESTROY)
+			e2:SetType(EFFECT_TYPE_IGNITION)
+			e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+			e2:SetRange(LOCATION_MZONE)
+			e2:SetCountLimit(1,id)
+			e2:SetCost(s.spcost)
+			e2:SetTarget(s.sptg)
+			e2:SetOperation(s.spop)
+			tc:RegisterEffect(e2)
 
 			tc:RegisterFlagEffect(id,0,0,0)
 	end
@@ -88,6 +98,48 @@ function s.flipop(e,tp,eg,ep,ev,re,r,rp)
 
 
 	Duel.RegisterFlagEffect(tp,id,0,0,0)
+end
+
+
+function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return c:GetAttackAnnouncedCount()==0 end
+	--Cannot attack this turn
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(3206)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_OATH+EFFECT_FLAG_CLIENT_HINT)
+	e1:SetCode(EFFECT_CANNOT_ATTACK)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+	c:RegisterEffect(e1,true)
+end
+function s.desfilter(c,tp)
+	return c:IsFaceup() and c:IsSetCard(0x4) and Duel.GetMZoneCount(tp,c)>0
+end
+function s.spfilter(c,e,tp)
+	return c:IsRace(RACE_WARRIOR) and c:IsSetCard(0x4) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return false end
+	if chk==0 then return Duel.IsExistingTarget(s.desfilter,tp,LOCATION_ONFIELD,0,1,nil,tp)
+		and Duel.IsExistingTarget(s.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g1=Duel.SelectTarget(tp,s.desfilter,tp,LOCATION_ONFIELD,0,1,1,nil,tp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g2=Duel.SelectTarget(tp,s.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g1,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g2,1,0,0)
+	e:SetLabelObject(g1:GetFirst())
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local tg=Duel.GetTargetCards(e)
+	if #tg==0 then return end
+	local tc1=tg:GetFirst()
+	local tc2=tg:GetNext()
+	if tc1~=e:GetLabelObject() then tc1,tc2=tc2,tc1 end
+	if tc1 and Duel.Destroy(tc1,REASON_EFFECT)>0 and tc2 then
+		Duel.SpecialSummon(tc2,0,tp,tp,false,false,POS_FACEUP)
+	end
 end
 
 function s.atktg(e,c)
