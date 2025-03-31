542203884936637fcee76cedd464c39edfae224f
Fran Luque <franluque2@gmail.com>
2025-03-31 19:55:13 -0300

    Add IPC Order Team Building

--- script/c881563229.lua
+++ script/c881563229.lua
@@ -1,4 +1,41 @@
---IPC Summons TEMPNAME CHANGE
-function c881563229.initial_effect(c)
-	
+--IPC Order: Team Building
+Duel.LoadScript ("wb_aux.lua")
+Duel.LoadScript ("wb_aprilfools_aux.lua")
+local s,id=GetID()
+function s.initial_effect(c)
+    local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGPRY_CONJURE)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+
+end
+
+
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return not Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT)
+		and Duel.GetLocationCount(tp,LOCATION_MZONE)>2
+		and Duel.IsPlayerCanSpecialSummonMonster(tp,881563220,SET_IPC,TYPE_MONSTER+TYPE_EFFECT,1400,2000,4,RACE_WARRIOR,ATTRIBUTE_LIGHT) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,3,tp,0)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	if not Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) and Duel.GetLocationCount(tp,LOCATION_MZONE)>2
+		and Duel.IsPlayerCanSpecialSummonMonster(tp,881563220,SET_IPC,TYPE_MONSTER+TYPE_EFFECT,1400,2000,4,RACE_WARRIOR,ATTRIBUTE_LIGHT) then
+		for i=1,3 do
+			local token=Duel.CreateToken(tp,881563220)
+            Duel.SpecialSummonStep(token, SUMMON_TYPE_SPECIAL, tp, tp, false, false, POS_FACEUP)
+		end
+		Duel.SpecialSummonComplete()
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_FIELD)
+		e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+		e1:SetDescription(aux.Stringid(id, 1))
+		e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
+		e1:SetTargetRange(1,0)
+		e1:SetTarget(function(e,c) return not (c:IsSetCard(SET_IPC) or c:IsType(TYPE_TOKEN)) end)
+		e1:SetReset(RESET_PHASE+PHASE_END)
+		Duel.RegisterEffect(e1,tp)
+	end
 end
