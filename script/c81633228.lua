c012c010a854957e651ea951866177e15a0160bd
Fran Luque <franluque2@gmail.com>
2025-04-10 02:22:25 -0300

    Add Sector Security Crackdown

--- script/c81633228.lua
+++ script/c81633228.lua
@@ -27,10 +27,58 @@ function s.op(e,tp,eg,ep,ev,re,r,rp)
 
 
 
+		local e2=Effect.CreateEffect(e:GetHandler())
+		e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e2:SetRange(LOCATION_MZONE)
+		e2:SetCode(EVENT_BATTLE_DESTROYED)
+		e2:SetCondition(s.spcon2)
+		e2:SetTarget(s.sptg2)
+		e2:SetOperation(s.spop)
+		Duel.RegisterEffect(e2,tp)
 
 
 	end
 	e:SetLabel(1)
+end
+
+function s.spcon2(e,tp,eg,ep,ev,re,r,rp)
+	local tc=eg:GetFirst()
+	local rc=tc:GetReasonCard()
+	return #eg==1 and rc:IsControler(tp) and rc:GetPreviousLocation(LOCATION_EXTRA)
+		and tc:IsMonster() and tc:IsReason(REASON_BATTLE) and tc:IsLocation(LOCATION_GRAVE|LOCATION_REMOVED)
+end
+
+function s.sptg2(e,tp,eg,ep,ev,re,r,rp,chk)
+	local tc=eg:GetFirst()
+	local rc=tc:GetReasonCard()
+	local bc=rc:GetBattleTarget()
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and bc:IsCanBeSpecialSummoned(e,0,tp,false,false)
+			and Duel.GetFlagEffect(tp, id)==1
+		 end
+end
+
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+
+	if Duel.SelectYesNo(tp, aux.Stringid(id, 1)) then
+		Duel.Hint(HINT_CARD, tp, id)
+		Duel.RegisterFlagEffect(tp, id, 0, 0, 0)
+		local c=e:GetHandler()
+		local tc=eg:GetFirst()
+		local rc=tc:GetReasonCard()
+		local bc=rc:GetBattleTarget()
+		Duel.SpecialSummon(bc,0,tp,tp,false,false,POS_FACEUP)
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+		e1:SetCode(EFFECT_CHANGE_BATTLE_DAMAGE)
+		e1:SetValue(HALF_DAMAGE)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		bc:RegisterEffect(e1,true)
+		
+
+	
+	end
 end
 
 function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
