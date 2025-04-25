7987fee6d8143da0ec1de93864f992c76f4e4ec2
CaptPurpleHeart <ben.simga@gmail.com>
2025-04-25 15:35:37 +0100

    fusion test

--- script/c81632869.lua
+++ script/c81632869.lua
@@ -25,41 +25,44 @@ function s.initial_effect(c)
 	e1:SetTarget(s.pltg)
 	e1:SetOperation(s.plop)
 	c:RegisterEffect(e1)
+    --Gain 1000 ATK for each "Crystal Beast" monster sent to the GY
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,0))
+	e3:SetCategory(CATEGORY_ATKCHANGE)
+	e3:SetType(EFFECT_TYPE_QUICK_O)
+	e3:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+	e3:SetCode(EVENT_FREE_CHAIN)
+	e3:SetHintTiming(TIMING_DAMAGE_STEP)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCondition(s.atkcon)
+	e3:SetCost(s.atkcost)
+	e3:SetOperation(s.atkop)
+	c:RegisterEffect(e3)
 end
 function s.ffilter(c,fc,sumtype,tp,sub,mg,sg)
 	return c:IsSetCard(0x1034,fc,0,tp)
 end
-function s.spcostfilter(c)
-	return c:IsCode(81632867) and c:IsAbleToGraveAsCost() and (c:IsLocation(LOCATION_SZONE)
-		or ((c:IsMonsterCard() and c:GetCounter(0x1107)>0) and c:GetOwner()~=c:GetControler()))
+function s.hspfilter(c)
+	return c:IsMonsterCard() and c:IsFaceup() and c:IsAbleToGraveAsCost() and c:GetCounter(0x1107)>0
 end
-function s.rescon(sg,e,tp,mg)
-	return Duel.GetLocationCountFromEx(tp,tp,sg,e:GetHandler())>0
-		and sg:FilterCount(Card.IsLocation,nil,LOCATION_SZONE)==1
-end
-function s.spcon(e,c)
-	if c==nil then return true end
+function s.hspcon(e,c)
+	if not c then return true end
 	local tp=c:GetControler()
-	local g=Duel.GetMatchingGroup(s.spcostfilter,tp,LOCATION_SZONE,0,nil)
-	if #g==g:FilterCount(Card.IsLocation,nil,LOCATION_SZONE) then return false end
-	return #g>=2 and aux.SelectUnselectGroup(g,e,tp,2,2,s.rescon,0)
+	return Duel.GetLocationCountFromEx(tp,tp,nil,c)>0
+		and Duel.IsExistingMatchingCard(s.hspfilter,tp,LOCATION_STZONE,0,2,nil)
 end
-function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,c)
-	local g=Duel.GetMatchingGroup(s.spcostfilter,tp,LOCATION_SZONE,0,nil)
-	local sg=aux.SelectUnselectGroup(g,e,tp,2,2,s.rescon,1,tp,HINTMSG_REMOVE,nil,nil,true)
-	if #sg>0 then
-		sg:KeepAlive()
-		e:SetLabelObject(sg)
-		return true
-	else
-		return false
-	end
+function s.hsptg(e,tp,eg,ep,ev,re,r,rp,chk,c)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,s.hspfilter,tp,LOCATION_STZONE,0,2,2,true,nil)
+	if not g then return false end
+	g:KeepAlive()
+	e:SetLabelObject(g)
+	return true
 end
-function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
+function s.hspop(e,tp,eg,ep,ev,re,r,rp,c)
 	local g=e:GetLabelObject()
 	if not g then return end
-	c:SetMaterial(g)
-	Duel.SendtoGrave(g,POS_FACEUP,REASON_COST|REASON_MATERIAL)
+	Duel.SendtoGrave(g,REASON_COST|REASON_MATERIAL)
 	g:DeleteGroup()
 end
 function s.plfilter(c)
@@ -79,13 +82,31 @@ function s.plop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(p,LOCATION_SZONE)==0 then
 		Duel.SendtoGrave(tc,REASON_RULE,nil,PLAYER_NONE)
 	elseif tc:CheckUniqueOnField(p) and Duel.MoveToField(tc,tp,p,LOCATION_SZONE,POS_FACEUP,tc:IsMonsterCard()) then
-		--Treated as a Continuous Spell
-		local e1=Effect.CreateEffect(e:GetHandler())
+        tc:AddCounter(0x1107,1)
+	end
+end
+function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetHandler():HasFlagEffect(id) then return false end
+	return aux.StatChangeDamageStepCondition()
+end
+function s.atkfilter(c)
+	return c:IsMonsterCard() and c:IsFaceup() and c:IsAbleToGraveAsCost() and c:GetCounter(0x1107)>0
+end
+function s.atkcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g=Duel.GetMatchingGroup(s.atkfilter,tp,LOCATION_SZONE,0,nil)
+	if chk==0 then return #g>0 and not g:IsContains(e:GetHandler()) end
+	Duel.SendtoGrave(g,REASON_COST)
+	e:SetLabel(#g)
+end
+function s.atkop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsFaceup() and c:IsRelateToEffect(e) then
+		--Increase ATK
+		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-		e1:SetCode(EFFECT_CHANGE_TYPE)
-		e1:SetValue(TYPE_SPELL|TYPE_CONTINUOUS)
-		e1:SetReset((RESET_EVENT|RESETS_STANDARD)&~RESET_TURN_SET)
-		tc:RegisterEffect(e1)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetValue(e:GetLabel()*1000)
+		e1:SetReset(RESET_EVENT|RESETS_STANDARD_DISABLE)
+		c:RegisterEffect(e1)
 	end
 end
