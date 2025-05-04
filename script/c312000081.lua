e494ac7c0d8a17ea0a9e58baf469faea7c98520b
Skelecrit3129 <60719734+Skelecrit3129@users.noreply.github.com>
2025-05-04 20:01:49 +0100

    "Eternal Sunshine" ID update for Luna traps

--- script/c312000081.lua
+++ script/c312000081.lua
@@ -7,7 +7,7 @@ function s.initial_effect(c)
 	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
 	e1:SetCode(EFFECT_CHANGE_CODE)
 	e1:SetRange(LOCATION_DECK)
-	e1:SetValue(101208071)
+	e1:SetValue(28903523)
 	c:RegisterEffect(e1)
 
 	--Activate
