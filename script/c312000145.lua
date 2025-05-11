9a841c74da4d583b04cd32846e18e1b19b084d45
Skelecrit3129 <60719734+Skelecrit3129@users.noreply.github.com>
2025-05-11 15:49:31 +0100

    fix

--- script/c312000145.lua
+++ script/c312000145.lua
@@ -71,8 +71,7 @@ function s.plfilter(c)
 end
 function s.pltg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local ct=e:GetHandler():GetCounter(0x1658)
-	if chk==0 then return ct>0 and Duel.GetLocationCount(tp,LOCATION_MZONE)>=ct-1
-		and Duel.IsExistingMatchingCard(s.plfilter,tp,LOCATION_HAND+LOCATION_GRAVE,0,ct,nil) end
+	if chk==0 then return ct>0 and Duel.GetLocationCount(tp,LOCATION_MZONE)>=ct-1 end
 end
 function s.plop(e,tp,eg,ep,ev,re,r,rp)
 	local ct=e:GetLabel()
