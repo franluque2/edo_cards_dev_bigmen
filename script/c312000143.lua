d35b932d589604c1594f42036f8914fe1679d080
Skelecrit3129 <60719734+Skelecrit3129@users.noreply.github.com>
2025-05-11 12:41:09 +0100

    fix

--- script/c312000143.lua
+++ script/c312000143.lua
@@ -100,7 +100,7 @@ function s.ntcon(e,c,minc)
 	return minc==0 and Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE)>0
 end
 function s.nttg(e,c)
-	return c:IsLevel(5) and c:IsRace(RACE_FIEND) and c:IsAttribute(ATTRIBUTE_DARK))
+	return c:IsLevel(5) and c:IsRace(RACE_FIEND) and c:IsAttribute(ATTRIBUTE_DARK)
 end
 
 function s.descon(e,tp,eg,ep,ev,re,r,rp)
