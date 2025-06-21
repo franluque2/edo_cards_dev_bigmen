20740ab413f370acc189c015a6f8f4a65e0c1304
Fran Luque <franluque2@gmail.com>
2025-06-21 08:32:58 -0300

    Add "Qliphort Cloud" and "Apoqliphort Observer"

---
+++ script/c851632180.lua
@@ -0,0 +1,159 @@
+--Qliphort Cloud
+local s,id=GetID()
+function s.initial_effect(c)
+	--pendulum summon
+	Pendulum.AddProcedure(c)
+
+    local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_SPSUMMON_PROC)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	e1:SetCondition(s.spcon)
+	c:RegisterEffect(e1)
+
+    local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CANNOT_NEGATE)
+	e2:SetRange(LOCATION_PZONE)
+	e2:SetTargetRange(1,0)
+	e2:SetTarget(s.splimit)
+	c:RegisterEffect(e2)
+
+    local ep2=Effect.CreateEffect(c)
+	ep2:SetDescription(aux.Stringid(id,4))
+	ep2:SetCategory(CATEGORY_SUMMON)
+	ep2:SetType(EFFECT_TYPE_IGNITION)
+	ep2:SetRange(LOCATION_PZONE)
+	ep2:SetCountLimit(1)
+	ep2:SetCondition(s.condition)
+	ep2:SetTarget(s.target)
+	ep2:SetOperation(s.operation)
+	c:RegisterEffect(ep2)
+
+
+    local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e3:SetCategory(CATEGORY_REMOVE)
+	e3:SetProperty(EFFECT_FLAG_DELAY)
+	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e3:SetCountLimit(1,{id,1})
+	e3:SetTarget(s.bantarget)
+	e3:SetOperation(s.banoperation)
+	c:RegisterEffect(e3)
+
+    local e7=Effect.CreateEffect(c)
+	e7:SetType(EFFECT_TYPE_SINGLE)
+	e7:SetCode(EFFECT_IMMUNE_EFFECT)
+	e7:SetProperty(EFFECT_FLAG_SINGLE_RANGE+EFFECT_FLAG_UNCOPYABLE)
+	e7:SetRange(LOCATION_MZONE)
+	e7:SetValue(aux.qlifilter)
+	c:RegisterEffect(e7)
+
+    local e8=Effect.CreateEffect(c)
+	e8:SetDescription(aux.Stringid(id,2))
+	e8:SetCategory(CATEGORY_DESTROY+CATEGORY_SEARCH+CATEGORY_TOHAND)
+	e8:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e8:SetCode(EVENT_RELEASE)
+	e8:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+	e8:SetTarget(s.destg)
+	e8:SetOperation(s.desop)
+	c:RegisterEffect(e8)
+
+end
+
+s.listed_series={SET_QLI}
+function s.splimit(e,c)
+	return not c:IsSetCard(SET_QLI)
+end
+
+
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(Card.IsSetCard,tp,LOCATION_PZONE,0,1,e:GetHandler(),SET_QLI)
+end
+function s.sumfilter(c)
+	return c:IsSetCard(SET_QLI) and c:IsSummonable(true,nil)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.sumfilter,tp,LOCATION_HAND|LOCATION_MZONE,0,1,nil) end
+	local g=Duel.GetMatchingGroup(s.sumfilter,tp,LOCATION_HAND|LOCATION_MZONE,0,nil)
+	Duel.SetOperationInfo(0,CATEGORY_SUMMON,g,1,0,0)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SUMMON)
+	local tc=Duel.SelectMatchingCard(tp,s.sumfilter,tp,LOCATION_HAND|LOCATION_MZONE,0,1,1,nil):GetFirst()
+	if tc then
+		Duel.Summon(tp,tc,true,nil)
+	end
+end
+
+
+function s.filter(c)
+	return c:IsFaceup() and c:IsSetCard(SET_QLI)
+end
+function s.spcon(e,c)
+	if c==nil then return true end
+	return Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.filter,c:GetControler(),LOCATION_ONFIELD,0,1,nil)
+end
+
+
+
+function s.bantarget(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToRemove, tp, LOCATION_MZONE, LOCATION_MZONE, 1, nil) end
+    local g=Duel.GetMatchingGroup(Card.IsAbleToRemove, tp, LOCATION_MZONE, LOCATION_MZONE, nil)
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,#g,0,0)
+
+end
+
+function s.banoperation(e,tp,eg,ep,ev,re,r,rp)
+    local g=Duel.GetMatchingGroup(Card.IsAbleToRemove, tp, LOCATION_MZONE, LOCATION_MZONE, nil)
+    if Duel.Remove(g,0,REASON_EFFECT+REASON_TEMPORARY)~=0 then
+        for tc in g:Iter() do
+            Duel.ReturnToField(tc)
+        end
+    end
+end
+
+
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsOnField() end
+	if chk==0 then return Duel.IsExistingTarget(aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectTarget(tp,aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,2,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,#g,0,0)
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	local tg=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
+	local sg=tg:Filter(Card.IsRelateToEffect,nil,e)
+	Duel.Destroy(sg,REASON_EFFECT)
+	sg=Duel.GetOperatedGroup()
+	local d1=0
+	local d2=0
+	local tc=sg:GetFirst()
+	for tc in aux.Next(sg) do
+		if tc then
+			if tc:IsPreviousControler(0) then d1=d1+1
+			else d2=d2+1 end
+		end
+	end
+	if d1>0 and Duel.IsExistingMatchingCard(s.adtohandfilter, 0, LOCATION_DECK, 0, d1, nil) and Duel.SelectYesNo(0,aux.Stringid(id,3)) then s.tohandop(0,d1) end
+	if d2>0 and Duel.IsExistingMatchingCard(s.adtohandfilter, 1, LOCATION_DECK, 0, d2, nil) and Duel.SelectYesNo(1,aux.Stringid(id,3)) then s.tohandop(1,d2) end
+end
+
+function s.adtohandfilter(c)
+    return c:IsMonster() and c:IsType(TYPE_PENDULUM) and c:IsAbleToHand()
+end
+
+function s.tohandop(p, num)
+    local g=Duel.GetMatchingGroup(s.adtohandfilter,p,LOCATION_DECK,0,nil)
+    if #g>=num then
+        Duel.Hint(HINT_SELECTMSG,p,HINTMSG_ATOHAND)
+        local sg=g:Select(p,num,num,nil)
+        Duel.SendtoHand(sg,nil,REASON_EFFECT)
+        Duel.ConfirmCards(1-p,sg)
+    end
+end
---
+++ script/c851632181.lua
@@ -0,0 +1,156 @@
+--Apoqliphort Observer
+local s,id=GetID()
+function s.initial_effect(c)
+	--pendulum summon
+	Pendulum.AddProcedure(c)
+	c:EnableReviveLimit()
+	Fusion.AddProcMixN(c,true,true,aux.FilterBoolFunctionEx(Card.IsSetCard,SET_QLI),3)
+	Fusion.AddContactProc(c,s.contactfil,s.contactop,s.splimit2,nil,nil,nil,false)
+
+    --splimit
+        local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CANNOT_NEGATE)
+	e2:SetRange(LOCATION_PZONE)
+	e2:SetTargetRange(1,0)
+	e2:SetTarget(s.splimit)
+	c:RegisterEffect(e2)
+
+    local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_HAND_LIMIT)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetRange(LOCATION_PZONE)
+	e1:SetTargetRange(0,1)
+	e1:SetValue(0)
+	c:RegisterEffect(e1)
+
+
+    local e5=Effect.CreateEffect(c)
+	e5:SetType(EFFECT_TYPE_SINGLE)
+	e5:SetProperty(EFFECT_FLAG_SINGLE_RANGE+EFFECT_FLAG_UNCOPYABLE)
+	e5:SetRange(LOCATION_MZONE)
+	e5:SetCode(EFFECT_IMMUNE_EFFECT)
+	e5:SetValue(s.efilter)
+	c:RegisterEffect(e5)
+
+    	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetCategory(CATEGORY_NEGATE+CATEGORY_DESTROY+CATEGORY_TOHAND)
+	e3:SetType(EFFECT_TYPE_QUICK_O)
+	e3:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
+	e3:SetCode(EVENT_CHAINING)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCountLimit(1,{id,1})
+	e3:SetCondition(s.negcon)
+	e3:SetTarget(s.negtg)
+	e3:SetOperation(s.negop)
+	c:RegisterEffect(e3)
+
+
+    local e6=Effect.CreateEffect(c)
+	e6:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e6:SetCode(EVENT_CHAIN_SOLVING)
+	e6:SetRange(LOCATION_PZONE)
+	e6:SetOperation(s.handes)
+	c:RegisterEffect(e6)
+
+
+end
+s.listed_series={SET_QLI}
+function s.splimit(e,c)
+	return not c:IsSetCard(SET_QLI)
+end
+
+
+function s.splimit2(e,se,sp,st)
+	return (st&SUMMON_TYPE_FUSION)==SUMMON_TYPE_FUSION or e:GetHandler():GetLocation()~=LOCATION_EXTRA 
+end
+function s.contactfil(tp)
+	return Duel.GetReleaseGroup(tp)
+end
+function s.contactop(g)
+	Duel.Release(g,REASON_COST|REASON_MATERIAL)
+end
+
+
+function s.efilter(e,te)
+	if te:IsSpellTrapEffect() then
+		return true
+	else
+		return s.levelchk(e,te)
+	end
+end
+
+function s.levelchk(e,te)
+	if te:IsActiveType(TYPE_MONSTER) and te:IsActivated() then
+		local lv=e:GetHandler():GetLevel()
+		local ec=te:GetOwner()
+		if ec:IsType(TYPE_LINK) then
+			return ec:GetLink()<lv
+		elseif ec:IsType(TYPE_XYZ) then
+			return ec:GetOriginalRank()<lv
+		else
+			return ec:GetOriginalLevel()<lv
+		end
+	else
+		return false
+	end
+end
+
+
+function s.negcon(e,tp,eg,ep,ev,re,r,rp)
+	return not e:GetHandler():IsStatus(STATUS_BATTLE_DESTROYED) and ep==1-tp and Duel.IsChainNegatable(ev)
+end
+function s.negtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsAbleToExtra() and Duel.IsExistingMatchingCard(s.tohandfilter,tp,LOCATION_PZONE,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
+	if re:GetHandler():IsRelateToEffect(re) then
+		Duel.SetOperationInfo(0,CATEGORY_DESTROY,eg,1,0,0)
+	end
+end
+function s.tohandfilter(c,e,tp)
+	return c:IsSetCard(SET_QLI) and c:IsAbleToHand()
+end
+function s.negop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if Duel.NegateActivation(ev) then
+		if re:GetHandler():IsRelateToEffect(re) then
+			Duel.Destroy(eg,REASON_EFFECT)
+		end
+		if not c:IsRelateToEffect(e) then return end
+		Duel.BreakEffect()
+        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
+		local rg=Duel.SelectMatchingCard(tp,s.tohandfilter,tp,LOCATION_PZONE,0,1,1,nil)
+		if #rg>0 then
+			Duel.BreakEffect()
+			Duel.HintSelection(rg,true)
+			if Duel.SendtoHand(rg,nil,REASON_EFFECT)>0 then
+                Duel.MoveToField(c,tp,tp,LOCATION_PZONE,POS_FACEUP,true)
+            end
+		end
+
+	end
+end
+
+function s.filter(c,p)
+	return c:IsFaceup() and c:IsSetCard(SET_QLI) and c:IsControler(p) and c:IsOriginalType(TYPE_MONSTER)
+end
+
+function s.fuqlifilter(c)
+    return c:IsFaceup() and c:IsSetCard(SET_QLI)
+end
+
+s[0]=0
+function s.handes(e,tp,eg,ep,ev,re,r,rp)
+	local trig_loc,chain_id,targetcards=Duel.GetChainInfo(ev,CHAININFO_TRIGGERING_LOCATION,CHAININFO_CHAIN_ID,CHAININFO_TARGET_CARDS)
+	if not (ep==1-tp and chain_id~=s[0] and targetcards and targetcards:IsExists(s.filter,1,nil,tp)) then return end
+	s[0]=chain_id
+    local num=Duel.GetMatchingGroupCount(s.fuqlifilter, tp, LOCATION_ONFIELD, LOCATION_ONFIELD, nil)
+    if num==0 then return end
+	if Duel.GetFieldGroupCount(tp,0,LOCATION_HAND)>=num and Duel.SelectYesNo(1-tp,aux.Stringid(id,0)) then
+		Duel.DiscardHand(1-tp,nil,num,num,REASON_EFFECT|REASON_DISCARD,nil)
+		Duel.BreakEffect()
+	else Duel.NegateEffect(ev) end
+end
