b44918685a8555686578174397aec79f194a7b02
Fran Luque <franluque2@gmail.com>
2025-06-21 00:51:03 -0300

    Add "Ashened for Evermore"

---
+++ script/c851632174.lua
@@ -0,0 +1,150 @@
+--Ashened for Evermore
+Duel.LoadScript ("wb_aux.lua")
+local s,id=GetID()
+function s.initial_effect(c)
+    	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_ACTIVATE)
+	e0:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e0)
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_TRAP_ACT_IN_SET_TURN)
+	e2:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
+	e2:SetCondition(s.actcon)
+	c:RegisterEffect(e2)
+
+    local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_ADD_RACE)
+	e1:SetRange(LOCATION_SZONE)
+	e1:SetTargetRange(0,LOCATION_MZONE)
+	e1:SetCondition(function(e) return Duel.IsTurnPlayer(1-e:GetHandlerPlayer()) end)
+	e1:SetValue(RACE_PYRO)
+	c:RegisterEffect(e1)
+
+
+    local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,0))
+	e4:SetType(EFFECT_TYPE_QUICK_O)
+	e4:SetCode(EVENT_FREE_CHAIN)
+	e4:SetRange(LOCATION_SZONE)
+    e4:SetCountLimit(1,id)
+	e4:SetCost(s.cost)
+	e4:SetTarget(s.tdtg)
+	e4:SetOperation(s.tdop)
+	c:RegisterEffect(e4)
+
+
+    	local e5=Effect.CreateEffect(c)
+	e5:SetDescription(aux.Stringid(id,3))
+	e5:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e5:SetCode(EVENT_LEAVE_FIELD)
+	e5:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+	e5:SetRange(LOCATION_SZONE)
+	e5:SetCountLimit(1,{id,1})
+	e5:SetCondition(s.spcon2)
+	e5:SetTarget(s.sptg)
+	e5:SetOperation(s.spop)
+	c:RegisterEffect(e5)
+
+
+end
+s.listed_names={CARD_VEIDOS_ERUPTION_DRAGON,CARD_OBSIDIM_ASHENED_CITY}
+
+function s.darkpyrofilter(c)
+    return c:IsFaceup() and c:IsAttribute(ATTRIBUTE_DARK) and c:IsRace(RACE_PYRO)
+end
+
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,nil) end
+	Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_COST|REASON_DISCARD)
+end
+
+function s.actcon(e)
+	return Duel.IsExistingMatchingCard(s.darkpyrofilter, e:GetHandlerPlayer(), LOCATION_MZONE, LOCATION_MZONE, 1, nil)
+end
+
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsMainPhase()
+end
+function s.tdfilter(c)
+	return c:IsSetCard(SET_ASHENED) and c:IsType(TYPE_CONTINUOUS) and c:IsSpellTrap() and c:IsSSetable()
+end
+function s.tdtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.tdfilter, tp, LOCATION_DECK, 0, 1, nil) end
+end
+function s.tdop(e,tp,eg,ep,ev,re,r,rp)
+    if not e:GetHandler():IsRelateToEffect(e) then return end
+    local g=Duel.GetMatchingGroup(s.tdfilter,tp,LOCATION_DECK,0,nil)
+    if #g>0 then
+        local sc=g:Select(tp,1,1,nil):GetFirst()
+        Duel.SSet(tp,sc)
+
+        if sc:IsTrap() then
+            local e1=Effect.CreateEffect(e:GetHandler())
+            e1:SetDescription(aux.Stringid(id,1))
+            e1:SetType(EFFECT_TYPE_SINGLE)
+            e1:SetCode(EFFECT_TRAP_ACT_IN_SET_TURN)
+            e1:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
+            e1:SetReset(RESET_EVENT|RESETS_STANDARD)
+            sc:RegisterEffect(e1)
+        end
+    end
+    local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetDescription(aux.Stringid(id,2))
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
+	e1:SetTargetRange(1,0)
+	e1:SetTarget(s.splimit)
+	e1:SetReset(RESET_PHASE|PHASE_END,2)
+	Duel.RegisterEffect(e1,tp)
+	--lizard check
+	aux.addTempLizardCheck(e:GetHandler(),tp,s.lizfilter)
+end
+function s.splimit(e,c)
+	return not c:IsRace(RACE_PYRO) and c:IsLocation(LOCATION_EXTRA)
+end
+function s.lizfilter(e,c)
+	return not c:IsOriginalRace(RACE_PYRO)
+end
+
+
+
+function s.spcfilter(c,tp,rp)
+	return c:IsPreviousPosition(POS_FACEUP) and c:ListsCode(CARD_OBSIDIM_ASHENED_CITY) and c:IsPreviousLocation(LOCATION_ONFIELD)
+		and ((rp~=tp) or (c:IsReason(REASON_EFFECT) and c:GetReasonEffect():GetHandler():IsCode(CARD_VEIDOS_ERUPTION_DRAGON)))
+end
+function s.spcon2(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.spcfilter,1,nil,tp,rp)
+end
+function s.spfilter(c,e,tp)
+	if c:IsLocation(LOCATION_DECK) and Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return false end
+	if c:IsLocation(LOCATION_EXTRA) and Duel.GetLocationCountFromEx(tp,tp,nil,c)<=0 then return false end
+	return (c:IsSetCard(SET_ASHENED))
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chk==0 then return Duel.GetLocationCountFromEx(tp)>0
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK|LOCATION_EXTRA,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK|LOCATION_EXTRA)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK|LOCATION_EXTRA,0,1,1,nil,e,tp)
+	if #g>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+
+        local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetDescription(3001)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE+EFFECT_FLAG_CLIENT_HINT)
+		e1:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+		e1:SetRange(LOCATION_MZONE)
+		e1:SetValue(1)
+		e1:SetReset(RESETS_STANDARD_PHASE_END)
+		g:GetFirst():RegisterEffect(e1)
+
+	end
+end
