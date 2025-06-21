eccafe70f4552c1dce4cee1ebdd6eaef45ae5bde
Fran Luque <franluque2@gmail.com>
2025-06-21 02:18:40 -0300

    Add "UA Atacante"

--- script/c851632177.lua
+++ script/c851632177.lua
@@ -1,4 +1,137 @@
 --U.A. Atacante
-function c851632177.initial_effect(c)
-	
+Duel.LoadScript ("wb_aux.lua")
+local s,id=GetID()
+function s.initial_effect(c)
+
+    local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,1))
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_SPSUMMON_PROC)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e1:SetRange(LOCATION_HAND+LOCATION_GRAVE)
+	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	e1:SetCondition(s.spcon)
+	e1:SetTarget(s.sptg)
+	e1:SetOperation(s.spop)
+	c:RegisterEffect(e1)
+
+    local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,2))
+	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_HAND)
+	e2:SetCountLimit(1,id)
+	e2:SetCost(Cost.SelfDiscard)
+	e2:SetTarget(s.thtg)
+	e2:SetOperation(s.thop)
+	c:RegisterEffect(e2)
+
+    local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,0))
+	e3:SetCategory(CATEGORY_SUMMON)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3:SetProperty(EFFECT_FLAG_DELAY)
+	e3:SetCode(EVENT_SUMMON_SUCCESS)
+    e3:SetCountLimit(1,{id,1})
+	e3:SetTarget(s.sumtg)
+	e3:SetOperation(s.sumop)
+	c:RegisterEffect(e3)
+
+    local e4=e3:Clone()
+    e4:SetCode(EVENT_SPSUMMON_SUCCESS)
+    c:RegisterEffect(e4)
+end
+s.listed_names={id}
+s.listed_series={SET_UA}
+function s.spfilter(c,ft)
+	return c:IsFaceup() and c:IsSetCard(SET_UA) and not c:IsCode(id) and c:IsAbleToHandAsCost()
+		and (ft>0 or c:GetSequence()<5)
+end
+function s.spcon(e,c)
+	if c==nil then return true end
+	local tp=e:GetHandlerPlayer()
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	local rg=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_MZONE,0,nil,ft)
+	return (ft>-1 and #rg>0 and aux.SelectUnselectGroup(rg,e,tp,1,1,nil,0) and (e:GetHandler():IsLocation(LOCATION_HAND))) or (ft>0 and e:GetHandler():IsLocation(LOCATION_HAND|LOCATION_GRAVE) and Duel.IsExistingMatchingCard(aux.TRUE, e:GetHandlerPlayer(), 0, LOCATION_MZONE, 1, nil))
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,c)
+	local c=e:GetHandler()
+	local g=nil
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+    local b1=ft>0 and c:IsLocation(LOCATION_HAND|LOCATION_GRAVE) and Duel.IsExistingMatchingCard(aux.TRUE, e:GetHandlerPlayer(), 0, LOCATION_MZONE, 1, nil)
+    local b2=ft>-1 and c:IsLocation(LOCATION_HAND) and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_MZONE,0,1,nil,ft)
+    if not (b1 or b2) then return false end
+    if b2 and (not b1 or Duel.SelectYesNo(tp,aux.Stringid(id,0))) then
+        local rg=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_MZONE,0,nil,ft)
+        local g=aux.SelectUnselectGroup(rg,e,tp,1,1,nil,1,tp,HINTMSG_RTOHAND,nil,nil,true)
+        if #g>0 then
+            g:KeepAlive()
+            e:SetLabelObject(g)
+            return true
+        end
+    elseif b1 then
+        return true
+    end
+	return false
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
+	local g=e:GetLabelObject()
+	if not g then return end
+	Duel.SendtoHand(g,nil,REASON_COST)
+	g:DeleteGroup()
+end
+
+function s.thfilter(c)
+	return c:IsSetCard(SET_UA) and c:IsSpell() and c:IsType(TYPE_FIELD) and c:IsAbleToHand()
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
+
+function s.ntcon(e,c,minc)
+	if c==nil then return true end
+	return minc==0 and Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE)>0
+end
+
+function s.filter(c)
+    if not c:IsSetCard(SET_UA) then
+        return false
+    end
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+    e1:SetCode(EFFECT_SUMMON_PROC)
+    e1:SetCondition(s.ntcon)
+    c:RegisterEffect(e1)
+    local summonable=c:IsSummonable(true,e1)
+    e1:Reset()
+	return summonable
+end
+function s.sumtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_HAND,0,1,nil) and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 end
+	Duel.SetOperationInfo(0,CATEGORY_SUMMON,nil,1,0,0)
+end
+function s.sumop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_HAND,0,1,1,nil)
+	if #g>0 then
+        local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+		e1:SetCode(EFFECT_SUMMON_PROC)
+		e1:SetCondition(s.ntcon)
+		e1:SetReset(RESETS_STANDARD_PHASE_END)
+		g:GetFirst():RegisterEffect(e1)
+		Duel.Summon(tp,g:GetFirst(),true,e1)
+        e1:Reset()
+	end
 end
