6ef22f26e00c7a354c5ce5b7dc5330ab0721cf21
Fran Luque <franluque2@gmail.com>
2025-06-21 23:53:42 -0300

    Add "Kurichaos"

--- script/c851632180.lua
+++ script/c851632180.lua
@@ -1,4 +1,5 @@
 --Qliphort Cloud
+Duel.LoadScript ("wb_aux.lua")
 local s,id=GetID()
 function s.initial_effect(c)
 	--pendulum summon
--- script/c851632181.lua
+++ script/c851632181.lua
@@ -1,4 +1,5 @@
 --Apoqliphort Observer
+Duel.LoadScript ("wb_aux.lua")
 local s,id=GetID()
 function s.initial_effect(c)
 	--pendulum summon
