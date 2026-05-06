ab736d06f7cbfdbc52300940b007a9a36d78f493
Fran Luque <franluque2@gmail.com>
2026-05-05 21:28:24 -0300

    Add "Where HEROes Began"

---
+++ script/c781563011.lua
@@ -0,0 +1,142 @@
+--Where HEROes Began
+Duel.LoadScript("big_skill_aux.lua")
+local s, id = GetID()
+function s.initial_effect(c)
+    local e3 = Effect.CreateEffect(c)
+    e3:SetType(EFFECT_TYPE_FIELD + EFFECT_TYPE_CONTINUOUS)
+    e3:SetCode(EVENT_STARTUP)
+    e3:SetRange(0x5f)
+    e3:SetCountLimit(1)
+    e3:SetOperation(s.shuffledownop)
+    c:RegisterEffect(e3)
+	
+        local e1, e2 = BSkillaux.CreateBasicSkill(c, id, s.flipconpassive, s.flipoppassive, nil,nil, nil, true, nil)
+    c:RegisterEffect(e1)
+    c:RegisterEffect(e2)
+
+    	aux.GlobalCheck(s, function()
+		s.turncounter = {}
+		s.turncounter[0] = 0
+		s.turncounter[1] = 0
+        end)
+
+end
+
+function s.flipconpassive(e, tp, eg, ep, ev, re, r, rp)
+    return Duel.GetFlagEffect(tp, id) == 0 and Duel.GetCurrentChain() == 0
+end
+
+function s.flipoppassive(e, tp, eg, ep, ev, re, r, rp)
+    Duel.RegisterFlagEffect(tp, id, 0, 0, 0)
+    Duel.Hint(HINT_SKILL_FLIP, tp, id|(1 << 32))
+    local c = e:GetHandler()
+
+            local e2 = Effect.CreateEffect(e:GetHandler())
+        e2:SetType(EFFECT_TYPE_FIELD + EFFECT_TYPE_CONTINUOUS)
+        e2:SetCode(EVENT_PHASE + PHASE_STANDBY)
+        e2:SetCondition(s.flipconsb)
+        e2:SetCountLimit(1)
+        e2:SetOperation(s.flipopsb)
+        Duel.RegisterEffect(e2, tp)
+
+        local e1 = Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_FIELD + EFFECT_TYPE_CONTINUOUS)
+    e1:SetCode(EVENT_PREDRAW)
+    e1:SetCountLimit(1)
+    e1:SetCondition(function(e, tp, eg, ep, ev, re, r, rp) return Duel.IsTurnPlayer(tp) end)
+    e1:SetOperation(s.addheroicpouch)
+    Duel.RegisterEffect(e1, tp)
+
+        local e9 = Effect.CreateEffect(c)
+    e9:SetType(EFFECT_TYPE_FIELD + EFFECT_TYPE_CONTINUOUS)
+    e9:SetCode(EVENT_TURN_END)
+    e9:SetCountLimit(1)
+    e9:SetCondition(function(e, tp, eg, ep, ev, re, r, rp) return Duel.IsTurnPlayer(tp) end)
+    e9:SetOperation(s.removeheroicpouch)
+    Duel.RegisterEffect(e9, tp)
+end
+
+
+function s.flipconsb(e, tp, eg, ep, ev, re, r, rp)
+    return Duel.GetCurrentChain() == 0 and Duel.GetTurnPlayer() == tp
+        and s.turncounter[tp] < 6
+end
+
+function s.flipopsb(e, tp, eg, ep, ev, re, r, rp)
+    Duel.Hint(HINT_CARD, tp, id)
+    if s.turncounter[tp] > 1 then
+        if s.turncounter[tp] > 2 then
+            if s.turncounter[tp] > 3 then
+                if s.turncounter[tp] > 4 then
+                    if s.turncounter[tp] == 5 then
+                        local gencard = Duel.CreateToken(tp, 00191749)
+                        Duel.SendtoHand(gencard, tp, REASON_RULE)
+                        Duel.ConfirmCards(1 - tp, gencard)
+                    end
+                else
+                    local gencard = Duel.CreateToken(tp, 63703130)
+                    Duel.SendtoHand(gencard, tp, REASON_RULE)
+                    Duel.ConfirmCards(1 - tp, gencard)
+                end
+            else
+                local gencard = Duel.CreateToken(tp, 37318031)
+                Duel.SendtoHand(gencard, tp, REASON_RULE)
+                Duel.ConfirmCards(1 - tp, gencard)
+            end
+        else
+            local gencard = Duel.CreateToken(tp, 00213326)
+            Duel.SendtoHand(gencard, tp, REASON_RULE)
+            Duel.ConfirmCards(1 - tp, gencard)
+        end
+    else
+        if Duel.GetFlagEffect(tp, id) == 1 then
+            local gencard = Duel.CreateToken(tp, 74825788)
+            Duel.SendtoHand(gencard, tp, REASON_RULE)
+            Duel.ConfirmCards(1 - tp, gencard)
+        end
+    end
+
+    s.turncounter[tp] = s.turncounter[tp] + 1
+end
+
+local bricks={20721928,84327329,58932615,21844576,79979666,86188410,59793705,45906428,11881272,63035430,55428811,18482473,33995387, CARD_POLYMERIZATION, 40237839}
+local polyaccess={CARD_POLYMERIZATION, 40237839}
+
+function s.cardfilter(c, tp)
+    return c:IsCode(table.unpack(bricks)) and c:GetSequence() >= (Duel.GetFieldGroupCount(tp, LOCATION_DECK, 0) - (20))
+end
+
+function s.shuffledownop(e, tp, eg, ep, ev, re, r, rp)
+    local g = Duel.GetMatchingGroup(s.cardfilter, tp, LOCATION_DECK, 0, nil, tp)
+    local oneallowed=g:Filter(Card.IsCode, nil, table.unpack(polyaccess)):GetFirst()
+    if oneallowed then
+        g:RemoveCard(oneallowed)
+    end
+    if #g == Duel.GetFieldGroupCount(tp, LOCATION_DECK, 0) then return end
+    if #g > 0 then
+        Duel.MoveToDeckBottom(g)
+    end
+end
+
+
+
+function s.addheroicpouch(e, tp, eg, ep, ev, re, r, rp)
+    Duel.Hint(HINT_CARD, tp, id)
+
+    local g = Group.CreateGroup()
+    local tc = Duel.CreateToken(tp, id+1)
+    tc:RegisterFlagEffect(id, 0, 0, 0, tp)
+    g:AddCard(tc)
+
+    Duel.SendtoHand(g, nil, REASON_RULE)
+    Duel.ConfirmCards(1-tp, g)
+end
+
+function s.remfilter(c)
+    return c:IsCode(id+1) and c:IsSpell() and c:GetFlagEffect(id) > 0
+end
+
+function s.removeheroicpouch(e, tp, eg, ep, ev, re, r, rp)
+    local g = Duel.GetMatchingGroup(s.remfilter, tp, LOCATION_ALL, 0, nil)
+    Duel.RemoveCards(g)
+end
---
+++ script/c781563012.lua
@@ -0,0 +1,75 @@
+--Heroic Utility Pouch
+local s, id = GetID()
+function s.initial_effect(c)
+    --Normal Spell
+    	local e1=Effect.CreateEffect(c)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+
+end
+
+local EHERO_SPARKMAN = 20721928
+local EHERO_CLAYMAN = 84327329
+local EHERO_BURST = 58932615
+local EHERO_AVIAN = 21844576
+local EHERO_BLADEDGE = 59793705
+local EHERO_WILDHEART = 86188410
+local EHERO_BUBBLEMAN = 79979666
+local EHERO_NEOS = 89943723
+
+s.cardsperhero = {}
+s.cardsperhero[0]={}
+s.cardsperhero[1]={}
+
+
+s.cardsperhero[EHERO_SPARKMAN] = { 97362768 }
+s.cardsperhero[EHERO_CLAYMAN] = { 511000484, 511000998, 22479888 }
+s.cardsperhero[EHERO_BURST] = { 27191436 } --511000021 Burst Impact, disabled for balance reasons
+s.cardsperhero[EHERO_AVIAN] = { 19394153, 511000488, 71060915 }
+s.cardsperhero[EHERO_BLADEDGE] = { 84361420 }
+s.cardsperhero[EHERO_WILDHEART] = { 29612557, 511001353 }
+s.cardsperhero[EHERO_BUBBLEMAN] = { 05285665, 61968753, 80075749, 53586134, 511002154}
+s.cardsperhero[EHERO_NEOS] = { 10186633,14088859,16169772,35255456,41933425,52098461,80170678,18302224,46570372,52553471,73239437,89058026,11913700,47274077,42015635,74414885,75047173 }
+
+
+function s.filter(c)
+	return c:IsFaceup() and c:IsCode(EHERO_SPARKMAN, EHERO_CLAYMAN, EHERO_BURST, EHERO_AVIAN, EHERO_BLADEDGE, EHERO_WILDHEART, EHERO_BUBBLEMAN, EHERO_NEOS)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.filter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,LOCATION_MZONE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	local g=Duel.SelectTarget(tp,s.filter,tp,LOCATION_MZONE,0,1,1,nil)
+    s.announce_filter={}
+    local code=g:GetFirst():GetCode()
+    local hero_cards = s.cardsperhero[code]
+    for _, cardcode in ipairs(hero_cards) do
+        if #s.announce_filter==0 then
+            table.insert(s.announce_filter,cardcode)
+            table.insert(s.announce_filter,OPCODE_ISCODE)
+        else
+            table.insert(s.announce_filter,cardcode)
+            table.insert(s.announce_filter,OPCODE_ISCODE)
+            table.insert(s.announce_filter,OPCODE_OR)
+        end
+    end
+    local ac=Duel.AnnounceCard(tp,table.unpack(s.announce_filter))
+	Duel.SetTargetParam(ac)
+    Duel.SetOperationInfo(0,CATEGORY_ANNOUNCE,nil,0,tp,ANNOUNCE_CARD_FILTER)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+    local code=Duel.GetChainInfo(0,CHAININFO_TARGET_PARAM)
+    local c=e:GetHandler()
+    Card.Recreate(c, code, nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,true)
+    	if c:IsRelateToEffect(e) and e:IsHasType(EFFECT_TYPE_ACTIVATE) then
+		if c:IsHasEffect(EFFECT_CANNOT_TO_HAND) then return end
+		c:CancelToGrave()
+		Duel.SendtoHand(c, tp, REASON_EFFECT)
+        Duel.ConfirmCards(1-tp, c)
+	end
+
+end
