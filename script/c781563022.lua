1d78a3d1a6bd71ffea2aced9f5b14d74432dd8ce
Fran Luque <franluque2@gmail.com>
2026-06-05 22:40:03 -0300

    Fixes to sunavalon

--- script/c781563022.lua
+++ script/c781563022.lua
@@ -27,6 +27,7 @@ function s.flipoppassive(e, tp, eg, ep, ev, re, r, rp)
 
     s.registersunvines(tp)
 
+    s.rewritetree(e,tp)
     
     local e2=Effect.CreateEffect(c)
     e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
@@ -47,6 +48,54 @@ function s.flipoppassive(e, tp, eg, ep, ev, re, r, rp)
     Duel.RegisterEffect(e6, tp)
 end
 
+function s.treefilter(c)
+    return c:IsCode(92770064) and (c:GetFlagEffect(id)==0)
+end
+
+
+function s.rewritetree(e,tp)
+    local g=Duel.GetMatchingGroup(s.treefilter, tp, LOCATION_EXTRA, 0, nil)
+    for tc in g:Iter() do
+        tc:RegisterFlagEffect(id, 0,0,0)
+        local effs={tc:GetOwnEffects()}
+
+        for _, eff in ipairs(effs) do
+            if eff:GetCategory()&CATEGORY_DESTROY~=0 then
+
+                local neweff=eff:Clone()
+                neweff:SetCost(s.newcost)
+                neweff:SetTarget(s.newtg)
+
+                eff:Reset()
+                tc:RegisterEffect(neweff)
+            end
+        end
+    end
+end
+
+function s.newcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	e:SetLabel(100)
+	return true
+end
+function s.costfilter(c,dg,lg)
+	return c:IsFaceup() and c:IsLinkMonster() and lg:IsContains(c) and (c:IsSetCard(SET_SUNVINE) or c:GetControler()~=c:GetOwner())
+end
+function s.newtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	local lg=c:GetLinkedGroup()
+	local dg=Duel.GetMatchingGroup(nil,tp,0,LOCATION_ONFIELD,nil,e)
+	if chk==0 then
+		if e:GetLabel()~=100 then return false end
+		e:SetLabel(0)
+		return #dg>0 and Duel.CheckReleaseGroupCost(tp,s.costfilter,1,false,aux.ReleaseCheckTarget,nil,dg,lg)
+	end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
+	local sg=Duel.SelectReleaseGroupCost(tp,s.costfilter,1,1,false,aux.ReleaseCheckTarget,nil,dg,lg)
+	Duel.Release(sg,REASON_COST)
+	local lk=sg:GetFirst():GetLink()
+	e:SetLabel(lk)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,dg,lk,0,0)
+end
 
 function s.efilter(e,c)
 	return c:IsCode(91557476)
