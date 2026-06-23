959ea8f517edb9ac70d984f446098771b6843480
Fran Luque <franluque2@gmail.com>
2026-06-23 00:02:13 -0300

    Balance Changes

--- script/c781563034.lua
+++ script/c781563034.lua
@@ -50,9 +50,40 @@ function s.flipoppassive(e, tp, eg, ep, ev, re, r, rp)
     e6:SetTarget(s.efilter)
     e6:SetValue(1)
     Duel.RegisterEffect(e6, tp)
+
+        local e2=Effect.CreateEffect(c)
+    e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+    e2:SetCode(EVENT_ADJUST)
+    e2:SetCondition(s.rewritecardscon)
+    e2:SetOperation(s.rewritecardsop)
+    Duel.RegisterEffect(e2, tp)
 end
 
 
 function s.efilter(e,c)
 	return c:IsCode(40591390,101402037) -- TODO: change 101402037 id to real id when dreadnought is released
+end
+
+
+
+function s.rewritecardscon(e,tp,eg,ep,ev,re,r,rp)
+    return Duel.IsExistingMatchingCard(s.torewritefilter, tp, LOCATION_ALL, 0, 1, nil)
+end
+
+function s.torewritefilter(c)
+    return c:IsOriginalCode(60461804) and c:IsOriginalType(TYPE_MONSTER) and c:GetFlagEffect(id) == 0
+end
+
+function s.rewritecardsop(e,tp,eg,ep,ev,re,r,rp)
+    local g=Duel.GetMatchingGroup(s.torewritefilter, tp, LOCATION_ALL, 0, nil)
+    for tc in g:Iter() do
+        tc:RegisterFlagEffect(id, 0,0,0)
+        local effs={tc:GetOwnEffects()}
+
+        for _, eff in ipairs(effs) do
+            if (eff:GetType()&EFFECT_TYPE_FIELD)==EFFECT_TYPE_FIELD and (eff:GetCode()&EFFECT_UPDATE_ATTACK)==EFFECT_UPDATE_ATTACK then
+                eff:Reset()
+            end
+        end
+    end
 end
