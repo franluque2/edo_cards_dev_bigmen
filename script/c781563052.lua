fa7de9dbb10e37ff6d9fbca295a6a7b1676cf153
Fran Luque <franluque2@gmail.com>
2026-08-17 23:54:53 -0300

    Nerf Yuri Skill

--- script/c781563052.lua
+++ script/c781563052.lua
@@ -26,10 +26,39 @@ function s.flipoppassive(e, tp, eg, ep, ev, re, r, rp)
         e2:SetValue(s.sumlimit)
         Duel.RegisterEffect(e2,tp)
 
+            local e0=Effect.CreateEffect(c)
+    e0:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+    e0:SetCode(EVENT_ADJUST)
+    e0:SetCondition(s.rewritedharedevilcon)
+    e0:SetOperation(s.rewritedharedevilop)
+    Duel.RegisterEffect(e0,tp)
 end
 
 
 function s.sumlimit(e,c)
 	if not c then return false end
 	return c:IsControler(e:GetHandlerPlayer())
+end
+
+local CARD_PREDAPLANT_DHAREDEIL=82370493
+
+function s.nottaggeddharedevilfilter(c)
+    return c:IsCode(CARD_PREDAPLANT_DHAREDEIL) and c:GetFlagEffect(id)==0
+end
+
+function s.rewritedharedevilcon(e,tp,eg,ep,ev,re,r,rp)
+    return Duel.IsExistingMatchingCard(s.nottaggeddharedevilfilter,tp,LOCATION_ALL,0,1,nil)
+end
+
+function s.rewritedharedevilop(e,tp,eg,ep,ev,re,r,rp)
+    local g=Duel.GetMatchingGroup(s.nottaggeddharedevilfilter,tp,LOCATION_ALL,0,nil)
+    for tc in g:Iter() do
+        tc:RegisterFlagEffect(id,0,0,1)
+        local effs={tc:GetOwnEffects()}
+        for _, eff in ipairs(effs) do
+            if eff:GetDescription()==aux.Stringid(CARD_PREDAPLANT_DHAREDEIL,0) then
+                eff:Reset()
+            end
+        end
+    end
 end
